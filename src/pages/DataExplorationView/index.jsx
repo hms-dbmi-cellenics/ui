a3416e5c611c4135819bda5705f0b2ac65ebbbe7
iva babukova <ibabukova@gmail.com>
2020-04-15 17:07:28 +0100

    Add back the components on the page

--- src/pages/DataExplorationView/index.jsx
+++ src/pages/DataExplorationView/index.jsx
@@ -180,7 +180,30 @@ class ExplorationViewPage extends React.Component {
 
     return (
       <>
-        <Scatterplot />
+        <Row>
+          <Col>
+            <PageHeader
+              className="site-page-header"
+              title="Exploration"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col span={16}>
+            <Scatterplot />
+          </Col>
+          <Col span={8}>
+            <Space direction="vertical" style={{ width: '100%' }}>
+              <Dropdown
+                overlay={menu}
+                trigger={['click']}
+              >
+                <Search placeholder="Search or browse tools..." onChange={(e) => this.filterTools(e.target.value)} />
+              </Dropdown>
+              {this.renderToolbar()}
+            </Space>
+          </Col>
+        </Row>
       </>
     );
   }
