10a6f33e3e05ec294eadda8091b14642da3111b6
iva babukova <ibabukova@gmail.com>
2020-05-01 12:38:11 +0100

    one working and one failed and commented out test

---
+++ src/pages/DataExplorationView/index.test.jsx
@@ -0,0 +1,11 @@
+/* eslint-env jest */
+import React from 'react';
+import { shallow, mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// tests to do:
+/**
+ * 1. Everything renders correctly
+ * 2. On search for tools, the correct tool gets opened
+ * 3. On search for plots, the correct plot gets opened
+ */
