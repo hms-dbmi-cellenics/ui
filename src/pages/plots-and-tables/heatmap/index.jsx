67778d0062610c04bffa25ee223e2e67f01e6349
Marcell Pek <marcell.pek@marcellp.net>
2020-08-27 13:00:39 +0100

    fix

--- src/pages/plots-and-tables/heatmap/index.jsx
+++ src/pages/plots-and-tables/heatmap/index.jsx
@@ -4,7 +4,11 @@ import {
 } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
 import { Vega } from 'react-vega';
-import heatmap from './heatmap.json';
+import _ from 'lodash';
+
+import cellSets from './cellSets.json';
+import expression from './expression.json';
+
 import DimensionsRangeEditor from '../components/DimensionsRangeEditor';
 import ColourbarDesign from '../components/ColourbarDesign';
 import LegendEditorSpecial from './components/LegendEditorSpecial';
@@ -12,6 +16,8 @@ import TitleDesign from '../components/TitleDesign';
 import FontDesign from '../components/FontDesign';
 import { updatePlotConfig, loadPlotConfig } from '../../../redux/actions/plots/index';
 import Header from '../components/Header';
+
+import generateSpec from '../../../utils/plotSpecs/generateHeatmapSpec';
 
 const { Panel } = Collapse;
 const routes = [
@@ -47,333 +53,35 @@ const HeatmapPlot = () => {
     dispatch(loadPlotConfig(experimentId, plotUuid, plotType));
   }, []);
 
-  const generateSpec = () => {
-    let legend = [];
-    if (config.legendLocation === 'horizontal') {
-      legend = [
-        {
-          fill: 'color',
-          type: 'gradient',
-          orient: 'bottom',
-          direction: 'horizontal',
-          title: ['Intensity'],
-          labelFont: { value: config.masterFont },
-          titleFont: { value: config.masterFont },
-          gradientLength: {
-            signal: 'width',
-          },
-        },
-        {
-          fill: 'colorids',
-          title: 'Cluster ID',
-          type: 'symbol',
-          orient: 'top',
-          offset: 40,
-          symbolType: 'square',
-          symbolSize: { value: 200 },
-          direction: 'horizontal',
-          labelFont: { value: config.masterFont },
-          titleFont: { value: config.masterFont },
-        }];
-    }
-    if (config.legendLocation === 'vertical') {
-      legend = [
-        {
-          fill: 'color',
-          type: 'gradient',
-          title: ['Intensity'],
-          labelFont: { value: config.masterFont },
-          titleFont: { value: config.masterFont },
-          gradientLength: {
-            signal: 'height / 3',
-          },
-        },
-        {
-          fill: 'colorids',
-          title: 'Cluster ID',
-          type: 'symbol',
-          orient: 'right',
-          symbolType: 'square',
-          symbolSize: { value: 200 },
-          direction: 'vertical',
-          labelFont: { value: config.masterFont },
-          titleFont: { value: config.masterFont },
-        }];
-    }
-    if (config.legendLocation === 'hide') {
-      legend = null;
-    }
-
-
-    return {
-      $schema: 'http//s:vega.github.io/schema/vega/v5.json',
-      width: config.width,
-      height: config.height,
-      autosize: { type: 'fit', resize: true },
-
-      data: [
-        {
-          name: 'cellNames',
-          values: heatmap.cellNames,
-          copy: true,
-          transform: [
-            {
-              type: 'identifier',
-              as: 'cellIndex',
-            },
-            {
-              type: 'formula',
-              as: 'cellIndex',
-              expr: 'datum.cellIndex-1',
-            },
-          ],
-        },
-        {
-          name: 'clusterIds',
-          values: heatmap.categories,
-          copy: true,
-          transform: [
-            {
-              type: 'flatten',
-              fields: ['values'],
-              as: ['cluster'],
-            },
-            {
-              type: 'window',
-              ops: ['row_number'],
-              as: ['cellIndex'],
-            },
-            {
-              type: 'formula',
-              as: 'cellIndex',
-              expr: 'datum.cellIndex-1',
-            },
-            {
-              type: 'lookup',
-              from: 'cellNames',
-              key: 'cellIndex',
-              fields: ['cellIndex'],
-              values: ['data'],
-              as: ['cellName'],
-            },
-            {
-              type: 'collect',
-              sort: { field: 'cluster' },
-            },
-          ],
-        },
-        {
-          name: 'heatmapData',
-          values: config.selectedData,
-          copy: true,
-          transform: [
-            {
-              type: 'flatten',
-              fields: ['expression'],
-              index: 'cellIndex',
-            },
-            {
-              type: 'lookup',
-              from: 'cellNames',
-              key: 'cellIndex',
-              fields: ['cellIndex'],
-              values: ['data'],
-              as: ['cellName'],
-            },
-          ],
-        },
-      ],
-
-      scales: [
-        {
-          name: 'x',
-          type: 'band',
-          domain: {
-            data: 'clusterIds',
-            field: 'cellName',
-          },
-          range: 'width',
-        },
-        {
-          name: 'y',
-          type: 'band',
-          domain: {
-            data: 'heatmapData',
-            field: 'geneName',
-          },
-          range: 'height',
-        },
-        {
-          name: 'color',
-          type: 'linear',
-          range: {
-            scheme: config.colGradient,
-          },
-          domain: {
-            data: 'heatmapData',
-            field: 'expression',
-          },
-          zero: false,
-          nice: true,
-        },
-        {
-          name: 'colorids',
-          type: 'ordinal',
-          range: [
-            'red', 'green', 'blue', 'teal', 'orange', 'purple', 'cyan', 'magenta',
-          ],
-
-          domain: {
-            data: 'clusterIds',
-            field: 'cluster',
-            sort: true,
-          },
-        },
-      ],
-      axes: [
-        {
-          from: { data: 'heatmapData' },
-          orient: 'left',
-          scale: 'y',
-          labelColor: config.labelColour,
-          domain: false,
-          // title: 'Gene',
-          labelFont: { value: config.masterFont },
-          titleFont: { value: config.masterFont },
-        },
-      ],
-
-      legends: legend,
-
-      marks: [
-        {
-          type: 'rect',
-          from: {
-            data: 'heatmapData',
-          },
-          encode: {
-            enter: {
-              x: {
-                scale: 'x',
-                field: 'cellName',
-              },
-              y: {
-                scale: 'y',
-                field: 'geneName',
-              },
-              width: {
-                scale: 'x',
-                band: 1,
-              },
-              height: {
-                scale: 'y',
-                band: 1,
-              },
-            },
-            update: {
-              fill: {
-                scale: 'color',
-                field: 'expression',
-              },
-            },
-          },
-        },
-        {
-          type: 'rect',
-          from: {
-            data: 'clusterIds',
-          },
-          encode: {
-            enter: {
-              x: {
-                scale: 'x',
-                field: 'cellName',
-              },
-              y: { value: -30 },
-              width: {
-                scale: 'x',
-                band: 1,
-              },
-              height: {
-                value: 20,
-              },
-            },
-            update: {
-              fill: {
-                scale: 'colorids',
-                field: 'cluster',
-                opacity: { value: 1 },
-              },
-              stroke: {
-                scale: 'colorids',
-                field: 'cluster',
-                opacity: { value: 1 },
-              },
-            },
-          },
-        },
-      ],
-      title:
-      {
-        text: { value: config.titleText },
-        color: { value: config.masterColour },
-        anchor: { value: config.titleAnchor },
-        font: { value: config.masterFont },
-        dx: { value: config.bounceX },
-        fontSize: { value: config.titleSize },
-      },
-    };
-  };
-
   // obj is a subset of what default config has and contains only the things we want change
   const updatePlotWithChanges = (obj) => {
     dispatch(updatePlotConfig(plotUuid, obj));
   };
 
-  const populateData = (value) => {
-    const newSelectedData = [];
-    config.selectedData = [];
+  const onDisplayedGenesSelect = (value) => {
     if (config.selectedGenes.length >= 53 || config.selectedGenes.length === 0) {
-      config.labelColour = 'transparent';
+      updatePlotWithChanges({ labelColour: 'transparent' });
     } else {
-      config.labelColour = 'black';
+      updatePlotWithChanges({ labelColour: 'black' });
     }
-    if (config.selectedGenes.length === 0 || value === 'redraw') {
-      updatePlotWithChanges({ selectedData: heatmap.heatmapData });
-      config.labelColour = 'transparent';
 
+    if (config.selectedGenes.length === 0 || value === 'redraw') {
+      updatePlotWithChanges({ labelColour: 'transparent' });
       return 0;
     }
-    for (let i = 0; i < heatmap.heatmapData.length; i += 1) {
-      const currentGene = heatmap.heatmapData[i].geneName;
-      if (config.selectedGenes.includes(currentGene)) {
-        newSelectedData.push({
-          geneName: currentGene,
-          expression: heatmap.heatmapData[i].expression,
-        });
-      }
-    }
-
-    updatePlotWithChanges({ selectedData: newSelectedData });
   };
 
   const getSortedGenes = () => {
-    let i;
+    const sortedGenes = Object.keys(expression);
 
-    const sortedGenes = [];
+    return sortedGenes.map((value) => ({ value }));
+  };
 
-    const genes = heatmap.heatmapData;
-    const genenames = [...new Set(genes.map((item) => item.geneName))];
-    genenames.sort();
+  const generateVegaData = () => {
+    const newCellSets = _.map(cellSets, (value, cellSetId) => ({ ...value, cellSetId }));
+    const newExpression = _.map(expression, (value, geneName) => ({ ...value, geneName }));
 
-    for (i = 0; i < genenames.length; i += 1) {
-      const value = genenames[i];
-      sortedGenes.push({
-        value,
-      });
-    }
-
-    return sortedGenes;
+    return { cellSets: newCellSets, expression: newExpression };
   };
 
 
@@ -390,7 +98,7 @@ const HeatmapPlot = () => {
             <Collapse defaultActiveKey={['1']}>
               <Panel header='Preview' key='1'>
                 <center>
-                  <Vega data={heatmap} spec={generateSpec()} renderer='canvas' />
+                  <Vega spec={generateSpec(config)} data={generateVegaData()} renderer='canvas' />
                 </center>
               </Panel>
             </Collapse>
@@ -411,14 +119,14 @@ const HeatmapPlot = () => {
                   <Button
                     type='primary'
                     config={config}
-                    onClick={() => populateData()}
+                    onClick={() => onDisplayedGenesSelect()}
                   >
                     Draw heatmap
                   </Button>
                   <Button
                     type='primary'
                     config={config}
-                    onClick={() => populateData('redraw')}
+                    onClick={() => onDisplayedGenesSelect('redraw')}
                   >
                     Reset
                   </Button>
