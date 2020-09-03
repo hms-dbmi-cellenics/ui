6b9477267e85492fb54c15a32ab589313da98ddf
Marcell Pek <marcell.pek@marcellp.net>
2020-09-03 13:27:10 +0100

    fix

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,11 +3,26 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import Head from 'next/head';
 import App from 'next/app';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import withReduxStore from '../redux/with-redux-store';
 import ContentWrapper from '../components/ContentWrapper';
 import PreloadContent from '../components/PreloadContent';
 import isBrowser from '../utils/environment';
 import '../../assets/self-styles.less';
+import '../../assets/nprogress.css';
+
+
+Router.events.on('routeChangeStart', (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => {
+  console.log('complete!');
+
+  NProgress.done();
+});
+Router.events.on('routeChangeError', () => NProgress.done());
 
 class MyApp extends App {
   // eslint-disable-next-line class-methods-use-this
