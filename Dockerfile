eb417ace4a061224680914b08c5be390402a4350
Marcell Pek <marcell.pek@marcellp.net>
2020-09-15 10:33:00 +0100

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # pull official base image
-FROM node:13.12.0-alpine
+FROM node:13.12.0-alpine AS builder
 
 # set working directory
 WORKDIR /app
@@ -7,14 +7,14 @@ WORKDIR /app
 # add `/app/node_modules/.bin` to $PATH
 ENV PATH /app/node_modules/.bin:$PATH
 
-# Copy node_modules we previously used.
-# This is not a great idea for a development build, but
-# in a CI environment we already have this cached, so we
-# may as well use it.
-COPY . ./
+# copy npm dependencies
+COPY package.json package-lock.json /app/
 
-# build the app
-RUN yarn build
+# install dependencies
+RUN npm ci --only=production
+
+# copy rest of app
+COPY . .
 
 # start app in production mode
-CMD ["yarn", "prod"]
+CMD ["npm",  "run", "prod"]
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,25 +1,21 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { Provider } from 'react-redux';
 import Head from 'next/head';
-import App from 'next/app';
 import Router from 'next/router';
 import NProgress from 'nprogress';
-import withReduxStore from '../redux/with-redux-store';
 import ContentWrapper from '../components/ContentWrapper';
 import PreloadContent from '../components/PreloadContent';
 import isBrowser from '../utils/environment';
+import wrapper from '../redux/store';
 import '../../assets/self-styles.less';
 import '../../assets/nprogress.css';
-
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-class MyApp extends App {
-  // eslint-disable-next-line class-methods-use-this
-  mainContent(Component, pageProps) {
+const WrappedApp = ({ Component, pageProps }) => {
+  const mainContent = () => {
     if (isBrowser) {
       return (
         <ContentWrapper>
@@ -28,20 +24,17 @@ class MyApp extends App {
       );
     }
     return (<PreloadContent />);
-  }
+  };
 
-  render() {
-    const { Component, pageProps, store } = this.props;
-    return (
-      <Provider store={store}>
-        <Head>
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <title>Biomage</title>
-        </Head>
-        {this.mainContent(Component, pageProps)}
-      </Provider>
-    );
-  }
-}
+  return (
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <title>Biomage</title>
+      </Head>
+      {mainContent(Component, pageProps)}
+    </>
+  );
+};
 
-export default withReduxStore(MyApp);
+export default wrapper.withRedux(WrappedApp);
