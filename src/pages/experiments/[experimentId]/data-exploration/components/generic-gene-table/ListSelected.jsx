adb51b4c7ef5c6db467e9f078ccde916406edee9
stefanbabukov <stefanbabukov98@gmail.com>
2020-10-07 11:19:04 +0100

    everything should be done

--- src/pages/experiments/[experimentId]/data-exploration/components/generic-gene-table/ListSelected.jsx
+++ src/pages/experiments/[experimentId]/data-exploration/components/generic-gene-table/ListSelected.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   Button, Select,
 } from 'antd';
@@ -8,27 +8,30 @@ const ListSelected = () => {
   const selectedGenes = useSelector((state) => state.genes.selected);
   const [listed, setListed] = useState(false);
   console.log(selectedGenes, listed);
-  if (listed && !selectedGenes.length) setListed(false);
-  if (listed) {
+  if (selectedGenes.length) {
+    if (listed) {
+      return (
+        <>
+          <Button type='link' size='small'
+            onClick={() => { setListed(false) }}
+          >Hide selected</Button>
+          <Select
+            value={selectedGenes}
+            mode='multiple'
+            showArrow={false}
+            removeIcon={(<div></div>)}
+            style={{ width: '100%' }}
+          />
+        </>
+      );
+    }
     return (
-      <>
-        <Button type='link' size='small'
-          onClick={() => { setListed(false) }}
-        >Hide selected</Button>
-        <Select
-          value={selectedGenes}
-          mode='multiple'
-          showArrow={false}
-          style={{ width: '100%' }}
-        />
-      </>
-    );
+      <Button type='link' size='small'
+        onClick={() => { setListed(true) }}
+      >List selected</Button>
+    )
   }
-  return (
-    <Button type='link' size='small'
-      onClick={() => { setListed(true) }}
-    >List selected</Button>
-  )
+  return (null);
 }
 
 export default ListSelected;
