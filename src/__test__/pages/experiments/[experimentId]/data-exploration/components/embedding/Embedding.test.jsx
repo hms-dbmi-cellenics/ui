5319c2e73ca79e0d28cede75db83b19c5402d988
Anugerah Erlaut <aerlaut@live.com>
2021-01-29 06:43:53 +0700

    fix tests on embedding

--- src/__test__/pages/experiments/[experimentId]/data-exploration/components/embedding/Embedding.test.jsx
+++ src/__test__/pages/experiments/[experimentId]/data-exploration/components/embedding/Embedding.test.jsx
@@ -31,7 +31,7 @@ const height = 200;
 describe('Embedding', () => {
   const initialState = {
     embeddings: {
-      umap: {
+      PCA: {
         ...initialEmbeddingState,
         loading: false,
         data: [[-13, 32], [6, 7], [43, 9], [57, 3]],
@@ -85,7 +85,7 @@ describe('Embedding', () => {
 
     component = mount(
       <Provider store={store}>
-        <Embedding experimentId='1234' embeddingType='umap' width={width} height={height} />
+        <Embedding experimentId='1234' embeddingType='PCA' width={width} height={height} />
       </Provider>,
     );
   });
@@ -96,259 +96,259 @@ describe('Embedding', () => {
 
   configure({ adapter: new Adapter() });
 
-  it('renders correctly a umap embedding', () => {
-    // const scatterplot = component.find(Scatterplot);
+  it('renders correctly a PCA embedding', () => {
+    const scatterplot = component.find(Scatterplot);
 
-    // expect(component.find('Embedding').length).toEqual(1);
-    // expect(scatterplot.length).toEqual(1);
-    // expect(scatterplot.getElement().props.mapping).toEqual('umap');
-    // expect(scatterplot.getElement().props.cellColors).toEqual(
-    //   new Map(
-    //     Object.entries({
-    //       // cell #2 is currently being hovered over, so it is black
-    //       2: [0, 0, 0],
+    expect(component.find('Embedding').length).toEqual(1);
+    expect(scatterplot.length).toEqual(1);
+    expect(scatterplot.getElement().props.mapping).toEqual('PCA');
+    expect(scatterplot.getElement().props.cellColors).toEqual(
+      new Map(
+        Object.entries({
+          // cell #2 is currently being hovered over, so it is black
+          2: [0, 0, 0],
 
-    //       // cell #3 is in louvain, which is currently in focus. it should be red.
-    //       3: [255, 0, 0],
-    //     }),
-    //   ),
-    // );
+          // cell #3 is in louvain, which is currently in focus. it should be red.
+          3: [255, 0, 0],
+        }),
+      ),
+    );
 
-    // expect(scatterplot.getElement().props.cells).toEqual(
-    //   {
-    //     0: {
-    //       mappings: {
-    //         umap: [-13, 32],
-    //       },
-    //     },
-    //     1: {
-    //       mappings: {
-    //         umap: [6, 7],
-    //       },
-    //     },
-    //     2: {
-    //       mappings: {
-    //         umap: [43, 9],
-    //       },
-    //     },
-    //     3: {
-    //       mappings: {
-    //         umap: [57, 3],
-    //       },
-    //     },
-    //   },
-    // );
+    expect(scatterplot.getElement().props.cells).toEqual(
+      {
+        0: {
+          mappings: {
+            PCA: [-13, 32],
+          },
+        },
+        1: {
+          mappings: {
+            PCA: [6, 7],
+          },
+        },
+        2: {
+          mappings: {
+            PCA: [43, 9],
+          },
+        },
+        3: {
+          mappings: {
+            PCA: [57, 3],
+          },
+        },
+      },
+    );
   });
 
-  // it('renders correctly a popover on lasso selection and closes it on cancel', () => {
-  //   const scatterplot = component.find(Scatterplot);
-  //   expect(component.find('ClusterPopover').length).toEqual(0);
+  it('renders correctly a popover on lasso selection and closes it on cancel', () => {
+    const scatterplot = component.find(Scatterplot);
+    expect(component.find('ClusterPopover').length).toEqual(0);
 
-  //   const selectedCellIds = new Set(['1', '2']);
-  //   // lasso select cells 1 and 2
-  //   act(() => {
-  //     scatterplot.getElement().props.updateCellsSelection(selectedCellIds);
-  //   });
-  //   component.update();
-  //   let popover = component.find('ClusterPopover');
-  //   expect(popover.length).toEqual(1);
+    const selectedCellIds = new Set(['1', '2']);
+    // lasso select cells 1 and 2
+    act(() => {
+      scatterplot.getElement().props.updateCellsSelection(selectedCellIds);
+    });
+    component.update();
+    let popover = component.find('ClusterPopover');
+    expect(popover.length).toEqual(1);
 
-  //   // close the popover
-  //   act(() => {
-  //     popover.getElement().props.onCancel();
-  //   });
-  //   component.update();
-  //   popover = component.find('ClusterPopover');
-  //   expect(popover.length).toEqual(0);
-  //   expect(store.getActions().length).toEqual(0);
-  // });
+    // close the popover
+    act(() => {
+      popover.getElement().props.onCancel();
+    });
+    component.update();
+    popover = component.find('ClusterPopover');
+    expect(popover.length).toEqual(0);
+    expect(store.getActions().length).toEqual(0);
+  });
 
-  // it('does not render the popover after lasso selection of 0 cells', () => {
-  //   const scatterplot = component.find(Scatterplot);
-  //   const selectedCellIds = new Set();
+  it('does not render the popover after lasso selection of 0 cells', () => {
+    const scatterplot = component.find(Scatterplot);
+    const selectedCellIds = new Set();
 
-  //   // lasso select cells 1 and 2
-  //   act(() => {
-  //     scatterplot.getElement().props.updateCellsSelection(selectedCellIds);
-  //   });
-  //   component.update();
+    // lasso select cells 1 and 2
+    act(() => {
+      scatterplot.getElement().props.updateCellsSelection(selectedCellIds);
+    });
+    component.update();
 
-  //   expect(component.find('ClusterPopover').length).toEqual(0);
-  // });
+    expect(component.find('ClusterPopover').length).toEqual(0);
+  });
 
-  // it('does not render cell info and crosshair when the popover is open', () => {
-  //   const scatterplot = component.find(Scatterplot);
-  //   // lasso select cells 1 and 2
-  //   const selectedCellIds = new Set(['1', '2']);
-  //   act(() => {
-  //     scatterplot.getElement().props.updateCellsSelection(selectedCellIds);
-  //   });
-  //   component.update();
+  it('does not render cell info and crosshair when the popover is open', () => {
+    const scatterplot = component.find(Scatterplot);
+    // lasso select cells 1 and 2
+    const selectedCellIds = new Set(['1', '2']);
+    act(() => {
+      scatterplot.getElement().props.updateCellsSelection(selectedCellIds);
+    });
+    component.update();
 
-  //   expect(component.find('ClusterPopover').length).toEqual(1);
-  //   expect(component.find(CrossHair).length).toEqual(0);
-  //   expect(component.find(CellInfo).length).toEqual(0);
-  // });
+    expect(component.find('ClusterPopover').length).toEqual(1);
+    expect(component.find(CrossHair).length).toEqual(0);
+    expect(component.find(CellInfo).length).toEqual(0);
+  });
 
-  // it('renders correctly a popover on lasso selection and creates a new cluster on create', () => {
-  //   const scatterplot = component.find(Scatterplot);
-  //   expect(component.find('ClusterPopover').length).toEqual(0);
+  it('renders correctly a popover on lasso selection and creates a new cluster on create', () => {
+    const scatterplot = component.find(Scatterplot);
+    expect(component.find('ClusterPopover').length).toEqual(0);
 
-  //   // lasso select cells 1 and 2
-  //   const selectedCellIds = new Set([1, 2]);
-  //   act(() => {
-  //     scatterplot.getElement().props.updateCellsSelection(selectedCellIds);
-  //   });
-  //   component.update();
-  //   const popover = component.find('ClusterPopover');
-  //   expect(popover.length).toEqual(1);
+    // lasso select cells 1 and 2
+    const selectedCellIds = new Set([1, 2]);
+    act(() => {
+      scatterplot.getElement().props.updateCellsSelection(selectedCellIds);
+    });
+    component.update();
+    const popover = component.find('ClusterPopover');
+    expect(popover.length).toEqual(1);
 
-  //   // click create in the popover
-  //   act(() => {
-  //     popover.getElement().props.onCreate();
-  //   });
-  //   component.update();
+    // click create in the popover
+    act(() => {
+      popover.getElement().props.onCreate();
+    });
+    component.update();
 
-  //   expect(component.find('ClusterPopover').length).toEqual(0);
-  //   expect(store.getActions().length).toEqual(2);
-  //   expect(store.getActions()[0].type).toEqual(CELL_SETS_CREATE);
-  //   expect(store.getActions()[0].payload.cellIds).toEqual(selectedCellIds);
-  // });
+    expect(component.find('ClusterPopover').length).toEqual(0);
+    expect(store.getActions().length).toEqual(2);
+    expect(store.getActions()[0].type).toEqual(CELL_SETS_CREATE);
+    expect(store.getActions()[0].payload.cellIds).toEqual(selectedCellIds);
+  });
 
-  // it('dispatches an action with updated cell information on hover', () => {
-  //   const scatterplot = component.find(Scatterplot);
+  it('dispatches an action with updated cell information on hover', () => {
+    const scatterplot = component.find(Scatterplot);
 
-  //   const hoveredCell = { cellId: 1 };
+    const hoveredCell = { cellId: 1 };
 
-  //   // hover over cells
-  //   act(() => {
-  //     scatterplot.getElement().props.updateCellsHover(hoveredCell);
-  //   });
+    // hover over cells
+    act(() => {
+      scatterplot.getElement().props.updateCellsHover(hoveredCell);
+    });
 
-  //   expect(store.getActions().length).toEqual(1);
-  //   expect(store.getActions()[0].type).toEqual(CELL_INFO_UPDATE);
-  //   expect(store.getActions()[0].payload.cellName).toEqual(hoveredCell.cellId);
-  // });
+    expect(store.getActions().length).toEqual(1);
+    expect(store.getActions()[0].type).toEqual(CELL_INFO_UPDATE);
+    expect(store.getActions()[0].payload.cellName).toEqual(hoveredCell.cellId);
+  });
 
-  // it('renders CrossHair and CellInfo components when user hovers over cell', () => {
-  //   store = mockStore(initialState);
+  it('renders CrossHair and CellInfo components when user hovers over cell', () => {
+    store = mockStore(initialState);
 
-  //   const mockProject = jest.fn((cellId) => store.getState().embeddings.umap.data[cellId]);
+    const mockProject = jest.fn((cellId) => store.getState().embeddings.PCA.data[cellId]);
 
-  //   const cellCoordinates = {
-  //     project: mockProject,
-  //   };
+    const cellCoordinates = {
+      project: mockProject,
+    };
 
-  //   component = mount(
-  //     <Provider store={store}>
-  //       <Embedding experimentId='1234' embeddingType='umap' width={width} height={height} />
-  //     </Provider>,
-  //   );
-  //   const scatterplot = component.find(Scatterplot);
+    component = mount(
+      <Provider store={store}>
+        <Embedding experimentId='1234' embeddingType='PCA' width={width} height={height} />
+      </Provider>,
+    );
+    const scatterplot = component.find(Scatterplot);
 
-  //   // hover over cells
-  //   act(() => {
-  //     component.find('div.vitessce-container').simulate('mouseMove');
-  //     scatterplot.getElement().props.updateViewInfo(cellCoordinates);
-  //   });
+    // hover over cells
+    act(() => {
+      component.find('div.vitessce-container').simulate('mouseMove');
+      scatterplot.getElement().props.updateViewInfo(cellCoordinates);
+    });
 
-  //   component.update();
+    component.update();
 
-  //   const crossHairs = component.find(CrossHair);
-  //   const cellInfo = component.find(CellInfo);
+    const crossHairs = component.find(CrossHair);
+    const cellInfo = component.find(CellInfo);
 
-  //   expect(mockProject).toHaveBeenCalledTimes(1);
-  //   expect(mockProject).toHaveBeenCalledWith(store.getState().cellInfo.cellName);
-  //   expect(crossHairs.length).toEqual(1);
-  //   expect(crossHairs.props().coordinates.current).toEqual(
-  //     {
-  //       x: store.getState().embeddings.umap.data[2][0],
-  //       y: store.getState().embeddings.umap.data[2][1],
-  //       width,
-  //       height,
-  //     },
-  //   );
-  //   expect(cellInfo.length).toEqual(1);
-  //   expect(crossHairs.props().coordinates.current).toEqual(crossHairs.props().coordinates.current);
-  // });
+    expect(mockProject).toHaveBeenCalledTimes(1);
+    expect(mockProject).toHaveBeenCalledWith(store.getState().cellInfo.cellName);
+    expect(crossHairs.length).toEqual(1);
+    expect(crossHairs.props().coordinates.current).toEqual(
+      {
+        x: store.getState().embeddings.PCA.data[2][0],
+        y: store.getState().embeddings.PCA.data[2][1],
+        width,
+        height,
+      },
+    );
+    expect(cellInfo.length).toEqual(1);
+    expect(crossHairs.props().coordinates.current).toEqual(crossHairs.props().coordinates.current);
+  });
 
-  // it('does not render CrossHair and CellInfo components when user zooms in or out of the embedding', () => {
-  //   store = mockStore(initialState);
+  it('does not render CrossHair and CellInfo components when user zooms in or out of the embedding', () => {
+    store = mockStore(initialState);
 
-  //   const mockProject = jest.fn((cellId) => store.getState().embeddings.umap.data[cellId]);
+    const mockProject = jest.fn((cellId) => store.getState().embeddings.PCA.data[cellId]);
 
-  //   const cellCoordinates = {
-  //     project: mockProject,
-  //   };
+    const cellCoordinates = {
+      project: mockProject,
+    };
 
-  //   component = mount(
-  //     <Provider store={store}>
-  //       <Embedding experimentId='1234' embeddingType='umap' width={width} height={height} />
-  //     </Provider>,
-  //   );
-  //   const scatterplot = component.find(Scatterplot);
+    component = mount(
+      <Provider store={store}>
+        <Embedding experimentId='1234' embeddingType='PCA' width={width} height={height} />
+      </Provider>,
+    );
+    const scatterplot = component.find(Scatterplot);
 
-  //   // hover over cells
-  //   act(() => {
-  //     component.find('div.vitessce-container').simulate('mouseMove');
-  //     component.find('div.vitessce-container').simulate('wheel');
-  //     scatterplot.getElement().props.updateViewInfo(cellCoordinates);
-  //   });
+    // hover over cells
+    act(() => {
+      component.find('div.vitessce-container').simulate('mouseMove');
+      component.find('div.vitessce-container').simulate('wheel');
+      scatterplot.getElement().props.updateViewInfo(cellCoordinates);
+    });
 
-  //   component.update();
+    component.update();
 
-  //   const crossHairs = component.find(CrossHair);
-  //   const cellInfo = component.find(CellInfo);
+    const crossHairs = component.find(CrossHair);
+    const cellInfo = component.find(CellInfo);
 
-  //   expect(mockProject).toHaveBeenCalledTimes(1);
-  //   expect(mockProject).toHaveBeenCalledWith(store.getState().cellInfo.cellName);
-  //   expect(crossHairs.length).toEqual(0);
-  //   expect(cellInfo.length).toEqual(0);
-  // });
+    expect(mockProject).toHaveBeenCalledTimes(1);
+    expect(mockProject).toHaveBeenCalledWith(store.getState().cellInfo.cellName);
+    expect(crossHairs.length).toEqual(0);
+    expect(cellInfo.length).toEqual(0);
+  });
 
-  // it('the gene expression view gets rendered correctly', () => {
-  //   const focusedState = {
-  //     ...initialState,
-  //     cellSets: {
-  //       ...initialState.cellSets,
-  //       selected: [],
-  //     },
-  //     genes: {
-  //       ...initialState.genes,
-  //       expression: {
-  //         loading: [],
-  //         data: {
-  //           REALGENE: {
-  //             min: 0,
-  //             max: 1.6,
-  //             expression: [0, 0.4, 0.5, 1.6],
-  //           },
-  //         },
-  //       },
-  //     },
-  //     cellInfo: {
-  //       ...initialState.cellInfo,
-  //       focus: {
-  //         ...initialState.cellInfo.focus,
-  //         store: 'genes',
-  //         key: 'REALGENE',
-  //       },
-  //     },
-  //   };
+  it('the gene expression view gets rendered correctly', () => {
+    const focusedState = {
+      ...initialState,
+      cellSets: {
+        ...initialState.cellSets,
+        selected: [],
+      },
+      genes: {
+        ...initialState.genes,
+        expression: {
+          loading: [],
+          data: {
+            REALGENE: {
+              min: 0,
+              max: 1.6,
+              expression: [0, 0.4, 0.5, 1.6],
+            },
+          },
+        },
+      },
+      cellInfo: {
+        ...initialState.cellInfo,
+        focus: {
+          ...initialState.cellInfo.focus,
+          store: 'genes',
+          key: 'REALGENE',
+        },
+      },
+    };
 
-  //   const geneExprStore = mockStore(focusedState);
+    const geneExprStore = mockStore(focusedState);
 
-  //   const embedding = mount(
-  //     <Provider store={geneExprStore}>
-  //       <Embedding experimentId='1234' embeddingType='umap' width={width} height={height} />
-  //     </Provider>,
-  //   );
+    const embedding = mount(
+      <Provider store={geneExprStore}>
+        <Embedding experimentId='1234' embeddingType='PCA' width={width} height={height} />
+      </Provider>,
+    );
 
-  //   const legend = embedding.find('Embedding div div img');
-  //   expect(legend.length).toEqual(1);
+    const legend = embedding.find('Embedding div div img');
+    expect(legend.length).toEqual(1);
 
-  //   const focusedGeneInfo = embedding.find('Embedding div label strong');
-  //   expect(focusedGeneInfo.length).toEqual(1);
-  //   expect(focusedGeneInfo.props().children).toEqual('REALGENE');
-  // });
+    const focusedGeneInfo = embedding.find('Embedding div label strong');
+    expect(focusedGeneInfo.length).toEqual(1);
+    expect(focusedGeneInfo.props().children).toEqual('REALGENE');
+  });
 });
