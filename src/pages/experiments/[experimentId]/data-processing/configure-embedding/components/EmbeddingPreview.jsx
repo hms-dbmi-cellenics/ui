d6689e0d599f6840f1beda060b1afe1251773823
Anugerah Erlaut <aerlaut@live.com>
2021-02-02 22:36:16 +0700

    update configure embedding to use new store

--- src/pages/experiments/[experimentId]/data-processing/configure-embedding/components/EmbeddingPreview.jsx
+++ src/pages/experiments/[experimentId]/data-processing/configure-embedding/components/EmbeddingPreview.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { useRouter } from 'next/router';
 import {
   Row, Col, Space, Button, Tooltip, PageHeader, Spin, Collapse,
