4e44062482b7c3c2de69ddc5eb9b4e4c6cb3d487
Marcell Pek <marcell.pek@marcellp.net>
2021-02-02 14:44:07 +0000

    fix

---
+++ src/pages/_error.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Result, Typography, Space } from 'antd';
+
+import FeedbackButton from '../components/FeedbackButton';
+
+const { Title, Text } = Typography;
+
+const Error = ({ statusCode }) => (
+  <Result
+    title={<Title level={2}>It&apos;s not you, it&apos;s us.</Title>}
+    icon={(
+      <img
+        alt='Illustration of people looking into bushes to find something.'
+        src='/undraw_server_down_s4lk.svg'
+        width={250}
+        height={250}
+      />
+    )}
+    subTitle={(
+      <>
+        <Title
+          level={4}
+          style={{ fontWeight: 'normal' }}
+        >
+          Sorry, something went wrong on our end. We&apos;re working hard to fix the problem.
+        </Title>
+
+        <Space direction='vertical' style={{ width: '100%' }}>
+          <Text>
+            If you need immediate help, please
+            {' '}
+            <a href='mailto:hello@biomage.net'>contact us</a>
+            .
+            {' '}
+            If the problem persists,
+            please leave feedback using the button below.
+            <br />
+            Thank you for your patience, we&apos;ll be up and running shortly.
+          </Text>
+
+          <Text type='secondary'>
+            {statusCode ? `HTTP ${statusCode}` : 'SSR error'}
+          </Text>
+        </Space>
+      </>
+    )}
+    extra={(
+      <Space direction='vertical' style={{ width: '100%' }}>
+        <FeedbackButton />
+      </Space>
+    )}
+  />
+);
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
