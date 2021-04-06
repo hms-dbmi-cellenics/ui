b09918c97796cc89049310e332201e320012f4c6
StefanBabukov <stefanbabukov98@gmail.com>
2021-04-06 14:55:38 +0300

    scratchpad deletion bug (#191)

--- src/redux/reducers/cellSets/cellSetsDelete.js
+++ src/redux/reducers/cellSets/cellSetsDelete.js
@@ -4,7 +4,6 @@ const cellSetsDelete = (state, action) => {
   const { key } = action.payload;
 
   const newState = _.cloneDeep(state);
-
   // Modify hierarchy to remove the cell set.
   newState.hierarchy = newState.hierarchy.filter((rootNode) => {
     // If it's a root we're removing, delete all its children.
@@ -29,7 +28,7 @@ const cellSetsDelete = (state, action) => {
   delete newState.properties[key];
 
   // If the key was in the list of selected keys, make sure we remove it from there.
-  newState.selected = newState.selected.filter((selectedKey) => selectedKey !== key);
+  newState.selected = newState.selected.cellSets?.filter((selectedKey) => selectedKey !== key);
 
   // Delete from hidden if it was selected to be hidden.
   newState.hidden.delete(key);
