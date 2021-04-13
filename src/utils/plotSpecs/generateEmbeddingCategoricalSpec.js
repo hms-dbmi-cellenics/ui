658ac4186f5b928e0c9e213d4c057b4f85c9b168
Martin Fosco <martinfosco@Martins-MacBook-Pro.local>
2021-04-13 15:03:23 -0300

    Fix categorical embedding in plots and tables

--- src/utils/plotSpecs/generateEmbeddingCategoricalSpec.js
+++ src/utils/plotSpecs/generateEmbeddingCategoricalSpec.js
@@ -208,13 +208,16 @@ const filterCells = (cellSets, selectedCellSet) => {
 const generateData = (cellSets, selectedCellSet, embeddingData) => {
   const newCellSets = filterCells(cellSets, selectedCellSet);
 
-  const test = newCellSets.filter((d) => d.cellId < embeddingData.length).map((data) => (
-    {
-      ...data,
-      x: embeddingData[data.cellId][0],
-      y: embeddingData[data.cellId][1],
-    }
-  ));
+  const test = newCellSets
+    .filter((d) => d.cellId < embeddingData.length)
+    .filter((data) => embeddingData[data.cellId]) // filter out cells removed in data processing
+    .map((data) => (
+      {
+        ...data,
+        x: embeddingData[data.cellId][0],
+        y: embeddingData[data.cellId][1],
+      }
+    ));
 
   return test;
 };
