90d42db2ef2c6f4518f418a622560bc9cdeaf58e
Martin Fosco <cosa65@users.noreply.github.com>
2021-04-16 13:42:49 -0300

    Data upload progress UI (#210)
    * Refactor UploadStatus and add new error messages for FileStream related errors
    
    * Switch pako for fflate that allows for background compression and implement upload status meter
    
    * Fix tests
    
    * Minor fix
    
    * Change .then promise handling to await
    
    * Separate storage.put into a different function
    
    Co-authored-by: Martin Fosco <martinfosco@192.168.1.5>
    Co-authored-by: Martin Fosco <martinfosco@Martins-MacBook-Pro.local>

--- src/utils/UploadStatus.js
+++ src/utils/UploadStatus.js
@@ -1,9 +1,46 @@
-const UploadStatus = {
-  UPLOADED: 'uploaded',
-  UPLOADING: 'uploading',
-  UPLOAD_ERROR: 'uploadError',
-  FILE_NOT_FOUND: 'fileNotFound',
-  DATA_MISSING: 'dataMissing',
-};
+class UploadStatus {
+  static UPLOADED = new UploadStatus('uploaded');
+
+  static UPLOADING = new UploadStatus('uploading');
+
+  static UPLOAD_ERROR = new UploadStatus('uploadError');
+
+  static FILE_NOT_FOUND = new UploadStatus('fileNotFound');
+
+  static DATA_MISSING = new UploadStatus('dataMissing');
+
+  static FILE_READ_ERROR = new UploadStatus('fileReadError');
+
+  static FILE_READ_ABORTED = new UploadStatus('fileReadAborted');
+
+  constructor(key) {
+    this.key = key;
+  }
+
+  toString() {
+    return this.key;
+  }
+
+  message() {
+    switch (this) {
+      case UploadStatus.UPLOADED:
+        return 'Uploaded';
+      case UploadStatus.UPLOADING:
+        return 'Uploading...';
+      case UploadStatus.UPLOAD_ERROR:
+        return 'Upload error';
+      case UploadStatus.FILE_NOT_FOUND:
+        return 'File not found';
+      case UploadStatus.DATA_MISSING:
+        return 'Data missing';
+      case UploadStatus.FILE_READ_ERROR:
+        return 'File read error';
+      case UploadStatus.FILE_READ_ABORTED:
+        return 'File read aborted';
+      default:
+        return '';
+    }
+  }
+}
 
 export default UploadStatus;
--- src/utils/loadAndCompressIfNecessary.js
+++ src/utils/loadAndCompressIfNecessary.js
@@ -1,20 +1,27 @@
-import pako from 'pako';
+import { gzip } from 'fflate';
+
+function uintArrayToBuffer(array) {
+  return array.buffer.slice(array.byteOffset, array.byteLength + array.byteOffset);
+}
 
 const loadAndCompressIfNecessary = async (file) => {
   const inGzipFormat = file.bundle.mime === 'application/gzip';
 
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
-    reader.onabort = () => reject(new Error('Operation aborted'));
-    reader.onerror = () => reject(new Error('Error'));
+    reader.onabort = () => reject(new Error('aborted'));
+    reader.onerror = () => reject(new Error('error'));
     reader.onload = () => {
       const loadedFile = reader.result;
 
       if (inGzipFormat) {
         resolve(loadedFile);
       } else {
-        const compressed = pako.gzip(loadedFile, { to: 'string' });
-        resolve(compressed);
+        gzip(new Uint8Array(loadedFile), {}, (error, compressedFile) => {
+          if (error) { reject(new Error('error')); }
+
+          resolve(uintArrayToBuffer(compressedFile));
+        });
       }
     };
 
