fe6ad50281674f451ee0030e95875f4b2d96b23e
Martin Fosco <cosa65@users.noreply.github.com>
2021-04-21 16:28:11 -0300

    Data upload progress ui (#216)
    * Refactor UploadStatus and add new error messages for FileStream related errors
    
    * Switch pako for fflate that allows for background compression and implement upload status meter
    
    * Fix tests
    
    * Minor fix
    
    * Change .then promise handling to await
    
    * Separate storage.put into a different function
    
    * Fix
    
    * Change from new UintArrayBuffer to Buffer.from()
    
    * Switch from using nodejs core Buffer to explicitly imported npm module
    
    Co-authored-by: Martin Fosco <martinfosco@192.168.1.5>
    Co-authored-by: Martin Fosco <martinfosco@Martins-MacBook-Pro.local>

--- src/utils/loadAndCompressIfNecessary.js
+++ src/utils/loadAndCompressIfNecessary.js
@@ -1,20 +1,30 @@
-import pako from 'pako';
+import { gzip } from 'fflate';
+import { Buffer } from 'buffer/';
+
+function uintArrayToBuffer(array) {
+  return array.buffer.slice(array.byteOffset, array.byteLength + array.byteOffset);
+}
 
 const loadAndCompressIfNecessary = async (file) => {
   const inGzipFormat = file.bundle.mime === 'application/gzip';
 
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
-    reader.onabort = () => reject(new Error('Operation aborted'));
-    reader.onerror = () => reject(new Error('Error'));
+    reader.onabort = () => reject(new Error('aborted'));
+    reader.onerror = () => reject(new Error('error'));
     reader.onload = () => {
       const loadedFile = reader.result;
 
       if (inGzipFormat) {
         resolve(loadedFile);
       } else {
-        const compressed = pako.gzip(loadedFile, { to: 'string' });
-        resolve(compressed);
+        const loadedFileUint = Buffer.from(loadedFile);
+
+        gzip(loadedFileUint, {}, (error, compressedFile) => {
+          if (error) { reject(new Error('error')); }
+
+          resolve(uintArrayToBuffer(compressedFile));
+        });
       }
     };
 
