5e9f69e6c585f866f1fef88589983222a1452047
Martin Fosco <martinfosco@Martins-MBP.fibertel.com.ar>
2021-04-29 13:44:05 -0300

    Set more coherent name for localValue

--- src/components/SliderWithInput.jsx
+++ src/components/SliderWithInput.jsx
@@ -13,10 +13,10 @@ const SliderWithInput = (props) => {
 
   const [, handleChange] = useUpdateThrottled(onUpdate, value);
 
-  const [newLocalValue, setNewValue] = useState(value);
+  const [localValue, setLocalValue] = useState(value);
 
   useEffect(() => {
-    setNewValue(value);
+    setLocalValue(value);
   }, [value]);
 
   const stepToSet = step ?? max / 200;
@@ -24,26 +24,26 @@ const SliderWithInput = (props) => {
   return (
     <div style={{ display: 'flex', whiteSpace: 'nowrap' }}>
       <Slider
-        value={newLocalValue}
+        value={localValue}
         min={min}
         max={max}
-        onChange={setNewValue}
-        onAfterChange={() => handleChange(newLocalValue)}
+        onChange={setLocalValue}
+        onAfterChange={() => handleChange(localValue)}
         step={stepToSet}
         disabled={disabled}
         style={{ minWidth: 100, display: 'inline-block', flexGrow: 100 }}
       />
 
       <InputNumber
-        value={newLocalValue}
+        value={localValue}
         min={min}
         max={max}
         onChange={(changedValue) => {
           const changedValueWithinBounds = Math.min(Math.max(changedValue, min), max);
-          setNewValue(changedValueWithinBounds);
+          setLocalValue(changedValueWithinBounds);
         }}
-        onPressEnter={() => { handleChange(newLocalValue); }}
-        onStep={() => { handleChange(newLocalValue); }}
+        onPressEnter={() => { handleChange(localValue); }}
+        onStep={() => { handleChange(localValue); }}
         step={stepToSet}
         disabled={disabled}
         style={{ width: 80, display: 'inline-block' }}
