af0b4890981f4c89161812bad3a4da4c18556980
Martin Fosco <martinfosco@192.168.1.9>
2021-05-11 19:44:24 -0300

    Remove unnecessary beforeEach

--- src/__test__/components/data-management/FileUploadModal.test.jsx
+++ src/__test__/components/data-management/FileUploadModal.test.jsx
@@ -14,38 +14,6 @@ const mockStore = configureMockStore([thunk]);
 const store = mockStore({});
 
 describe('FileUploadModal', () => {
-  beforeEach(() => {
-    Object.defineProperty(window, 'matchMedia', {
-      writable: true,
-      value: jest.fn().mockImplementation((query) => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // deprecated
-        removeListener: jest.fn(), // deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-      })),
-    });
-  });
-
-  beforeEach(() => {
-    Object.defineProperty(window, 'matchMedia', {
-      writable: true,
-      value: jest.fn().mockImplementation((query) => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // deprecated
-        removeListener: jest.fn(), // deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-      })),
-    });
-  });
-
   it('renders without options', () => {
     const component = shallow(
       <Provider store={store}>
