44aa8c074ed75394828c6a94d7b988487e42be90
Martin Fosco <cosa65@users.noreply.github.com>
2021-05-14 10:21:51 -0300

    Fix error status bug (#251)
    * Fix error status bug
    
    * Add same check to other type of file upload error
    
    Co-authored-by: Martin Fosco <martinfosco@Martins-MacBook-Pro.local>

--- src/__test__/utils/processUpload.test.js
+++ src/__test__/utils/processUpload.test.js
@@ -223,11 +223,18 @@ describe('processUpload (in development)', () => {
       (status) => status === UploadStatus.FILE_READ_ERROR,
     );
 
+    const uploadedFileStatuses = filesStatuses.filter(
+      (status) => status === UploadStatus.UPLOADED,
+    );
+
     // There are 3 files actions with status uploading
     expect(uploadingFileStatuses.length).toEqual(6);
 
     // There are 3 files actions with status upload error
     expect(errorFileStatuses.length).toEqual(3);
+
+    // There are no file actions with status successfully uploaded
+    expect(uploadedFileStatuses.length).toEqual(0);
   });
 
   it('Updates redux correctly when there are file upload errors', async () => {
@@ -261,10 +268,17 @@ describe('processUpload (in development)', () => {
       (status) => status === UploadStatus.UPLOAD_ERROR,
     );
 
+    const uploadedFileStatuses = filesStatuses.filter(
+      (status) => status === UploadStatus.UPLOADED,
+    );
+
     // There are 3 files actions with status uploading
     expect(uploadingFileStatuses.length).toEqual(6);
 
     // There are 3 files actions with status upload error
     expect(errorFileStatuses.length).toEqual(3);
+
+    // There are no file actions with status successfully uploaded
+    expect(uploadedFileStatuses.length).toEqual(0);
   });
 });
