eb9b4b3c5604430a89b92fa2504110a24b6d607e
Anugerah Erlaut <anugerah@biomage.net>
2021-05-18 21:00:53 +0700

    Gem2s loading UI (#257)

---
+++ src/__test__/components/GEM2SLoadingScreen.test.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { mount, shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import preloadAll from 'jest-next-dynamic';
+import {
+  Result, Button, Progress,
+} from 'antd';
+import GEM2SLoadingScreen from '../../components/GEM2SLoadingScreen';
+
+configure({ adapter: new Adapter() });
+
+describe('GEM2SLoadingScreen', () => {
+  beforeAll(async () => {
+    await preloadAll();
+  });
+
+  it('Does not render without pipeline status', () => {
+    expect(() => shallow(<GEM2SLoadingScreen />)).toThrow();
+  });
+
+  it('Renders toBeRun state correctly', () => {
+    const component = mount(
+      <GEM2SLoadingScreen pipelineStatus='toBeRun' />,
+    );
+
+    const display = component.find(Result);
+
+    expect(display.props().status).toEqual('info');
+    expect(display.find(Button).length).toEqual(1);
+    expect(display.find(Progress).length).toEqual(0);
+  });
+
+  it('Renders error state correctly', () => {
+    const component = mount(
+      <GEM2SLoadingScreen pipelineStatus='error' />,
+    );
+
+    const display = component.find(Result);
+
+    expect(display.props().status).toEqual('error');
+    expect(display.find(Button).length).toEqual(1);
+    expect(display.find(Progress).length).toEqual(0);
+  });
+
+  it('Renders running state correctly', () => {
+    const completedSteps = [
+      'step 1',
+      'step 2',
+    ];
+
+    const steps = [
+      'step 1',
+      'step 2',
+      'step 3',
+    ];
+
+    const component = mount(
+      <GEM2SLoadingScreen pipelineStatus='running' completedSteps={completedSteps} steps={steps} />,
+    );
+
+    const display = component.find(Result);
+
+    expect(display.props().status).toEqual('running');
+    expect(display.find(Button).length).toEqual(0);
+    expect(display.find(Progress).length).toEqual(1);
+
+    // Display step information as shown in steps
+    expect(display.find('span.ant-typography').first().text()).toEqual(steps[completedSteps.length]);
+  });
+});
--- src/components/ContentWrapper.jsx
+++ src/components/ContentWrapper.jsx
@@ -19,6 +19,7 @@ import NotificationManager from './notification/NotificationManager';
 import initUpdateSocket from '../utils/initUpdateSocket';
 import { loadPipelineStatus } from '../redux/actions/experimentSettings';
 import PipelineRedirectToDataProcessing from './PipelineRedirectToDataProcessing';
+import GEM2SLoadingScreen from './GEM2SLoadingScreen';
 
 import experimentUpdatesHandler from '../utils/experimentUpdatesHandler';
 
@@ -39,22 +40,27 @@ const ContentWrapper = (props) => {
   const route = router?.route || '';
 
   const {
-    loading: pipelineLoading,
-    error: pipelineError,
-    status: workerAndPipelineStatus,
+    loading: backendLoading,
+    error: backendError,
+    status: backendStatus,
   } = useSelector((state) => state.experimentSettings.pipelineStatus);
 
-  const pipelineStatusKey = workerAndPipelineStatus.pipeline?.status;
-  const pipelineErrors = ['FAILED', 'TIMED_OUT', 'ABORTED'];
+  const backendErrors = ['FAILED', 'TIMED_OUT', 'ABORTED'];
 
+  const pipelineStatusKey = backendStatus.pipeline?.status;
   const pipelineRunning = pipelineStatusKey === 'RUNNING';
-  const pipelineRunningError = pipelineErrors.includes(pipelineStatusKey);
+  const pipelineRunningError = backendErrors.includes(pipelineStatusKey);
+
+  const gem2sStatusKey = backendStatus.gem2s?.status;
+  const gem2sRunning = gem2sStatusKey === 'RUNNING';
+  const gem2sRunningError = backendErrors.includes(pipelineStatusKey);
+  const completedGem2sSteps = backendStatus.gem2s?.completedSteps;
 
   // This is used to prevent a race condition where the page would start loading immediately
-  // when the pipeline status was previously loaded. In that case, `pipelineLoading` is `false`
+  // when the pipeline status was previously loaded. In that case, `backendLoading` is `false`
   // and would be set to true only in the `loadPipelineStatus` action, the time between the
   // two events would allow pages to load.
-  const [pipelineStatusRequested, setPipelineStatusRequested] = useState(false);
+  const [backendStatusRequested, setBackendStatusRequested] = useState(false);
 
   const updateSocket = useRef(null);
   useEffect(() => {
@@ -68,12 +74,12 @@ const ContentWrapper = (props) => {
   }, [experimentId]);
 
   useEffect(() => {
-    if (pipelineStatusRequested) {
+    if (backendStatusRequested) {
       return;
     }
 
-    setPipelineStatusRequested(true);
-  }, [pipelineLoading]);
+    setBackendStatusRequested(true);
+  }, [backendLoading]);
 
   const BigLogo = () => (
     <div
@@ -205,12 +211,25 @@ const ContentWrapper = (props) => {
 
   const renderContent = () => {
     if (experimentId) {
-      if (pipelineLoading || !pipelineStatusRequested) {
+      if (
+        backendLoading || !backendStatusRequested) {
         return <PreloadContent />;
       }
 
-      if (pipelineError) {
-        return <Error errorText='Could not get current pipeline settings.' />;
+      if (backendError) {
+        return <Error errorText='Could not get backend settings.' />;
+      }
+
+      if (gem2sRunningError) {
+        return <GEM2SLoadingScreen pipelineStatus='error' />;
+      }
+
+      if (gem2sRunning) {
+        return <GEM2SLoadingScreen pipelineStatus='running' completedSteps={completedGem2sSteps} />;
+      }
+
+      if (gem2sStatusKey === 'NotCreated') {
+        return <GEM2SLoadingScreen pipelineStatus='toBeRun' />;
       }
 
       if (pipelineRunningError && !route.includes('data-processing')) {
@@ -220,7 +239,7 @@ const ContentWrapper = (props) => {
       if (pipelineRunning && !route.includes('data-processing')) {
         return <PipelineRedirectToDataProcessing experimentId={experimentId} pipelineStatus='running' />;
       }
-      
+
       if (process.env.NODE_ENV === 'development') {
         return children;
       }
@@ -229,7 +248,7 @@ const ContentWrapper = (props) => {
         return <PipelineRedirectToDataProcessing experimentId={experimentId} pipelineStatus='toBeRun' />;
       }
     }
-    
+
     return children;
   };
 
@@ -238,7 +257,7 @@ const ContentWrapper = (props) => {
   }) => {
     const noExperimentDisable = !experimentId ? disableIfNoExperiment : false;
     const pipelineStatusDisable = disabledByPipelineStatus && (
-      pipelineError || pipelineRunning || pipelineRunningError
+      backendError || pipelineRunning || pipelineRunningError
     );
 
     return (
