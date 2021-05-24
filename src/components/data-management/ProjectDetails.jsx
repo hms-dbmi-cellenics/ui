064e1f5d5d4e694063dfb0638ac9cbb0ad429b8b
Xavier Vergés <460418+xverges@users.noreply.github.com>
2021-05-24 21:19:10 +0200

    [BIOMAGE-1000] bad url handling to get the organism list (#268)
    * Different treatment for api and non api urls
    
    * Keep linter happy
    
    * Rename file

--- src/components/data-management/ProjectDetails.jsx
+++ src/components/data-management/ProjectDetails.jsx
@@ -22,7 +22,7 @@ import AnalysisModal from './AnalysisModal';
 import UploadDetailsModal from './UploadDetailsModal';
 import MetadataPopover from './MetadataPopover';
 
-import getFromApiExpectOK from '../../utils/getFromApiExpectOK';
+import { getFromUrlExpectOK } from '../../utils/getDataExpectOK';
 import {
   deleteSamples, updateSample,
 } from '../../redux/actions/samples';
@@ -60,7 +60,7 @@ const ProjectDetails = ({ width, height }) => {
   const analysisPath = '/experiments/[experimentId]/data-processing';
   const { data: speciesData } = useSWR(
     'https://biit.cs.ut.ee/gprofiler/api/util/organisms_list/',
-    getFromApiExpectOK,
+    getFromUrlExpectOK,
   );
   const projects = useSelector((state) => state.projects);
   const experiments = useSelector((state) => state.experiments);
@@ -374,7 +374,9 @@ const ProjectDetails = ({ width, height }) => {
             }}
             onClearAll={() => {
               activeProject.samples.forEach(
-                (sampleUuid) => dispatch(updateSample(sampleUuid, { metadata: { [key]: defaultNA } })),
+                (sampleUuid) => dispatch(
+                  updateSample(sampleUuid, { metadata: { [key]: defaultNA } }),
+                ),
               );
             }}
             massEdit
@@ -511,7 +513,9 @@ const ProjectDetails = ({ width, height }) => {
   };
 
   useEffect(() => {
-    if (projects.ids.length === 0 || !activeProject || !samples.ids.includes(activeProject.samples[0])) {
+    if (projects.ids.length === 0
+      || !activeProject
+      || !samples.ids.includes(activeProject.samples[0])) {
       setTableData([]);
       setTableColumns([]);
       return;
