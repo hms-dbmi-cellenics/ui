525386e43c169ec7acda34db80389bae7e3e3703
Martin Fosco <cosa65@users.noreply.github.com>
2021-05-27 11:36:07 -0300

    Fix transition between gem2s and data processing (#277)
    * Fix various bugs
    
    * Wrong prop types
    
    Co-authored-by: Martin Fosco <martinfosco@192.168.1.7>
    Co-authored-by: Xavier Verges <460418+xverges@users.noreply.github.com>

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Result, Button, Typography } from 'antd';
 import FeedbackButton from '../components/FeedbackButton';
 
@@ -46,15 +47,15 @@ const NotFoundPage = ({ title, subTitle, hint }) => (
 );
 
 NotFoundPage.defaultProps = {
-  hint: null,
+  hint: '',
   title: 'Page not found',
   subTitle: 'We can\'t seem to find the page you\'re looking for.',
 };
 
 NotFoundPage.propTypes = {
-  title: NotFoundPage.string,
-  subTitle: NotFoundPage.string,
-  hint: NotFoundPage.string,
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
+  hint: PropTypes.string,
 };
 
 export default NotFoundPage;
