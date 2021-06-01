c400bd2cb6d3b0703c288b91a51047418fc48d11
Martin Fosco <cosa65@users.noreply.github.com>
2021-06-01 15:25:30 -0300

    Fix delete project showing error that should not appear (#297)
    * Move select action to before other updates in redux
    
    * Fix tests
    
    * Add filter to avoid reselecting delected project if it is first in list
    
    Co-authored-by: Martin Fosco <martinfosco@Martins-MacBook-Pro.local>

--- src/redux/actions/projects/deleteProject.js
+++ src/redux/actions/projects/deleteProject.js
@@ -47,6 +47,16 @@ const deleteProject = (
       throw new Error(response.json().message);
     }
 
+    // If deleted project is the same as the active project, choose another project
+    if (projectUuid === activeProjectUuid) {
+      const leftoverProjectIds = projects.ids.filter((uuid) => uuid !== activeProjectUuid);
+
+      dispatch({
+        type: PROJECTS_SET_ACTIVE,
+        payload: { projectUuid: leftoverProjectIds.length ? leftoverProjectIds[0] : null },
+      });
+    }
+
     dispatch({
       type: EXPERIMENTS_DELETED,
       payload: {
@@ -69,14 +79,6 @@ const deleteProject = (
     dispatch({
       type: PROJECTS_SAVED,
     });
-
-    // If deleted project is the same as the active project, choose another project
-    if (projectUuid === activeProjectUuid) {
-      dispatch({
-        type: PROJECTS_SET_ACTIVE,
-        payload: { projectUuid: projects.ids.length > 1 ? projects.ids[0] : null },
-      });
-    }
   } catch (e) {
     pushNotificationMessage('error', errorTypes.PROJECTS_DELETE);
 
