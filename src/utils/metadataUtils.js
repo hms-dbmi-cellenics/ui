f32bb994ac07ef19f130d74f6c8821bf8790ba87
Anugerah Erlaut <aerlaut@live.com>
2021-06-02 21:57:28 +0700

    change from dash to underscore

--- src/utils/metadataUtils.js
+++ src/utils/metadataUtils.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-param-reassign */
-const metadataKeyToName = (key) => key.replace('_', ' ');
+const metadataKeyToName = (key) => key.replace('-', ' ').replace('_', ' ');
 
 const metadataNameToKey = (name) => `${name.trim().replace(/\s+/g, '_')}`;
 
-const temporaryMetadataKey = (columns) => `metadata-${columns.length}`;
+const temporaryMetadataKey = (columns) => `metadata_${columns.length}`;
 
 export {
   metadataKeyToName,
