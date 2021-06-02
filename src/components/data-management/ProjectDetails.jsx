3efc1ab58cf75119911179793136f1b261619215
Anugerah Erlaut <aerlaut@live.com>
2021-06-03 00:04:05 +0700

    remove trigger on SUCCEEDED

--- src/components/data-management/ProjectDetails.jsx
+++ src/components/data-management/ProjectDetails.jsx
@@ -614,7 +614,6 @@ const ProjectDetails = ({ width, height }) => {
           pipelineStatus.ABORTED,
           pipelineStatus.TIMED_OUT,
           pipelineStatus.FAILED,
-          pipelineStatus.SUCCEEDED,
         ].includes(backendStatus.gem2s.status)) {
           dispatch(runGem2s(experimentId));
         }
