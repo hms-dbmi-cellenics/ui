909e9e88653d356cd5043b496dfc3031b873fbc8
Xavier Verges <460418+xverges@users.noreply.github.com>
2021-06-03 08:16:08 +0200

    Fix

--- src/utils/fetchErrors.js
+++ src/utils/fetchErrors.js
@@ -1,14 +1,20 @@
-class ServerError extends Error {
+// Babel trsnapiling does not work properly extending native types
+// class ServerError extends Error
+function ServerError(message) {
+  this.message = message;
+  this.stack = Error().stack;
 }
+ServerError.prototype = Object.create(Error.prototype);
+ServerError.prototype.name = 'ServerError';
 
 const throwIfRequestFailed = (response, json, friendlyMessage) => {
   if (!response.ok) {
-    let { message } = json.message;
+    let { message } = json;
     if (response.status === 500) {
       console.error(`Status 500 fetching ${response.url}. Error in response: ${message}`);
       message = friendlyMessage;
     }
-    throw new ServerError(message);
+    throw (new ServerError(message));
   }
 };
 
