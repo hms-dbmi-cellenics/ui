394d9d0a4cd60abc779eb1ebda05636ea2cbcdac
stefanbabukov <stefanbabukov98@gmail.com>
2021-06-07 17:41:12 +0300

    fix tests

--- src/__test__/redux/reducers/samplesReducer.test.js
+++ src/__test__/redux/reducers/samplesReducer.test.js
@@ -115,7 +115,7 @@ describe('samplesReducer', () => {
       },
     });
 
-    expect(newState[sample1.uuid].fileNames).toEqual(new Set([fileName]));
+    expect(newState[sample1.uuid].fileNames).toEqual([fileName]);
     expect(newState[sample1.uuid].files[fileName]).toEqual(mockFile);
     expect(newState).toMatchSnapshot();
   });
@@ -144,7 +144,7 @@ describe('samplesReducer', () => {
       },
     });
 
-    expect(newState[sample1.uuid].fileNames).toEqual(new Set([fileName]));
+    expect(newState[sample1.uuid].fileNames).toEqual([fileName]);
     expect(newState[sample1.uuid].files[fileName]).toEqual(mockFile);
     expect(newState).toMatchSnapshot();
   });
