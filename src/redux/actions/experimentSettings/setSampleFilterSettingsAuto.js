6cd122996816e08574de6c808fcda5a54f1ca0fc
stefanbabukov <stefanbabukov98@gmail.com>
2021-06-25 14:31:50 +0300

    fix

--- src/redux/actions/experimentSettings/setSampleFilterSettingsAuto.js
+++ src/redux/actions/experimentSettings/setSampleFilterSettingsAuto.js
@@ -1,9 +1,9 @@
 import { EXPERIMENT_SETTINGS_SET_SAMPLE_FILTER_SETTINGS_AUTO } from '../../actionTypes/experimentSettings';
 
-const setSampleFilterSettingsAuto = (step, sampleId, enabled) => (dispatch) => {
+const setSampleFilterSettingsAuto = (step, sampleId, isAuto) => (dispatch) => {
   dispatch({
     type: EXPERIMENT_SETTINGS_SET_SAMPLE_FILTER_SETTINGS_AUTO,
-    payload: { step, sampleId, enabled },
+    payload: { step, sampleId, isAuto },
   });
 };
 
