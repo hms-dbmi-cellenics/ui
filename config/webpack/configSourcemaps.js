8624c5d64908d6b0ae4cfa738930b1f02c675389
Marcell Pek <marcell.pek@marcellp.net>
2021-06-30 15:36:03 +0100

    fix

--- config/webpack/configSourcemaps.js
+++ config/webpack/configSourcemaps.js
@@ -14,6 +14,28 @@ const webpackConfigSourcemaps = (config, { dev, isServer }) => {
     };
   }
 
+  // to be refactored
+  if (isServer) {
+    // deal antd style
+    const antStyles = /antd\/.*?\/style.*?/;
+    const origExternals = [...config.externals];
+    config.externals = [
+      (context, request, callback) => {
+        if (request.match(antStyles)) return callback();
+        if (typeof origExternals[0] === 'function') {
+          origExternals[0](context, request, callback);
+        } else {
+          callback();
+        }
+      },
+      ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+    ];
+    config.module.rules.unshift({
+      test: antStyles,
+      use: 'null-loader',
+    });
+  }
+
   return config;
 };
 
