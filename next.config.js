5d4a1cb32d3c3b46eb116964d415bf72fdb0d0b5
Marcell Pék <marcell@biomage.net>
2021-07-06 13:00:43 +0100

    [BIOMAGE-1159] Add sourcemaps to production build for easier debugging (#373)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * add sourcemap to nextjs config instead of webpack
    
    * fix
    
    * fix
    
    * fix
    
    * add-redux-debug
    
    * Enable codesigning
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 /* eslint-disable no-param-reassign */
-const withLess = require('@zeit/next-less');
-const withCSS = require('@zeit/next-css');
-const withImages = require('next-images');
-const webpack = require('webpack');
-const lessToJS = require('less-vars-to-js');
-
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
-const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
-
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
+
+const withPlugins = require('next-compose-plugins');
+const less = require('@zeit/next-less');
+const css = require('@zeit/next-css');
+const images = require('next-images');
+
+const lessToJS = require('less-vars-to-js');
+const webpackConfigPlugins = require('./config/webpack/configPlugins');
+const webpackConfigRules = require('./config/webpack/configRules');
+const webpackConfigSourcemaps = require('./config/webpack/configSourcemaps');
+
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 // Where your antd-custom.less file lives
 const themeVariables = lessToJS(
@@ -20,13 +22,6 @@ const themeVariables = lessToJS(
     'utf8',
   ),
 );
-
-// development or other environment
-const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-require('dotenv').config({ path: ((process.env.NODE_ENV === 'development') ? '.env.development' : '.env.production') });
-
-// analyse use webpack-bundle-analyzer
-const isAnalyse = process.env.NODE_ENV === 'analyse';
 
 // fix antd bug in dev development
 const devAntd = '@import "~antd/dist/antd.less";\n';
@@ -45,198 +40,59 @@ if (typeof require !== 'undefined') {
   require.extensions['.less'] = () => { };
 }
 
-const srcFolder = [
-  path.resolve(__dirname, './src/components'),
-  path.resolve(__dirname, './src/utils'),
-  path.resolve(__dirname, './src/pages'),
-  path.resolve(__dirname, './src/redux'),
-];
-
-const generalWebpackConfig = [
-  {
-    test: /\.(ttf|eot|svg)$/,
-    use: {
-      loader: 'file-loader',
-      options: {
-        name: 'fonts/[hash].[ext]',
+const nextConfig = {
+  // Redirects enforced by Next.
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/data-management',
+        permanent: false,
       },
-    },
-  },
-  {
-    test: /\.(woff|woff2)$/,
-    use: {
-      loader: 'url-loader',
-      options: {
-        name: 'fonts/[hash].[ext]',
-        limit: 5000,
-        mimetype: 'application/font-woff',
+      {
+        source: '/experiments',
+        destination: '/data-management',
+        permanent: false,
       },
-    },
+      {
+        source: '/experiments/:experimentId',
+        destination: '/experiments/:experimentId/data-exploration',
+        permanent: false,
+      },
+    ];
   },
-];
+  experimental: {
+    productionBrowserSourceMaps: true,
+  },
+  webpack: (config, params) => {
+    const { dev } = params;
 
-// Bundle analyzer
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+    const final = webpackConfigSourcemaps(
+      webpackConfigRules(
+        webpackConfigPlugins(
+          config,
+          params,
+        ), params,
+      ), params,
+    );
 
-module.exports = withBundleAnalyzer(
-  withCSS(
-    withLess(
-      withImages({
-        lessLoaderOptions: {
-          javascriptEnabled: true,
-          modifyVars: themeVariables,
-          localIdentName: '[local]___[hash:base64:5]',
-        },
-        webpack: (config, {
-          dev, isServer,
-        }) => {
-          config.plugins.push(new webpack.IgnorePlugin(/pages.*\/test.*/));
+    if (!dev) {
+      console.log('WebPack build configuration:');
+      console.log(util.inspect(config, false, null, true /* enable colors */));
+    }
 
-          if (isServer) {
-            // deal antd style
-            const antStyles = /antd\/.*?\/style.*?/;
-            const origExternals = [...config.externals];
-            config.externals = [
-              (context, request, callback) => {
-                if (request.match(antStyles)) return callback();
-                if (typeof origExternals[0] === 'function') {
-                  origExternals[0](context, request, callback);
-                } else {
-                  callback();
-                }
-              },
-              ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-            ];
-            config.module.rules.unshift({
-              test: antStyles,
-              use: 'null-loader',
-            });
-          }
+    return final;
+  },
+};
 
-          if (!isServer) {
-            config.node = {
-              fs: 'empty',
-            };
-          }
-
-          // analyse use webpack-bundle-analyser
-          if (isAnalyse) {
-            BundleAnalyzerPlugin({
-              analyzerMode: 'disabled',
-              generateStatsFile: true,
-              statsFilename: 'stats.json',
-            });
-          }
-
-          if (!dev) {
-            // polyfill IE11
-            const originalEntry = config.entry;
-            config.entry = async () => {
-              const entries = await originalEntry();
-              if (
-                entries['main.js']
-                && !entries['main.js'].includes('./assets/polyfills.js')
-              ) {
-                entries['main.js'].unshift('./assets/polyfills.js');
-              }
-              return entries;
-            };
-
-            // add other webpack plugins
-            config.plugins.push(
-              ...[
-                // replace uglyJsPlugin
-                new TerserPlugin({
-                  cache: true,
-                  terserOptions: {
-                    ecma: 6,
-                    warnings: false,
-                    extractComments: false, // remove comment
-                    output: {
-                      comments: false,
-                    },
-                    compress: {
-                      drop_console: false, // remove console
-                    },
-                    ie8: false,
-                  },
-                }),
-                new OptimizeCssPlugin({
-                  // eslint-disable-next-line global-require
-                  cssProcessor: require('cssnano'),
-                  cssProcessorOptions: {
-                    discardComments: { removeAll: true },
-                  },
-                  canPrint: true, // print info to console
-                }),
-                new MomentLocalesPlugin(),
-              ],
-            );
-            config.module.rules.push({
-              test: /\.js$/,
-              include: srcFolder,
-              options: {
-                workerParallelJobs: 50,
-                // additional node.js arguments
-                workerNodeArgs: ['--max-old-space-size=1024'],
-              },
-              loader: 'thread-loader',
-            });
-            generalWebpackConfig.forEach((c) => config.module.rules.push(c));
-            config.devtool = 'source-map';
-          } else {
-            config.module.rules.push({
-              test: /\.js$/,
-              enforce: 'pre',
-              include: srcFolder,
-              options: {
-                emitWarning: true,
-                configFile: path.resolve('.eslintrc'),
-                eslint: {
-                  configFile: path.resolve(__dirname, '.eslintrc'),
-                },
-              },
-              loader: 'eslint-loader',
-            });
-            generalWebpackConfig.forEach((c) => config.module.rules.push(c));
-            config.devtool = 'cheap-module-inline-source-map';
-          }
-          return config;
-        },
-        webpackDevMiddleware: (config) => config,
-        serverRuntimeConfig: { // Will only be available on the server side
-          rootDir: path.join(__dirname, './'),
-          PORT: isDev ? 5000 : (process.env.PORT || 5000),
-        },
-        publicRuntimeConfig: { // Will be available on both server and client
-          staticFolder: '/static',
-          isDev, // Pass through env variables
-        },
-        env: {
-          // You can add env variables that will be passed on to the client here.
-        },
-        async redirects() {
-          return [
-            {
-              source: '/',
-              destination: '/data-management',
-              permanent: false,
-            },
-            {
-              source: '/experiments',
-              destination: '/data-management',
-              permanent: false,
-            },
-            {
-              source: '/experiments/:experimentId',
-              destination: '/experiments/:experimentId/data-exploration',
-              permanent: false,
-            },
-          ];
-        },
-      }),
-    ),
-  ),
-);
+module.exports = withPlugins([
+  [images],
+  [less, {
+    lessLoaderOptions: {
+      javascriptEnabled: true,
+      modifyVars: themeVariables,
+      localIdentName: '[local]___[hash:base64:5]',
+    },
+  }],
+  [css],
+], nextConfig);
