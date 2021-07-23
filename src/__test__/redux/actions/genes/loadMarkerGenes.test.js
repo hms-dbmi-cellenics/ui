45b951a7249468d2df1d9056e4f96bd4b8573359
Martin Fosco <martinfosco@Martins-MacBook-Pro.local>
2021-07-23 13:41:01 -0300

    Add loadMarkerGenes action creator tests

---
+++ src/__test__/redux/actions/genes/loadMarkerGenes.test.js
@@ -0,0 +1,119 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import loadMarkerGenes from '../../../../redux/actions/genes/loadMarkerGenes';
+import { MARKER_GENES_ERROR, MARKER_GENES_LOADED, MARKER_GENES_LOADING } from '../../../../redux/actionTypes/genes';
+import initialState from '../../../../redux/reducers/genes/initialState';
+import { fetchCachedWork } from '../../../../utils/cacheRequest';
+
+jest.mock('localforage');
+jest.mock('../../../../utils/cacheRequest');
+
+const mockStore = configureStore([thunk]);
+
+describe('loadMarkerGenes action', () => {
+  const experimentId = '1234';
+
+  const experimentSettings = {
+    processing: {
+      configureEmbedding: {
+        clusteringSettings: {
+          method: 'methodId',
+        },
+      },
+    },
+    backendStatus: {
+      status: {
+        pipeline: {
+          status: 'SUCCEEDED',
+          startDate: '2021-01-01T01:01:01.000Z',
+        },
+      },
+    },
+  };
+
+  it('throws when parameters are undefined or null', async () => {
+    const store = mockStore({});
+
+    const errorMessage = 'Null or undefined parameter/s for loadMarkerGenes';
+
+    try {
+      await store.dispatch(loadMarkerGenes(null, 1));
+
+      // eslint-disable-next-line no-undef
+      fail('it should not reach here');
+    } catch (e) {
+      expect(e.message).toEqual(errorMessage);
+    }
+
+    try {
+      await store.dispatch(loadMarkerGenes(1, undefined));
+      // eslint-disable-next-line no-undef
+      fail('it should not reach here');
+    } catch (e) {
+      expect(e.message).toEqual(errorMessage);
+    }
+
+    try {
+      await store.dispatch(loadMarkerGenes(1));
+      // eslint-disable-next-line no-undef
+      fail('it should not reach here');
+    } catch (e) {
+      expect(e.message).toEqual(errorMessage);
+    }
+  });
+
+  it('dispatches appropriately on success', async () => {
+    const store = mockStore({
+      genes: {
+        ...initialState,
+      },
+      experimentSettings,
+    });
+
+    const order = ['geneA'];
+    const data = {
+      geneA: {
+        expression: [1],
+        mean: 1,
+        stdev: 1,
+      },
+    };
+
+    const mockResult = { order, data };
+
+    fetchCachedWork.mockImplementationOnce(() => new Promise((resolve) => resolve(mockResult)));
+
+    await store.dispatch(loadMarkerGenes(experimentId, 10));
+
+    const loadingAction = store.getActions()[0];
+    expect(loadingAction.type).toEqual(MARKER_GENES_LOADING);
+    expect(loadingAction).toMatchSnapshot();
+
+    const loadedAction = store.getActions()[1];
+    expect(loadedAction.type).toEqual(MARKER_GENES_LOADED);
+    expect(loadedAction.payload.genes).toEqual(order);
+    expect(loadedAction.payload.data).toEqual(data);
+    expect(loadedAction).toMatchSnapshot();
+  });
+
+  it('dispatches appropriately on error', async () => {
+    const store = mockStore({
+      genes: {
+        ...initialState,
+      },
+      experimentSettings,
+    });
+
+    fetchCachedWork.mockImplementationOnce(() => new Promise((resolve, reject) => reject(new Error('random error!'))));
+
+    await store.dispatch(loadMarkerGenes(experimentId, 10));
+
+    const loadingAction = store.getActions()[0];
+    expect(loadingAction.type).toEqual(MARKER_GENES_LOADING);
+    expect(loadingAction).toMatchSnapshot();
+
+    const loadedAction = store.getActions()[1];
+    expect(loadedAction.type).toEqual(MARKER_GENES_ERROR);
+    expect(loadedAction).toMatchSnapshot();
+  });
+});
