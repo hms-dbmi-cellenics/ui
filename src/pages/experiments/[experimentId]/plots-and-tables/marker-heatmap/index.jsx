11aa145867ed8bb02caf5c5201fe7a6f17d21122
stefanbabukov <stefanbabukov98@gmail.com>
2021-08-03 16:13:30 +0300

    commit progress

--- src/pages/experiments/[experimentId]/plots-and-tables/marker-heatmap/index.jsx
+++ src/pages/experiments/[experimentId]/plots-and-tables/marker-heatmap/index.jsx
@@ -1,9 +1,28 @@
+/* eslint-disable import/no-unresolved */
+
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
+import populateHeatmapData from 'components/plots/helpers/populateHeatmapData';
+import { updatePlotConfig, loadPlotConfig } from 'redux/actions/componentConfig';
+
+const plotUuid = 'markerHeatmapPlotMain';
+const plotType = 'marker-heatmap';
+
+const route = {
+  path: 'marker-heatmap',
+  breadcrumbName: 'Marker-Heatmap',
+};
 
 const MarkerHeatmapPlot = ({ experimentId }) => {
   const dispatch = useDispatch();
+  const config = useSelector((state) => state.componentConfig[plotUuid]?.config);
+  const { expression: expressionData } = useSelector((state) => state.genes);
+  const { error, loading } = expressionData;
+
+  useEffect(() => {
+    dispatch(loadPlotConfig(experimentId, plotUuid, plotType));
+  }, []);
 };
 
 MarkerHeatmapPlot.propTypes = {
