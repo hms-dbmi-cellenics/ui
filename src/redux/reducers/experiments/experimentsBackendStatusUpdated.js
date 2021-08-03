d1d2eaca5ca406a15073c6d5e81bd580db92a369
Anugerah Erlaut <aerlaut@live.com>
2021-08-03 20:11:53 +0700

    fix access errors when document does not have status yet

--- src/redux/reducers/experiments/experimentsBackendStatusUpdated.js
+++ src/redux/reducers/experiments/experimentsBackendStatusUpdated.js
@@ -4,15 +4,15 @@ import produce from 'immer';
 const experimentsBackendStatusUpdated = produce((draft, action) => {
   const { experimentId, backendStatus: { pipeline, gem2s } } = action.payload;
 
-  draft[experimentId].meta.pipeline.startDate = pipeline.startDate;
-  draft[experimentId].meta.pipeline.stopDate = pipeline.stopDate;
-  draft[experimentId].meta.pipeline.status = pipeline.status;
-  draft[experimentId].meta.pipeline.stepsCompleted = pipeline.stepsCompleted;
+  draft[experimentId].meta.pipeline = {
+    ...draft[experimentId].meta.pipeline || {},
+    ...pipeline,
+  };
 
-  draft[experimentId].meta.gem2s.startDate = gem2s.startDate;
-  draft[experimentId].meta.gem2s.stopDate = gem2s.stopDate;
-  draft[experimentId].meta.gem2s.status = gem2s.status;
-  draft[experimentId].meta.gem2s.stepsCompleted = gem2s.stepsCompleted;
+  draft[experimentId].meta.gem2s = {
+    ...draft[experimentId].meta.gem2s || {},
+    ...gem2s,
+  };
 });
 
 export default experimentsBackendStatusUpdated;
