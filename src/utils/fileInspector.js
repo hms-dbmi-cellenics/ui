4abe6357d351d9e7af6368dc0572f3eecee9c3d6
James Robinson <robioson@pm.me>
2021-08-16 15:30:20 +0100

    Renames

--- src/utils/fileInspector.js
+++ src/utils/fileInspector.js
@@ -4,13 +4,10 @@ import techOptions from './fileUploadSpecifications';
 import readFileToBuffer from './readFileToBuffer';
 
 const Verdict = {
-  INVALID_NAME: 0,
-  INVALID_FORMAT: 0,
-  VALID_ZIPPED: 1,
-  VALID_UNZIPPED: 2,
-  VALID_MATRIX: 3,
-  VALID_FEATURES: 4,
-  VALID_BARCODES: 5,
+  INVALID_NAME: -2,
+  INVALID_FORMAT: -1,
+  VALID_ZIPPED: 0,
+  VALID_UNZIPPED: 1,
 };
 
 const MATRIX_SIGNATURE = Buffer.from('%%MatrixMarket', 'ascii');
@@ -18,8 +15,9 @@ const FEATURES_SIGNATURE = Buffer.from('ENS', 'ascii');
 
 const GZIP_SIGNATURE = Buffer.from([0x1f, 0x8b]);
 
-// Validate a file requested for upload to the platform.
 const inspectFile = async (file, technology) => {
+  // Validate a file requested for upload to the platform.
+
   // immediately discard file if filename is not in valid set
   const validNames = techOptions[technology].acceptedFiles;
   if (!validNames.has(file.name)) {
@@ -29,15 +27,15 @@ const inspectFile = async (file, technology) => {
   // if name is valid, inspect first 16 bytes to validate format
   let data = await readFileToBuffer(file.slice(0, 16));
 
-  const fileSignature = data.slice(0, 2);
-  const isGzipped = !fileSignature.compare(GZIP_SIGNATURE);
+  const isGzipped = !data.slice(0, 2).compare(GZIP_SIGNATURE);
 
   if (isGzipped) {
+    // if gzipped, decompress a small chunk to further validate contents
     // eslint-disable-next-line no-unused-vars
-    const deflateStream = new Gunzip((chunk, _) => {
+    const gunzip = new Gunzip((chunk, _) => {
       data = Buffer.from(chunk.slice(0, 16));
     });
-    deflateStream.push(await readFileToBuffer(file.slice(0, 128)));
+    gunzip.push(await readFileToBuffer(file.slice(0, 128)));
   }
 
   const valid = isGzipped ? Verdict.VALID_ZIPPED : Verdict.VALID_UNZIPPED;
