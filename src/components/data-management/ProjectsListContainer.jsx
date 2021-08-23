235c97573f7fbc579a992084b6333aed3f0ac21c
Martin Fosco <martinfosco@Martins-MacBook-Pro.local>
2021-08-23 11:43:42 -0300

    Minor update id shape

--- src/components/data-management/ProjectsListContainer.jsx
+++ src/components/data-management/ProjectsListContainer.jsx
@@ -78,7 +78,7 @@ const ProjectsListContainer = (props) => {
         {
           projects.ids.map((uuid) => (
             <Card
-              data-test-id={projects[uuid].name}
+              data-test-id={`project-card-${projects[uuid].name}`}
               key={uuid}
               type='primary'
               style={activeProjectUuid === uuid ? activeProjectStyle : { cursor: 'pointer' }}
