5661d5bb9b9f4098ec174d22666e82b859ef29e7
Anugerah Erlaut <aerlaut@live.com>
2021-08-23 18:21:30 +0700

    remove wdyr

--- src/utils/wdyr.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line global-require
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  whyDidYouRender(React, {
-    trackAllPureComponents: false,
-  });
-}
