a2f710e05cd392c66683bc18c507456fc08449b2
Anugerah Erlaut <aerlaut@live.com>
2021-08-31 15:13:59 +0700

    Launch analysis modal show loading state

--- src/components/data-management/AnalysisModal.jsx
+++ src/components/data-management/AnalysisModal.jsx
@@ -10,6 +10,7 @@ import {
   Col,
   List,
 } from 'antd';
+import { ClipLoader } from 'react-spinners';
 import EditableField from '../EditableField';
 import { updateExperiment, saveExperiment } from '../../redux/actions/experiments';
 import validateInputs, { rules } from '../../utils/validateInputs';
@@ -32,9 +33,10 @@ const NewExperimentModal = (props) => {
   const [isWorking, setIsWorking] = useState(false);
 
   useEffect(() => {
-    setExperimentsList(
-      activeProject?.experiments?.map((experimentId) => experiments[experimentId]) || [],
-    );
+    const updatedList = activeProject?.experiments?.map((experimentId) => experiments[experimentId])
+      .filter((experiment) => experiment !== undefined);
+
+    setExperimentsList(updatedList);
   }, [activeProject, experiments]);
   useEffect(() => {
     setIsWorking(!visible);
@@ -44,6 +46,82 @@ const NewExperimentModal = (props) => {
     rules.MIN_1_CHAR,
     rules.ALPHANUM_DASH_SPACE,
   ];
+
+  const renderAnalysisList = () => {
+    if (experimentsList.length === 0) {
+      return (
+        <Row justify='center'>
+          <ClipLoader size={30} color='#8f0b10' />
+        </Row>
+      );
+    }
+
+    return (
+      <List
+        size='small'
+        bordered
+        dataSource={experimentsList}
+        itemLayout='vertical'
+        loading={experimentsList?.length === 0}
+        renderItem={(experiment) => (
+          <List.Item
+            key={`${experiment.id}`}
+            extra={(
+              <Row type='flex' align='middle' data-test-class='launch-analysis-item'>
+                <Col>
+                  <Button
+                    type='primary'
+                    onClick={() => {
+                      setIsWorking(true);
+                      onLaunch(experiment.id);
+                    }}
+                    disabled={numFieldsEditing > 0 || isWorking}
+                    onKeyDown={(e) => {
+                      if (e.key === 'Enter') {
+                        setIsWorking(true);
+                        onLaunch(experiment.id);
+                      }
+                    }}
+                  >
+                    Launch
+                  </Button>
+                </Col>
+              </Row>
+            )}
+          >
+            <Space direction='vertical' size='small'>
+              <strong>
+                <EditableField
+                  onAfterSubmit={async (name) => {
+                    dispatch(updateExperiment(experiment.id, { name: name.trim() }));
+                  }}
+                  value={experiment.name}
+                  validationFunc={(name) => validateInputs(name, validationChecks).isValid}
+                  deleteEnabled={false}
+                  onEditing={(editing) => {
+                    setNumFieldsEditing(Math.max(0, numFieldsEditing + (editing || -1)));
+                  }}
+                />
+              </strong>
+              <EditableField
+                onAfterSubmit={async (description) => {
+                  dispatch(
+                    updateExperiment(experiment.id, { description: description.trim() }),
+                  );
+                  await dispatch(saveExperiment(experiment.id));
+                }}
+                value={experiment.description}
+                deleteEnabled={false}
+                onEditing={(editing) => {
+                  setNumFieldsEditing(Math.max(0, numFieldsEditing + (editing || -1)));
+                }}
+              />
+            </Space>
+          </List.Item>
+        )}
+      />
+    );
+  };
 
   return (
     <Modal
@@ -59,69 +137,7 @@ const NewExperimentModal = (props) => {
             <Title level={4}>
               Select Analysis
             </Title>
-            <List
-              size='small'
-              bordered
-              dataSource={experimentsList}
-              itemLayout='vertical'
-              loading={experimentsList?.length === 0}
-              renderItem={(experiment) => (
-                <List.Item
-                  key={`${experiment.id}`}
-                  extra={(
-                    <Row type='flex' align='middle' data-test-class='launch-analysis-item'>
-                      <Col>
-                        <Button
-                          type='primary'
-                          onClick={() => {
-                            setIsWorking(true);
-                            onLaunch(experiment.id);
-                          }}
-                          disabled={numFieldsEditing > 0 || isWorking}
-                          onKeyDown={(e) => {
-                            if (e.key === 'Enter') {
-                              setIsWorking(true);
-                              onLaunch(experiment.id);
-                            }
-                          }}
-                        >
-                          Launch
-                        </Button>
-                      </Col>
-                    </Row>
-                  )}
-                >
-                  <Space direction='vertical' size='small'>
-                    <strong>
-                      <EditableField
-                        onAfterSubmit={async (name) => {
-                          dispatch(updateExperiment(experiment.id, { name: name.trim() }));
-                        }}
-                        value={experiment.name}
-                        validationFunc={(name) => validateInputs(name, validationChecks).isValid}
-                        deleteEnabled={false}
-                        onEditing={(editing) => {
-                          setNumFieldsEditing(Math.max(0, numFieldsEditing + (editing || -1)));
-                        }}
-                      />
-                    </strong>
-                    <EditableField
-                      onAfterSubmit={async (description) => {
-                        dispatch(
-                          updateExperiment(experiment.id, { description: description.trim() }),
-                        );
-                        await dispatch(saveExperiment(experiment.id));
-                      }}
-                      value={experiment.description}
-                      deleteEnabled={false}
-                      onEditing={(editing) => {
-                        setNumFieldsEditing(Math.max(0, numFieldsEditing + (editing || -1)));
-                      }}
-                    />
-                  </Space>
-                </List.Item>
-              )}
-            />
+            {renderAnalysisList()}
           </Space>
         </Col>
       </Row>
