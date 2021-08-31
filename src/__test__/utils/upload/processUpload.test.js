f6564625a440db0038a6594bfe4180ede3aa3664
Martin Fosco <martinfosco@Martins-MacBook-Pro.local>
2021-08-31 16:29:03 -0300

    Fix tests

--- src/__test__/utils/upload/processUpload.test.js
+++ src/__test__/utils/upload/processUpload.test.js
@@ -1,314 +1,300 @@
-/* eslint-disable max-len */
-// import configureMockStore from 'redux-mock-store';
-// import thunk from 'redux-thunk';
-// import waitForActions from 'redux-mock-store-await-actions';
-// import uuid from 'uuid';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import waitForActions from 'redux-mock-store-await-actions';
+import uuid from 'uuid';
+import axios from 'axios';
+import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
+import { SAMPLES_FILE_UPDATE } from '../../../redux/actionTypes/samples';
+import initialSampleState, { sampleTemplate } from '../../../redux/reducers/samples/initialState';
+import initialProjectState, { projectTemplate } from '../../../redux/reducers/projects/initialState';
+import initialExperimentState, { experimentTemplate } from '../../../redux/reducers/experiments/initialState';
+import { processUpload } from '../../../utils/upload/processUpload';
+import UploadStatus from '../../../utils/upload/UploadStatus';
 
-// import { Storage } from 'aws-amplify';
-// import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
-// import { SAMPLES_FILE_UPDATE } from '../../../redux/actionTypes/samples';
+enableFetchMocks();
+const validFilesList = [
+  {
+    name: 'WT13/features.tsv.gz',
+    bundle: {
+      name: 'features.tsv.gz',
+      path: '/WT13/features.tsv.gz',
+      type: 'application/gzip',
+    },
+    upload: { status: UploadStatus.UPLOADING },
+    errors: '',
+    compressed: true,
+    valid: true,
+  },
+  {
+    name: 'WT13/barcodes.tsv.gz',
+    bundle: {
+      name: 'barcodes.tsv.gz',
+      path: '/WT13/barcodes.tsv.gz',
+      type: 'application/gzip',
+    },
+    upload: { status: UploadStatus.UPLOADING },
+    errors: '',
+    compressed: true,
+    valid: true,
+  },
+  {
+    name: 'WT13/matrix.mtx.gz',
+    bundle: {
+      name: 'matrix.mtx.gz',
+      path: '/WT13/matrix.mtx.gz',
+      type: 'application/gzip',
+    },
+    upload: { status: UploadStatus.UPLOADING },
+    errors: '',
+    compressed: true,
+    valid: true,
+  },
+];
+const sampleType = '10X Chromium';
+const mockSampleUuid = 'sample-uuid';
+const mockProjectUuid = 'project-uuid';
+const mockExperimentId = 'experiment-id';
+jest.mock('uuid', () => jest.fn());
+uuid.v4 = jest.fn(() => 'sample-uuid');
+const initialState = {
+  projects: {
+    ...initialProjectState,
+    ids: [mockProjectUuid],
+    meta: {
+      activeProjectUuid: mockProjectUuid,
+    },
+    [mockProjectUuid]: {
+      ...projectTemplate,
+      samples: [mockSampleUuid],
+      experiments: [mockExperimentId],
+    },
+    errorProjectUuid: {
+      ...projectTemplate,
+      samples: [mockSampleUuid],
+      experiments: [mockExperimentId],
+    },
+  },
+  experiments: {
+    ...initialExperimentState,
+    [mockExperimentId]: {
+      ...experimentTemplate,
+      id: mockExperimentId,
+    },
+  },
+  samples: {
+    ...initialSampleState,
+    ids: [mockSampleUuid],
+    meta: {
+      loading: true,
+      error: false,
+    },
+    [mockSampleUuid]: {
+      ...sampleTemplate,
+      uuid: [mockSampleUuid],
+      projectUuid: mockProjectUuid,
+    },
+  },
+};
+// Based on https://stackoverflow.com/a/51045733
+const flushPromises = () => new Promise(setImmediate);
+const mockStore = configureMockStore([thunk]);
 
-// import initialSampleState, { sampleTemplate } from '../../../redux/reducers/samples/initialState';
-// import initialProjectState, { projectTemplate } from '../../../redux/reducers/projects/initialState';
-// import initialExperimentState, { experimentTemplate } from '../../../redux/reducers/experiments/initialState';
+jest.mock('../../../utils/upload/loadAndCompressIfNecessary',
+  () => jest.fn().mockImplementation(
+    (bundle) => {
+      if (!bundle.valid) {
+        return Promise.reject(new Error('error'));
+      }
+      return Promise.resolve('loadedGzippedFile');
+    },
+  ));
 
-// import { processUpload } from '../../../utils/upload/processUpload';
-// import UploadStatus from '../../../utils/upload/UploadStatus';
+jest.mock('../../../utils/environment', () => ({
+  __esModule: true,
+  isBrowser: () => false,
+  ssrGetCurrentEnvironment: () => 'development',
+}));
 
-// enableFetchMocks();
+jest.mock('../../../redux/actions/samples/saveSamples', () => jest.fn().mockImplementation(() => ({
+  type: 'samples/saved',
+})));
 
-// const validFilesList = [
-//   {
-//     name: 'WT13/features.tsv.gz',
-//     bundle: {
-//       name: 'features.tsv.gz',
-//       path: '/WT13/features.tsv.gz',
-//       type: 'application/gzip',
-//     },
-//     upload: { status: UploadStatus.UPLOADING },
-//     errors: '',
-//     compressed: true,
-//     valid: true,
-//   },
-//   {
-//     name: 'WT13/barcodes.tsv.gz',
-//     bundle: {
-//       name: 'barcodes.tsv.gz',
-//       path: '/WT13/barcodes.tsv.gz',
-//       type: 'application/gzip',
-//     },
-//     upload: { status: UploadStatus.UPLOADING },
-//     errors: '',
-//     compressed: true,
-//     valid: true,
-//   },
-//   {
-//     name: 'WT13/matrix.mtx.gz',
-//     bundle: {
-//       name: 'matrix.mtx.gz',
-//       path: '/WT13/matrix.mtx.gz',
-//       type: 'application/gzip',
-//     },
-//     upload: { status: UploadStatus.UPLOADING },
-//     errors: '',
-//     compressed: true,
-//     valid: true,
-//   },
-// ];
+describe('processUpload', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-// const sampleType = '10X Chromium';
+  beforeEach(() => {
+    fetchMock.resetMocks();
+    fetchMock.doMock();
+    fetchMock.mockResponse(JSON.stringify('theSignedUrl'), { status: 200 });
+  });
 
-// const mockSampleUuid = 'sample-uuid';
-// const mockProjectUuid = 'project-uuid';
-// const mockExperimentId = 'experiment-id';
+  it('Uploads and updates redux correctly when there are no errors', async () => {
+    const store = mockStore(initialState);
 
-// jest.mock('uuid', () => jest.fn());
-// uuid.v4 = jest.fn(() => 'sample-uuid');
+    const mockAxiosCalls = [];
+    const uploadSuccess = (params) => {
+      mockAxiosCalls.push(params);
+      return Promise.resolve('Resolved');
+    };
 
-// const initialState = {
-//   projects: {
-//     ...initialProjectState,
-//     ids: [mockProjectUuid],
-//     meta: {
-//       activeProjectUuid: mockProjectUuid,
-//     },
-//     [mockProjectUuid]: {
-//       ...projectTemplate,
-//       samples: [mockSampleUuid],
-//       experiments: [mockExperimentId],
-//     },
-//     errorProjectUuid: {
-//       ...projectTemplate,
-//       samples: [mockSampleUuid],
-//       experiments: [mockExperimentId],
-//     },
-//   },
-//   experiments: {
-//     ...initialExperimentState,
-//     [mockExperimentId]: {
-//       ...experimentTemplate,
-//       id: mockExperimentId,
-//     },
-//   },
-//   samples: {
-//     ...initialSampleState,
-//     ids: [mockSampleUuid],
-//     meta: {
-//       loading: true,
-//       error: false,
-//     },
-//     [mockSampleUuid]: {
-//       ...sampleTemplate,
-//       uuid: [mockSampleUuid],
-//       projectUuid: mockProjectUuid,
-//     },
-//   },
-// };
+    axios.request = jest.fn()
+      .mockImplementationOnce(uploadSuccess)
+      .mockImplementationOnce(uploadSuccess)
+      .mockImplementationOnce(uploadSuccess);
 
-// // Based on https://stackoverflow.com/a/51045733
-// const flushPromises = () => new Promise(setImmediate);
+    await processUpload(
+      validFilesList,
+      sampleType,
+      store.getState().samples,
+      mockProjectUuid,
+      store.dispatch,
+    );
 
-// const mockStore = configureMockStore([thunk]);
+    // Wait for uploads to be made
+    await waitForActions(
+      store,
+      new Array(3).fill({
+        type: SAMPLES_FILE_UPDATE,
+        payload: { fileDiff: { upload: { status: UploadStatus.UPLOADED } } },
+      }),
+      { matcher: waitForActions.matchers.containing },
+    );
 
-// jest.mock('../../../utils/upload/loadAndCompressIfNecessary',
-//   () => jest.fn().mockImplementation(
-//     (bundle) => {
-//       if (!bundle.valid) {
-//         return Promise.reject(new Error('error'));
-//       }
+    // Three axios put calls are made
+    expect(mockAxiosCalls.length).toBe(3);
+    // Each put call is made with the correct information
+    expect(mockAxiosCalls[0].data).toEqual('loadedGzippedFile');
+    expect(mockAxiosCalls[1].data).toEqual('loadedGzippedFile');
+    expect(mockAxiosCalls[2].data).toEqual('loadedGzippedFile');
 
-//       return Promise.resolve('loadedGzippedFile');
-//     },
-//   ));
+    // Wait until all put promises are resolved
+    await flushPromises();
+    const fileUpdateActions = store.getActions().filter(
+      (action) => action.type === SAMPLES_FILE_UPDATE,
+    );
+    const uploadProperties = fileUpdateActions.map((action) => action.payload.fileDiff.upload);
+    const uploadingStatusProperties = uploadProperties.filter(
+      ({ status }) => status === UploadStatus.UPLOADING,
+    );
+    const uploadedStatusProperties = uploadProperties.filter(
+      ({ status }) => status === UploadStatus.UPLOADED,
+    );
+    // There are 6 files actions with status uploading
+    expect(uploadingStatusProperties.length).toEqual(6);
+    // There are 3 files actions with status uploaded
+    expect(uploadedStatusProperties.length).toEqual(3);
+    // After uploading ends successfully the upload promises are removed
+    uploadedStatusProperties.forEach(({ amplifyPromise }) => {
+      expect(amplifyPromise).toBeNull();
+    });
+  });
 
-// jest.mock('../../../utils/environment', () => ({
-//   __esModule: true,
-//   isBrowser: () => false,
-//   ssrGetCurrentEnvironment: () => 'development',
-// }));
+  it('Updates redux correctly when there are file load and compress errors', async () => {
+    const store = mockStore(initialState);
+    const invalidFiles = validFilesList.map((file) => ({ ...file, valid: false }));
 
-// jest.mock('../../../redux/actions/samples/saveSamples', () => jest.fn().mockImplementation(() => ({
-//   type: 'samples/saved',
-// })));
+    processUpload(
+      invalidFiles,
+      sampleType,
+      store.getState().samples,
+      mockProjectUuid,
+      store.dispatch,
+    );
 
-// let mockStorageCalls = [];
+    // Wait for uploads to be made
+    await waitForActions(
+      store,
+      new Array(3).fill({
+        type: SAMPLES_FILE_UPDATE,
+        payload: { fileDiff: { upload: { status: UploadStatus.FILE_READ_ERROR } } },
+      }),
+      { matcher: waitForActions.matchers.containing },
+    );
 
-// Storage.put = jest.fn().mockImplementation(
-//   (bucketKey, file) => {
-//     mockStorageCalls.push({ bucketKey, file });
-//     if (bucketKey.includes('errorProjectUuid')) {
-//       return Promise.reject(new Error('error'));
-//     }
+    const fileUpdateActions = store.getActions().filter(
+      (action) => action.type === SAMPLES_FILE_UPDATE,
+    );
 
-//     return Promise.resolve('Resolved');
-//   },
-// );
+    const uploadProperties = fileUpdateActions.map((action) => action.payload.fileDiff.upload);
 
-// describe('processUpload (in development)', () => {
-//   afterEach(() => {
-//     mockStorageCalls = [];
-//     jest.clearAllMocks();
-//   });
+    const uploadingFileProperties = uploadProperties.filter(
+      ({ status }) => status === UploadStatus.UPLOADING,
+    );
+    const errorFileProperties = uploadProperties.filter(
+      ({ status }) => status === UploadStatus.FILE_READ_ERROR,
+    );
+    const uploadedFileProperties = uploadProperties.filter(
+      ({ status }) => status === UploadStatus.UPLOADED,
+    );
+    // There are 3 files actions with status uploading
+    expect(uploadingFileProperties.length).toEqual(3);
+    // There are 3 files actions with status upload error
+    expect(errorFileProperties.length).toEqual(3);
+    // There are no file actions with status successfully uploaded
+    expect(uploadedFileProperties.length).toEqual(0);
+  });
 
-//   beforeEach(() => {
-//     fetchMock.resetMocks();
-//     fetchMock.doMock();
-//     fetchMock.mockResponse(JSON.stringify({}));
-//   });
+  it('Updates redux correctly when there are file upload errors', async () => {
+    const store = mockStore(initialState);
 
-//   it('Uploads and updates redux correctly when there are no errors', async () => {
-//     const store = mockStore(initialState);
+    const mockAxiosCalls = [];
+    const uploadError = (params) => {
+      mockAxiosCalls.push(params);
+      return Promise.reject(new Error('Error'));
+    };
 
-//     processUpload(
-//       validFilesList,
-//       sampleType,
-//       store.getState().samples,
-//       mockProjectUuid,
-//       store.dispatch,
-//     );
+    axios.request = jest.fn()
+      .mockImplementationOnce(uploadError)
+      .mockImplementationOnce(uploadError)
+      .mockImplementationOnce(uploadError);
 
-//     // Wait for first storage puts to be made
-//     await waitForActions(
-//       store,
-//       new Array(6).fill(SAMPLES_FILE_UPDATE),
-//       { matcher: waitForActions.matchers.containing },
-//     );
+    processUpload(
+      validFilesList,
+      sampleType,
+      store.getState().samples,
+      'errorProjectUuid',
+      store.dispatch,
+    );
 
-//     // Three Storage.put calls are made
-//     expect(mockStorageCalls.length).toBe(3);
+    // Wait for uploads to be made
+    await waitForActions(
+      store,
+      new Array(3).fill({
+        type: SAMPLES_FILE_UPDATE,
+        payload: { fileDiff: { upload: { status: UploadStatus.UPLOAD_ERROR } } },
+      }),
+      { matcher: waitForActions.matchers.containing },
+    );
 
-//     // Each put call is made with the correct information
-//     expect(mockStorageCalls[0].file).toEqual('loadedGzippedFile');
-//     expect(mockStorageCalls[1].file).toEqual('loadedGzippedFile');
-//     expect(mockStorageCalls[2].file).toEqual('loadedGzippedFile');
+    const fileUpdateActions = store.getActions().filter(
+      (action) => action.type === SAMPLES_FILE_UPDATE,
+    );
 
-//     // Wait until all put promises are resolved
-//     await flushPromises();
+    const uploadProperties = fileUpdateActions.map((action) => action.payload.fileDiff.upload);
 
-//     const fileUpdateActions = store.getActions().filter(
-//       (action) => action.type === SAMPLES_FILE_UPDATE,
-//     );
+    const uploadingFileProperties = uploadProperties.filter(
+      ({ status }) => status === UploadStatus.UPLOADING,
+    );
 
-//     const uploadProperties = fileUpdateActions.map((action) => action.payload.fileDiff.upload);
+    const errorFileProperties = uploadProperties.filter(
+      ({ status }) => status === UploadStatus.UPLOAD_ERROR,
+    );
 
-//     const uploadingStatusProperties = uploadProperties.filter(
-//       ({ status }) => status === UploadStatus.UPLOADING,
-//     );
+    const uploadedFileProperties = uploadProperties.filter(
+      ({ status }) => status === UploadStatus.UPLOADED,
+    );
 
-//     const uploadedStatusProperties = uploadProperties.filter(
-//       ({ status }) => status === UploadStatus.UPLOADED,
-//     );
-
-//     // There are 6 files actions with status uploading
-//     expect(uploadingStatusProperties.length).toEqual(6);
-
-//     // There are 3 files actions with status uploaded
-//     expect(uploadedStatusProperties.length).toEqual(3);
-
-//     // After uploading ends successfully the upload promises are removed
-//     uploadedStatusProperties.forEach(({ amplifyPromise }) => {
-//       expect(amplifyPromise).toBeNull();
-//     });
-//   });
-
-//   it('Updates redux correctly when there are file load and compress errors', async () => {
-//     const store = mockStore(initialState);
-
-//     const invalidFiles = validFilesList.map((file) => ({ ...file, valid: false }));
-
-//     processUpload(
-//       invalidFiles,
-//       sampleType,
-//       store.getState().samples,
-//       mockProjectUuid,
-//       store.dispatch,
-//     );
-
-//     await waitForActions(
-//       store,
-//       new Array(6).fill(SAMPLES_FILE_UPDATE),
-//       { matcher: waitForActions.matchers.containing },
-//     );
-
-//     const fileUpdateActions = store.getActions().filter(
-//       (action) => action.type === SAMPLES_FILE_UPDATE,
-//     );
-
-//     const uploadProperties = fileUpdateActions.map((action) => action.payload.fileDiff.upload);
-
-//     const uploadingFileProperties = uploadProperties.filter(
-//       ({ status }) => status === UploadStatus.UPLOADING,
-//     );
-
-//     const errorFileProperties = uploadProperties.filter(
-//       ({ status }) => status === UploadStatus.FILE_READ_ERROR,
-//     );
-
-//     const uploadedFileProperties = uploadProperties.filter(
-//       ({ status }) => status === UploadStatus.UPLOADED,
-//     );
-
-//     // There are 3 files actions with status uploading
-//     expect(uploadingFileProperties.length).toEqual(3);
-
-//     // There are 3 files actions with status upload error
-//     expect(errorFileProperties.length).toEqual(3);
-
-//     // There are no file actions with status successfully uploaded
-//     expect(uploadedFileProperties.length).toEqual(0);
-//   });
-
-//   it('Updates redux correctly when there are file upload errors', async () => {
-//     const store = mockStore(initialState);
-
-//     processUpload(
-//       validFilesList,
-//       sampleType,
-//       store.getState().samples,
-//       'errorProjectUuid',
-//       store.dispatch,
-//     );
-
-//     await waitForActions(
-//       store,
-//       new Array(9).fill(SAMPLES_FILE_UPDATE),
-//       { matcher: waitForActions.matchers.containing, throttleWait: 20 },
-//     );
-
-//     const fileUpdateActions = store.getActions().filter(
-//       (action) => action.type === SAMPLES_FILE_UPDATE,
-//     );
-
-//     const uploadProperties = fileUpdateActions.map((action) => action.payload.fileDiff.upload);
-
-//     const uploadingFileProperties = uploadProperties.filter(
-//       ({ status }) => status === UploadStatus.UPLOADING,
-//     );
-
-//     const errorFileProperties = uploadProperties.filter(
-//       ({ status }) => status === UploadStatus.UPLOAD_ERROR,
-//     );
-
-//     const uploadedFileProperties = uploadProperties.filter(
-//       ({ status }) => status === UploadStatus.UPLOADED,
-//     );
-
-//     // There are 3 files actions with status uploading
-//     expect(uploadingFileProperties.length).toEqual(6);
-
-//     // There are 3 files actions with status upload error
-//     expect(errorFileProperties.length).toEqual(3);
-
-//     // There are no file actions with status successfully uploaded
-//     expect(uploadedFileProperties.length).toEqual(0);
-
-//     // Upload end deletes aws promise (if there was one)
-//     errorFileProperties.forEach(({ amplifyPromise }) => {
-//       expect(amplifyPromise).toBeNull();
-//     });
-//   });
-// });
-
-describe('sdf', () => {
-  it('21233', () => { });
+    // There are 3 files actions with status uploading
+    expect(uploadingFileProperties.length).toEqual(6);
+    // There are 3 files actions with status upload error
+    expect(errorFileProperties.length).toEqual(3);
+    // There are no file actions with status successfully uploaded
+    expect(uploadedFileProperties.length).toEqual(0);
+    // Upload end deletes aws promise (if there was one)
+    errorFileProperties.forEach(({ amplifyPromise }) => {
+      expect(amplifyPromise).toBeNull();
+    });
+  });
 });
