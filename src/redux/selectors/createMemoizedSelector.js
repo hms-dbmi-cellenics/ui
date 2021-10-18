0454e12f5f569d8fdce1968c866753710ff1d5ad
Martin Fosco <martinfosco@Martins-MacBook-Pro.local>
2021-10-18 16:12:30 -0300

    Minor refactoring and add explanatory comments

--- src/redux/selectors/createMemoizedSelector.js
+++ src/redux/selectors/createMemoizedSelector.js
@@ -4,15 +4,36 @@ import memoize from 'lru-memoize';
 import _ from 'lodash';
 
 // based on https://www.aarongreenwald.com/blog/redux-reselect-parameters
+/**
+ *
+ * @param {*} selector The selector to memoize
+ * @param {*} options an object with different configuration options:
+ *        - inputSelectors: The selectors that prepare the input the selector will take,
+ *          look at reselect's input selectors for more information:
+ *          https://redux.js.org/usage/deriving-data-selectors#writing-memoized-selectors-with-reselect
+ *          (identity by default)
+ *        - maxCachedKeys: The amount of concurrent memoized selectors of this type
+ *          with different parameters supported (4 by default)
+ *        - comparisonOperator: The comparison operator to determine selector's keys,
+ *          (_.isEqual by default)
+ * @returns a memoized by params selector that also uses reselect
+ */
 const createMemoizedSelector = (
-  selector, inputSelectors = [(state) => state], maxCachedKeys = 4,
+  selector,
+  options = {},
 ) => {
+  const {
+    maxCachedKeys = 4,
+    inputSelectors = [(state) => state],
+    comparisonOperator = _.isEqual,
+  } = options;
+
   const makerFunction = (...params) => createSelector(
     inputSelectors,
     selector(...params),
   );
 
-  return memoize(maxCachedKeys, _.isEqual)(makerFunction);
+  return memoize(maxCachedKeys, comparisonOperator)(makerFunction);
 };
 
 export default createMemoizedSelector;
