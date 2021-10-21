12365f97d7366a40b6777dccd33ab15b72d65f1f
Martin Fosco <martinfosco@Martins-MacBook-Pro.local>
2021-10-21 15:49:43 -0300

    Minor change

--- src/redux/actions/samples/loadSamples.js
+++ src/redux/actions/samples/loadSamples.js
@@ -26,7 +26,7 @@ const loadSamples = (
     // Querying using projectUuid returns an array with oh objects with of `samples` key
     // Data[0] because 1 project contains only 1 experiment right now.
     // This has to be changed when we support multiple experiments per project.
-    if (projectUuid) samples = data[0].samples;
+    if (projectUuid) [{ samples }] = data;
 
     throwIfRequestFailed(response, data, endUserMessages.ERROR_FETCHING_SAMPLES);
 
