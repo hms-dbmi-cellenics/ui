6f40dca0da228f6f238a60cc438bfd3c0122032b
Martin Fosco <martinfosco@Martins-MacBook-Pro.local>
2021-11-09 11:32:54 -0300

    Add unpackResult tests

---
+++ src/__test__/utils/work/unpackResult.test.js
@@ -0,0 +1,58 @@
+import { decompress } from 'fflate';
+// eslint-disable-next-line camelcase
+import { JSON_parse } from 'uint8array-json-parser';
+
+import unpackResult from 'utils/work/unpackResult';
+
+jest.mock('fflate', () => ({
+  __esModule: true, // this property makes it work
+  decompress: jest.fn(),
+}));
+
+jest.mock('uint8array-json-parser', () => ({
+  __esModule: true, // this property makes it work
+  JSON_parse: jest.fn(),
+}));
+
+describe('unpackResult', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('works correctly', async () => {
+    const storageArrayBuffer = new ArrayBuffer();
+
+    const storageResp = { arrayBuffer: () => Promise.resolve(storageArrayBuffer) };
+
+    const decompressedUint8 = new Uint8Array();
+    const decompressedObject = { hi: 'bye' };
+
+    decompress.mockImplementation((aUint8array, callback) => {
+      callback(null, decompressedUint8);
+    });
+
+    JSON_parse.mockImplementation(() => decompressedObject);
+
+    const result = await unpackResult(storageResp);
+
+    expect(decompress).toHaveBeenCalled();
+    expect(JSON_parse).toHaveBeenCalledWith(decompressedUint8);
+
+    expect(result).toEqual(decompressedObject);
+  });
+
+  it('rejects if decompress fails', async () => {
+    const storageArrayBuffer = new ArrayBuffer();
+
+    const storageResp = { arrayBuffer: () => Promise.resolve(storageArrayBuffer) };
+
+    decompress.mockImplementation((aUint8array, callback) => {
+      callback('someError', null);
+    });
+
+    // This is necessary to trigger a failure if unpackResult doesn't throw
+    expect.assertions(1);
+
+    unpackResult(storageResp).catch((e) => expect(e).toMatch('someError'));
+  });
+});
