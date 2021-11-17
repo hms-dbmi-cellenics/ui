1b4fe74a35ca2551e9f353701fac24da32113c4f
Anugerah Erlaut <aerlaut@live.com>
2021-11-17 11:21:21 +0000

    move disable cache to another PR

--- src/__test__/utils/work/fetchWork.test.js
+++ src/__test__/utils/work/fetchWork.test.js
@@ -73,23 +73,4 @@ describe('fetchWork', () => {
       expect.anything(), expect.anything(), expect.anything(), GENE_EXPRESSION_ETAG,
     );
   });
-
-  it('Generates random ETag if environment is not Production and cache is disabled', async () => {
-    Storage.prototype.getItem = jest.fn((key) => (key === 'disableCache' ? 'true' : null));
-
-    const mockRandom = jest.fn();
-    global.Math.random = mockRandom;
-
-    await fetchWork(
-      experimentId,
-      workRequest,
-      mockReduxState(experimentId),
-      { timeout: 10 },
-    );
-
-    expect(mockRandom).toHaveBeenCalledTimes(1);
-    expect(mockSeekFromAPI).not.toHaveBeenCalledWith(
-      expect.anything(), expect.anything(), expect.anything(), GENE_EXPRESSION_ETAG,
-    );
-  });
 });
