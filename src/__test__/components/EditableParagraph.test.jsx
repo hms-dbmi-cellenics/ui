cc123abdc9228b7e26e8f5196044d413cc5138d5
Anugerah Erlaut <aerlaut@live.com>
2021-12-06 12:01:33 +0000

    add tests to EditableParagraph

--- src/__test__/components/EditableParagraph.test.jsx
+++ src/__test__/components/EditableParagraph.test.jsx
@@ -50,4 +50,65 @@ describe('EdtableParagraph', () => {
     // Description is shortened by default
     expect(screen.queryByText(/more/i)).toBeInTheDocument();
   });
+
+  it('Clicking outside the paragraph should cause the paragraph to update', () => {
+    const { container } = renderEditableParagraph();
+
+    const mockContent = 'This is a mock content';
+
+    act(() => {
+      userEvent.click(screen.getByLabelText(/edit/i));
+    });
+
+    const descriptionInput = container.querySelector('p[contenteditable="true"]');
+
+    act(() => {
+      userEvent.type(descriptionInput, `${mockContent}`);
+    });
+
+    userEvent.click(document.body);
+
+    expect(defaultProps.onUpdate).toHaveBeenCalledWith(mockContent);
+    expect(screen.getByText(mockContent)).toBeInTheDocument();
+
+    // Description is shortened by default
+    expect(screen.queryByText(/more/i)).toBeInTheDocument();
+  });
+
+  // We can not test if the content will be ellipsized properly because
+  // we are useing CSS to create the ellipsis effect
+  it('More and less toggles correctly', () => {
+    const moreText = 'more';
+    const lessText = 'less';
+
+    renderEditableParagraph({
+      value: 'This is a mock content',
+    });
+
+    // Contains more by default
+    expect(screen.getByText(moreText)).toBeInTheDocument();
+    expect(screen.queryByText(lessText)).toBeNull();
+
+    // Toggles to less
+    act(() => {
+      userEvent.click(screen.getByText(moreText));
+    });
+    expect(screen.getByText(lessText)).toBeInTheDocument();
+    expect(screen.queryByText(moreText)).toBeNull();
+
+    // Toggles to more
+    act(() => {
+      userEvent.click(screen.getByText(lessText));
+    });
+    expect(screen.getByText(moreText)).toBeInTheDocument();
+    expect(screen.queryByText(lessText)).toBeNull();
+
+    act(() => {
+      userEvent.click(screen.getByText(moreText));
+    });
+
+    // Toggles to less
+    expect(screen.getByText(lessText)).toBeInTheDocument();
+    expect(screen.queryByText(moreText)).toBeNull();
+  });
 });
