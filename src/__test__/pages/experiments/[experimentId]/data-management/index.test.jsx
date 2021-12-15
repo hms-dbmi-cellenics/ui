15873002fb9c2abbf9f8919302057e815924f368
Marcell Pek <hi@marcell.is>
2021-12-15 13:04:25 +0000

    fix

--- src/__test__/pages/experiments/[experimentId]/data-management/index.test.jsx
+++ src/__test__/pages/experiments/[experimentId]/data-management/index.test.jsx
@@ -25,8 +25,15 @@ import { setActiveProject } from 'redux/actions/projects';
 
 jest.mock('utils/data-management/downloadFromUrl');
 jest.mock('react-resize-detector', () => (props) => props.children({ width: 100, height: 100 }));
+
+// Necessary due to LaunchAnalysisButotn being imported.
 jest.mock('utils/crypt', () => ({
   browserGenerateDigest: () => 'randomhash',
+}));
+
+// Necessary due to storage being used in the default SamplesTable.
+jest.mock('@aws-amplify/storage', () => ({
+  get: jest.fn(() => Promise.resolve('https://mock-s3-url.com')),
 }));
 
 const firstProjectWithSamples = projects.find((p) => p.samples.length > 0);
