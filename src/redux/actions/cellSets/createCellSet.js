0b1742805f691da0f8e5513b50e66a0db2fa7deb
cosa65 <martin@biomage.net>
2021-12-16 14:06:20 -0300

    Working version of createCellSet using jsonMerger

--- src/redux/actions/cellSets/createCellSet.js
+++ src/redux/actions/cellSets/createCellSet.js
@@ -1,8 +1,9 @@
+import fetchAPI from 'utils/fetchAPI';
+import { isServerError } from 'utils/fetchErrors';
 import { v4 as uuidv4 } from 'uuid';
 import endUserMessages from '../../../utils/endUserMessages';
 import pushNotificationMessage from '../../../utils/pushNotificationMessage';
 import { CELL_SETS_CREATE } from '../../actionTypes/cellSets';
-import saveCellSets from './saveCellSets';
 
 const createCellSet = (experimentId, name, color, cellIds) => async (dispatch, getState) => {
   const {
@@ -34,8 +35,51 @@ const createCellSet = (experimentId, name, color, cellIds) => async (dispatch, g
     },
   });
 
-  await dispatch(saveCellSets(experimentId));
-  pushNotificationMessage('info', endUserMessages.NEW_CLUSTER_CREATED);
+  const dataToUpload = {
+    key: uuidv4(),
+    name,
+    color,
+    cellIds: [...cellIds].map((id) => parseInt(id, 10)),
+    rootNode: false,
+    type: 'cellSets',
+  };
+
+  const jsonMergerQuery = '$[?(@.key == "scratchpad")]';
+
+  const jsonMergerObject = {
+    $match: {
+      query: jsonMergerQuery,
+      value: {
+        children: [
+          dataToUpload,
+        ],
+      },
+    },
+  };
+
+  const url = `/v1/experiments/${experimentId}/cellSets`;
+
+  try {
+    const response = await fetchAPI(
+      url,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/boschni-json-merger+json',
+        },
+        body: JSON.stringify([jsonMergerObject]),
+      },
+    );
+
+    await response.json();
+
+    pushNotificationMessage('info', endUserMessages.NEW_CLUSTER_CREATED);
+  } catch (e) {
+    if (!isServerError(e)) {
+      console.error(`fetch ${url} error ${e.message}`);
+    }
+    pushNotificationMessage('error', endUserMessages.ERROR_SAVING);
+  }
 };
 
 export default createCellSet;
