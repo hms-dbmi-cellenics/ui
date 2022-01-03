7eeec02431519236f10e5bf297be8b0c65b059aa
Marcell Pék <marcell@biomage.net>
2022-01-03 14:39:49 +0000

    Update crypt.js

--- src/utils/crypt.js
+++ src/utils/crypt.js
@@ -38,7 +38,7 @@ const getWebhookUrl = () => {
 
 const browserGenerateDigest = async (message) => {
   const msgUint8 = new TextEncoder().encode(message);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+  const hashBuffer = await crypto.subtle.digest('SHA-1', msgUint8);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
 
