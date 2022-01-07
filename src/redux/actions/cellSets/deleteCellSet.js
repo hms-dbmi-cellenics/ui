509c1dd682a070845e1926abaff3747a5f6551f9
cosa65 <martin@biomage.net>
2022-01-07 14:39:22 -0300

    WIP

--- src/redux/actions/cellSets/deleteCellSet.js
+++ src/redux/actions/cellSets/deleteCellSet.js
@@ -1,5 +1,26 @@
-import { CELL_SETS_DELETE } from '../../actionTypes/cellSets';
-import saveCellSets from './saveCellSets';
+import { CELL_SETS_DELETE } from 'redux/actionTypes/cellSets';
+
+import { isServerError, throwIfRequestFailed } from 'utils/fetchErrors';
+import pushNotificationMessage from 'utils/pushNotificationMessage';
+import endUserMessages from 'utils/endUserMessages';
+import fetchAPI from 'utils/fetchAPI';
+
+const deleteCellSetJsonMerger = (cellSetKey) => (
+  [
+    {
+      children: [
+        {
+          $match: {
+            query: `$[?(@.key == ${cellSetKey})]`,
+            value: {
+              $remove: true,
+            },
+          },
+        },
+      ],
+    },
+  ]
+);
 
 const deleteCellSet = (experimentId, key) => async (dispatch, getState) => {
   const {
@@ -18,7 +39,39 @@ const deleteCellSet = (experimentId, key) => async (dispatch, getState) => {
     },
   });
 
-  await dispatch(saveCellSets(experimentId));
+  const url = `/v1/experiments/${experimentId}/cellSets`;
+  try {
+    const response = await fetchAPI(
+      url,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+          deleteCellSetJsonMerger(key),
+        ),
+      },
+    );
+
+    const json = await response.json();
+    throwIfRequestFailed(response, json, endUserMessages.ERROR_SAVING);
+
+    // dispatch({
+    //   type: CELL_SETS_SAVE,
+    //   payload: {
+    //     experimentId,
+    //     saved: json,
+    //   },
+    // });
+  } catch (e) {
+    if (!isServerError(e)) {
+      console.error(`fetch ${url} error ${e.message}`);
+    }
+
+    pushNotificationMessage('error', endUserMessages.ERROR_SAVING);
+  }
+  // await dispatch(saveCellSets(experimentId));
 };
 
 export default deleteCellSet;
