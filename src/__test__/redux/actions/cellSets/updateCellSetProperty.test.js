ff0d8857e3d4efee912055a91038d3f4a40afe76
cosa65 <martin@biomage.net>
2022-01-19 18:04:23 -0300

    Fix tests for updateCellSetProperty action creator

--- src/__test__/redux/actions/cellSets/updateCellSetProperty.test.js
+++ src/__test__/redux/actions/cellSets/updateCellSetProperty.test.js
@@ -1,8 +1,13 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
-import updateCellSetProperty from '../../../../redux/actions/cellSets/updateCellSetProperty';
-import initialState from '../../../../redux/reducers/cellSets/initialState';
+import waitForActions from 'redux-mock-store-await-actions';
+
+import updateCellSetProperty from 'redux/actions/cellSets/updateCellSetProperty';
+
+import { CELL_SETS_UPDATE_PROPERTY } from 'redux/actionTypes/cellSets';
+import initialState from 'redux/reducers/cellSets/initialState';
+
 import '__test__/test-utils/setupTests';
 
 enableFetchMocks();
@@ -10,8 +15,18 @@ const mockStore = configureStore([thunk]);
 
 describe('updateCellSetProperty action', () => {
   const experimentId = '1234';
-  const key = 'root';
-  const property = { name: 'Root node!' };
+  const key = 'node';
+  const property = { name: 'Some node!' };
+
+  const cellSetsState = {
+    ...initialState,
+    properties: {
+      [key]: {
+        rootNode: false,
+        parentNodeKey: 'someParentNodeKey',
+      },
+    },
+  };
 
   beforeEach(() => {
     const response = new Response(JSON.stringify({}));
@@ -22,32 +37,25 @@ describe('updateCellSetProperty action', () => {
   });
 
   it('Does not dispatch on loading state', async () => {
-    const store = mockStore({ cellSets: { loading: true, error: false } });
+    const store = mockStore({ cellSets: { ...cellSetsState, loading: true, error: false } });
     store.dispatch(updateCellSetProperty(experimentId, key, property));
+
     expect(store.getActions().length).toEqual(0);
   });
 
   it('Does not dispatch on error state', async () => {
-    const store = mockStore({ cellSets: { loading: false, error: true } });
+    const store = mockStore({ cellSets: { ...cellSetsState, loading: false, error: true } });
     store.dispatch(updateCellSetProperty(experimentId, key, property));
     expect(store.getActions().length).toEqual(0);
   });
 
   it('Dispatches an action to update property to the reducer', async () => {
-    const store = mockStore({ cellSets: { ...initialState, loading: false } });
+    const store = mockStore({ cellSets: { ...cellSetsState, loading: false } });
     store.dispatch(updateCellSetProperty(experimentId, key, property));
+
+    await waitForActions(store, [CELL_SETS_UPDATE_PROPERTY]);
 
     const firstAction = store.getActions()[0];
     expect(firstAction).toMatchSnapshot();
-  });
-
-  it('Last action dispatches cellSetSave event', async () => {
-    const store = mockStore({ cellSets: { ...initialState, loading: false } });
-    store.dispatch(updateCellSetProperty(experimentId, key, property));
-
-    const lastActionID = store.getActions().length - 1;
-    const lastAction = store.getActions()[lastActionID];
-
-    expect(lastAction).toMatchSnapshot();
   });
 });
