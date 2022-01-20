553d7b3542c7b0fc0cc51f3d03a2cd4df19b6b5c
cosa65 <martin@biomage.net>
2022-01-20 13:32:01 -0300

    Add tests for HierarchicalTree

--- src/__test__/components/data-exploration/hierarchical-tree/HierarchicalTree.test.jsx
+++ src/__test__/components/data-exploration/hierarchical-tree/HierarchicalTree.test.jsx
@@ -7,6 +7,26 @@ import '__test__/test-utils/setupTests';
 
 const experimentId = 'asd';
 describe('HierarchicalTree', () => {
+  const child = {
+    key: '1a',
+    name: 'first child',
+    color: '#00FF00',
+  };
+
+  const firstParent = {
+    key: '1',
+    name: 'parent 1',
+    rootNode: true,
+    children: [],
+  };
+
+  const secondParent = {
+    key: '2',
+    name: 'parent 2',
+    rootNode: true,
+    children: [],
+  };
+
   it('Renders correctly', () => {
     const treeData = [{
       key: '1',
@@ -59,7 +79,6 @@ describe('HierarchicalTree', () => {
       dragNode: {
         ...firstChild,
         pos: '0-0-0',
-        props: { eventKey: firstChild.key },
       },
       dragNodesKeys: [firstChild.key],
       dropPosition: 2,
@@ -67,7 +86,6 @@ describe('HierarchicalTree', () => {
       node: {
         ...thirdChild,
         pos: '0-0-3',
-        props: { eventKey: thirdChild.key },
       },
     };
 
@@ -93,19 +111,6 @@ describe('HierarchicalTree', () => {
   });
 
   it('Can\'t have child component change parent', () => {
-    const child = {
-      key: '1a',
-      name: 'first child',
-      color: '#00FF00',
-    };
-
-    const secondParent = {
-      key: '2',
-      name: 'parent 2',
-      rootNode: true,
-      children: [],
-    };
-
     const treeData = [
       {
         key: '1',
@@ -122,15 +127,13 @@ describe('HierarchicalTree', () => {
       dragNode: {
         ...child,
         pos: '0-0-0',
-        props: { eventKey: child.key },
       },
       dragNodesKeys: [child.key],
       dropPosition: 1,
-      dropToGap: false,
+      dropToGap: true,
       node: {
         ...secondParent,
         pos: '0-1-2',
-        props: { eventKey: secondParent.key },
       },
     };
 
@@ -151,20 +154,6 @@ describe('HierarchicalTree', () => {
   });
 
   it("Can't drop parent inside node", () => {
-    const firstParent = {
-      key: '1',
-      name: 'parent 1',
-      rootNode: true,
-      children: [],
-    };
-
-    const secondParent = {
-      key: '2',
-      name: 'parent 2',
-      rootNode: true,
-      children: [],
-    };
-
     const treeData = [
       firstParent,
       secondParent,
@@ -174,15 +163,13 @@ describe('HierarchicalTree', () => {
       dragNode: {
         ...secondParent,
         pos: '0-0',
-        props: { eventKey: secondParent.key },
       },
       dragNodesKeys: [secondParent.key],
       dropPosition: 1,
-      dropToGap: false,
+      dropToGap: true,
       node: {
         ...firstParent,
         pos: '0-0-1',
-        props: { eventKey: firstParent.key },
       },
     };
 
@@ -201,6 +188,136 @@ describe('HierarchicalTree', () => {
     component.update();
 
     expect(mockOnCellSetReorder).toHaveBeenCalledTimes(0);
+  });
+
+  it("Can't drag child across parent nodes", () => {
+    const treeData = [
+      firstParent,
+      secondParent,
+    ];
+
+    const dropInfo = {
+      dragNode: {
+        ...child,
+        pos: '0-0-1',
+      },
+      dragNodesKeys: [secondParent.key],
+      dropPosition: 4,
+      dropToGap: true,
+      node: {
+        ...secondParent,
+        pos: '0-1-0',
+      },
+    };
+
+    const mockOnCellSetReorder = jest.fn();
+
+    const component = mount(
+      <HierarchicalTree
+        treeData={treeData}
+        experimentId={experimentId}
+        onCellSetReorder={mockOnCellSetReorder}
+      />,
+    );
+
+    waitForComponentToPaint(component);
+    const tree = component.find('HierarchicalTree Tree');
+    tree.getElement().props.onDrop(dropInfo);
+    component.update();
+
+    expect(mockOnCellSetReorder).toHaveBeenCalledTimes(0);
+  });
+
+  it("Doesn't trigger drop if positions don't change", () => {
+    const treeData = [
+      firstParent,
+      secondParent,
+    ];
+
+    const dropInfo = {
+      dragNode: {
+        ...child,
+        pos: '0-0-1',
+      },
+      dragNodesKeys: [secondParent.key],
+      dropPosition: 1,
+      dropToGap: true,
+      node: {
+        ...child,
+        key: '1b',
+        pos: '0-0-1',
+      },
+    };
+
+    const mockOnCellSetReorder = jest.fn();
+
+    const component = mount(
+      <HierarchicalTree
+        treeData={treeData}
+        experimentId={experimentId}
+        onCellSetReorder={mockOnCellSetReorder}
+      />,
+    );
+
+    waitForComponentToPaint(component);
+    const tree = component.find('HierarchicalTree Tree');
+    tree.getElement().props.onDrop(dropInfo);
+    component.update();
+
+    expect(mockOnCellSetReorder).toHaveBeenCalledTimes(0);
+  });
+
+  it("Can't drop into a child, only in a gap", () => {
+    const treeData = [
+      firstParent,
+      secondParent,
+    ];
+
+    const dropInfo = {
+      dragNode: {
+        ...child,
+        pos: '0-0-1',
+      },
+      dragNodesKeys: [secondParent.key],
+      dropPosition: 4,
+      dropToGap: false,
+      node: {
+        ...child,
+        key: '1b',
+        pos: '0-0-5',
+      },
+    };
+
+    const mockOnCellSetReorder = jest.fn();
+
+    const component = mount(
+      <HierarchicalTree
+        treeData={treeData}
+        experimentId={experimentId}
+        onCellSetReorder={mockOnCellSetReorder}
+      />,
+    );
+
+    waitForComponentToPaint(component);
+    const tree = component.find('HierarchicalTree Tree');
+    tree.getElement().props.onDrop(dropInfo);
+    component.update();
+
+    expect(mockOnCellSetReorder).toHaveBeenCalledTimes(0);
+
+    // but dropToGap: false can be false when the dropInfo is rootNode
+    // (we see this when we are moving the cell set into the first spot)
+    const newDropInfo = {
+      ...dropInfo,
+      node: {
+        ...firstParent,
+        pos: '0-0',
+      },
+    };
+
+    tree.getElement().props.onDrop(newDropInfo);
+    component.update();
+    expect(mockOnCellSetReorder).toHaveBeenCalledTimes(1);
   });
 
   it('tree data is not checked by default', () => {
