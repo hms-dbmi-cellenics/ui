6587f9fba96f88c85a06c03e2b150cb1e3a7ca95
StefanBabukov <stefanbabukov98@gmail.com>
2022-01-26 12:07:28 +0000

    Update src/utils/fileNames.js
    Co-authored-by: Anugerah Erlaut <aerlaut@live.com>

--- src/utils/fileNames.js
+++ src/utils/fileNames.js
@@ -1,6 +1,14 @@
-const frequencyPlotCsvName = (experimentName, frequencyType) => (
-  `${experimentName.replace(/ /g, '_')}-freq-plot-${frequencyType}`
-);
+
+import { plotNames } from 'utils/constants';
+
+const plotCsvFilename = (experimentName, plotType, extras) => {
+
+const cleanExperimentName = experimentName.replace(/ /g, '_');
+const cleanPlotName = plotNames[plotType].replace(/ /g, '_').toLowerCase();
+const extras = extras.join('-').replace(/ /g, '_');
+
+return `${clearnExperimentName}-${cleanPlotName}-${extras}`;
+}
 
 // eslint-disable-next-line import/prefer-default-export
-export { frequencyPlotCsvName };
+export plotCsvFilename;
