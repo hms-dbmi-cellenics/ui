b5c766f655c5181949e6226106a7ff84ff899490
stefanbabukov <stefanbabukov98@gmail.com>
2022-02-08 12:30:32 +0000

    fix tests

--- src/__test__/components/data-exploration/generic-gene-table/CreateCellSetModal.test.jsx
+++ src/__test__/components/data-exploration/generic-gene-table/CreateCellSetModal.test.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-
+import { Provider } from 'react-redux';
+import { GENES_SELECT } from 'redux/actionTypes/genes';
+import fake from '__test__/test-utils/constants';
 import CreateCellSetModal from 'components/data-exploration/generic-gene-table/CreateCellSetModal';
+import { makeStore } from 'redux/store';
 
 describe('Create cell set modal', () => {
+  let storeState;
+  const selectedGenes = ['DOK3', 'DOK4'];
+
+  beforeEach(async () => {
+    storeState = makeStore();
+
+    await storeState.dispatch({
+      type: GENES_SELECT,
+      payload: {
+        experimentId: fake.EXPERIMENT_ID,
+        genes: selectedGenes,
+      },
+    });
+  });
+
   it('renders correctly', () => {
-    const selectedGenes = ['DOK3', 'DOK4'];
     const onCancel = jest.fn();
-    render(<CreateCellSetModal selectedGenes={selectedGenes} onCancel={onCancel} />);
+    render(<Provider store={storeState}><CreateCellSetModal onCancel={onCancel} /></Provider>);
 
     expect(screen.getAllByText('Create a new cell set based on gene expression')).toHaveLength(1);
     expect(screen.getByText('DOK3')).toBeInTheDocument();
