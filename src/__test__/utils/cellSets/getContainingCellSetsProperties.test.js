018cfa8833dde7911ca637bc24103464e0528c5c
Anugerah Erlaut <aerlaut@live.com>
2022-02-08 18:03:19 +0000

    update snapshots

--- src/__test__/utils/cellSets/getContainingCellSetsProperties.test.js
+++ src/__test__/utils/cellSets/getContainingCellSetsProperties.test.js
@@ -2,11 +2,10 @@ import getContainingCellSetsProperties from 'utils/cellSets/getContainingCellSet
 import { mockCellSets1 } from '__test__/test-utils/cellSets.mock';
 
 describe('get cell class properties tests', () => {
-  const { cellSets } = mockCellSets1;
   it('returns information for a passed cellId', () => {
     const cellId = 3;
     const cellSetClassKeys = ['test'];
-    const result = getContainingCellSetsProperties(cellId, cellSetClassKeys, cellSets);
+    const result = getContainingCellSetsProperties(cellId, cellSetClassKeys, mockCellSets1);
 
     expect(result).toMatchSnapshot();
   });
