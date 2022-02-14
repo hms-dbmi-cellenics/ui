49a2ecbb36a15512612891475e55940fdedba03f
stefanbabukov <stefanbabukov98@gmail.com>
2022-02-14 11:22:03 +0000

    add test for changing a window panel

--- src/__test__/layout/addWindow.test.js
+++ src/__test__/layout/addWindow.test.js
@@ -1,37 +1,29 @@
 import addWindow from 'redux/actions/layout/addWindow';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import initialState from 'redux/reducers/layout/initialState';
 
 const mockStore = configureStore([thunk]);
 
 describe('add window', () => {
+  const state = { layout: initialState };
+  let store;
+  beforeEach(() => {
+    store = mockStore(state);
+  });
+
   it('adds a window to existing ones', () => {
-    const state = {
-      layout: {
-        windows: {
-          first: {
-            first: 'samplewindow',
-            second: 'secondFirstwindow',
-            splitPercentage: 100,
-            direction: 'row',
-          },
-          second: 'secondWindow',
-          splitPercentage: 60,
-          direction: 'row',
-        },
-      },
-    };
-    const store = mockStore(state);
     const newState = store.dispatch(addWindow('newPanel', 'newWindow'));
     expect(newState).toMatchSnapshot();
   });
 
   it('adds a new window if layout is empty', () => {
-    const state = {
-      layout: {},
-    };
-    const store = mockStore(state);
-    const newState = store.dispatch(addWindow('newPanel', 'newWindow'));
+    const newState = store.dispatch(addWindow('newPanel'));
+    expect(newState).toMatchSnapshot();
+  });
+
+  it('updates panel if window exists', () => {
+    const newState = store.dispatch(addWindow('Differential expression', 'Genes'));
     expect(newState).toMatchSnapshot();
   });
 });
