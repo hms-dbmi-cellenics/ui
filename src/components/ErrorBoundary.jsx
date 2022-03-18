00061144f0a0717a71aa8ca7037c1e2809928077
Anugerah Erlaut <aerlaut@live.com>
2022-03-18 09:13:15 +0000

    Continue showing the UI if the platform doesn't blow

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -3,17 +3,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import postErrorToSlack from 'utils/postErrorToSlack';
 import { connect } from 'react-redux';
-import Error from 'pages/_error';
 
 // Implementation of https://reactjs.org/docs/error-boundaries.html
 // Using React class because it's not yet supported for functional components
 // according to https://stackoverflow.com/a/68075800/1940886
 class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
   static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
@@ -27,8 +21,6 @@ class ErrorBoundary extends React.Component {
   }
 
   render() {
-    if (this.state.hasError) return <Error />;
-
     return this.props.children;
   }
 }
