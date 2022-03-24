f9caec67364b13e070a495c2db653cecbde6b97c
Anugerah Erlaut <aerlaut@live.com>
2022-03-24 10:59:36 +0000

    error boundary show error page on error

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import postErrorToSlack from 'utils/postErrorToSlack';
 import { connect } from 'react-redux';
+import Error from 'pages/_error';
 
 // Implementation of https://reactjs.org/docs/error-boundaries.html
 // Using React class because it's not yet supported for functional components
@@ -10,14 +11,16 @@ import { connect } from 'react-redux';
 class ErrorBoundary extends React.Component {
   // The parent constructor needs to be called so that the
   // proper functions are setup and views can be rendered
-  // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
+    this.state = { hasError: false };
   }
 
   // This function needs to still be defined even if empty
   // so that the Component can render a view even when there are errors
-  static getDerivedStateFromError() { }
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
 
   componentDidCatch(error, errorInfo) {
     // Act on the error inside this function
@@ -27,6 +30,8 @@ class ErrorBoundary extends React.Component {
   }
 
   render() {
+    if (this.state.hasError) return <Error />;
+
     return this.props.children;
   }
 }
