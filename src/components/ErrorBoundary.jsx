61c9b4db646a82253e130c061b280cc22f8d6491
Anugerah Erlaut <aerlaut@live.com>
2022-03-24 16:00:11 +0000

    remove error view

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import postErrorToSlack from 'utils/postErrorToSlack';
 import { connect } from 'react-redux';
-import Error from 'pages/_error';
 
 // Implementation of https://reactjs.org/docs/error-boundaries.html
 // Using React class because it's not yet supported for functional components
@@ -11,16 +10,14 @@ import Error from 'pages/_error';
 class ErrorBoundary extends React.Component {
   // The parent constructor needs to be called so that the
   // proper functions are setup and views can be rendered
+  // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
   }
 
   // This function needs to still be defined even if empty
   // so that the Component can render a view even when there are errors
-  static getDerivedStateFromError() {
-    return { hasError: true };
-  }
+  static getDerivedStateFromError() { }
 
   componentDidCatch(error, errorInfo) {
     // Act on the error inside this function
@@ -30,8 +27,6 @@ class ErrorBoundary extends React.Component {
   }
 
   render() {
-    if (this.state.hasError) return <Error />;
-
     return this.props.children;
   }
 }
