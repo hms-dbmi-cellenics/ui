afb8b024cb9e2893deabdf20c434686bba933280
Anugerah Erlaut <aerlaut@live.com>
2022-03-24 11:30:11 +0000

    add link to paper

--- src/components/data-exploration/generic-gene-table/ExpressionCellSetModal.jsx
+++ src/components/data-exploration/generic-gene-table/ExpressionCellSetModal.jsx
@@ -133,7 +133,18 @@ const ExpressionCellSetModal = (props) => {
         </Form.List>
       </Form>
       <Alert type='info' message='To edit the list of genes, return to the gene list and change your selection.' style={{ margin: '0.5em 0' }} />
-      <Alert type='warning' message='Due to high dropout in scRNA-seq, cell sets defined using only a few genes are potentially misleading. We recommend using these cell sets for exploratory purposes only and to use computed clusters for comparisons.' />
+      <Alert
+        type='warning'
+        message={(
+          <>
+            Due to
+            {' '}
+            <a href='https://www.nature.com/articles/s41467-020-14976-9'>high dropout</a>
+            {' '}
+            in scRNA-seq, cell sets defined using only a few genes are potentially misleading. We recommend using these cell sets for exploratory purposes only and to use computed clusters for comparisons.
+          </>
+        )}
+      />
     </Modal>
   );
 };
