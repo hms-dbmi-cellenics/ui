74e9a0945a8774e8ac76ad3f06bded57ce7ac87c
cosa65 <martin@biomage.net>
2022-04-01 14:13:09 -0300

    Add loadExperiments/loadProjects that interacts with api v2

--- src/redux/actions/projects/loadProjects.js
+++ src/redux/actions/projects/loadProjects.js
@@ -2,24 +2,71 @@ import fetchAPI from 'utils/http/fetchAPI';
 import handleError from 'utils/http/handleError';
 import endUserMessages from 'utils/endUserMessages';
 import { PROJECTS_ERROR, PROJECTS_LOADED, PROJECTS_LOADING } from 'redux/actionTypes/projects';
-import loadSamples from '../samples/loadSamples';
+
+import config from 'config';
+import { api } from 'utils/constants';
+
+import loadSamples from 'redux/actions/samples/loadSamples';
+
+const toApiV1 = (experimentListV2) => {
+  const projectsListV1 = experimentListV2.map((experimentData) => {
+    const {
+      createdAt,
+      updatedAt,
+      id,
+      samplesOrder,
+      ...restOfExperimentData
+    } = experimentData;
+
+    // Not assigning "lastAnalyzed" because that is no longer used anywhere
+    const experimentDataV1 = {
+      createdDate: createdAt,
+      lastModified: updatedAt,
+      experiments: [id],
+      uuid: id,
+      samples: samplesOrder,
+      ...restOfExperimentData,
+    };
+
+    return experimentDataV1;
+  });
+
+  return projectsListV1;
+};
 
 const loadProjects = () => async (dispatch) => {
-  const url = '/v1/projects';
-
   dispatch({
     type: PROJECTS_LOADING,
   });
 
+  let url;
+
   try {
-    let data = await fetchAPI(url);
+    let data;
 
-    // filter out "projects" that are actually old experiments without a project
-    data = data.filter((project) => project.name !== project.uuid);
+    if (config.currentApiVersion === api.V1) {
+      url = '/v1/projects';
+      data = await fetchAPI(url);
 
-    await Promise.all(data
-      .filter((entry) => entry.samples.length)
-      .map((entry) => dispatch(loadSamples(false, entry.uuid))));
+      // filter out "projects" that are actually old experiments without a project
+      data = data.filter((project) => project.name !== project.uuid);
+
+      await Promise.all(data
+        .filter((entry) => entry.samples.length)
+        .map((entry) => dispatch(loadSamples(false, entry.uuid))));
+    } else if (config.currentApiVersion === api.V2) {
+      url = '/v2/experiments';
+      data = await fetchAPI(url);
+
+      data = toApiV1(data);
+
+      // This section commented out because we going to use it
+      // when samples loading is implemented in api v2
+
+      // await Promise.all(data
+      //   .filter((entry) => entry.samples.length)
+      //   .map((entry) => dispatch(loadSamples(false, entry.uuid))));
+    }
 
     const ids = data.map((project) => project.uuid);
 
