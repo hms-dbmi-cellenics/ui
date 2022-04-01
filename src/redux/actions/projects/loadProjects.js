7b773c5942fe3f154cff09514ef18fe457ff001b
cosa65 <martin@biomage.net>
2022-04-01 15:32:42 -0300

    Add explanatory comment

--- src/redux/actions/projects/loadProjects.js
+++ src/redux/actions/projects/loadProjects.js
@@ -68,6 +68,9 @@ const loadProjects = () => async (dispatch) => {
 
       data = toApiV1(data);
 
+      // This section commented out because we going to use it
+      // when samples loading is implemented in api v2
+
       // await Promise.all(data
       //   .filter((entry) => entry.samples.length)
       //   .map((entry) => dispatch(loadSamples(false, entry.uuid))));
