2974058210ed742d0ee047adbe7d83e8cfa3e30b
cosa65 <martin@biomage.net>
2022-04-07 14:31:31 -0300

    Add test for redux actions for deleteSample

--- src/__test__/redux/actions/samples/deleteSample.test.js
+++ src/__test__/redux/actions/samples/deleteSample.test.js
@@ -2,18 +2,25 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
 
-import deleteSamples from '../../../../redux/actions/samples/deleteSamples';
-import initialSampleState, { sampleTemplate } from '../../../../redux/reducers/samples/initialState';
-import initialProjectState, { projectTemplate } from '../../../../redux/reducers/projects/initialState';
+import deleteSamples from 'redux/actions/samples/deleteSamples';
+import initialSampleState, { sampleTemplate } from 'redux/reducers/samples/initialState';
+import initialProjectState, { projectTemplate } from 'redux/reducers/projects/initialState';
 
-import { saveProject } from '../../../../redux/actions/projects';
-import { SAMPLES_DELETE, SAMPLES_SAVED, SAMPLES_SAVING } from '../../../../redux/actionTypes/samples';
-import { PROJECTS_UPDATE } from '../../../../redux/actionTypes/projects';
-import { EXPERIMENTS_SAVING } from '../../../../redux/actionTypes/experiments';
+import { saveProject } from 'redux/actions/projects';
+import {
+  SAMPLES_DELETE, SAMPLES_DELETE_API_V1, SAMPLES_SAVED, SAMPLES_SAVING,
+} from 'redux/actionTypes/samples';
+import { PROJECTS_UPDATE } from 'redux/actionTypes/projects';
+import { EXPERIMENTS_SAVING } from 'redux/actionTypes/experiments';
+
+import config from 'config';
+import { api } from 'utils/constants';
+
+jest.mock('config');
 
 enableFetchMocks();
 
-jest.mock('../../../../redux/actions/projects/saveProject');
+jest.mock('redux/actions/projects/saveProject');
 saveProject.mockImplementation(() => async () => { });
 
 const mockStore = configureStore([thunk]);
@@ -67,12 +74,32 @@ describe('deleteSample action', () => {
     expect(actions[1].type).toEqual(PROJECTS_UPDATE);
 
     // Delete sample
-    expect(actions[2].type).toEqual(SAMPLES_DELETE);
+    expect(actions[2].type).toEqual(SAMPLES_DELETE_API_V1);
 
     // Experiments being saved
     expect(actions[3].type).toEqual(EXPERIMENTS_SAVING);
 
     // Resolve loading state
     expect(actions[4].type).toEqual(SAMPLES_SAVED);
+  });
+
+  it('Dispatches event correctly for api v2', async () => {
+    config.currentApiVersion = api.V2;
+
+    const store = mockStore(initialState);
+    await store.dispatch(deleteSamples([mockSampleUuid]));
+
+    // Sets up loading state for saving project
+    const actions = store.getActions();
+
+    expect(saveProject).toHaveBeenCalled();
+
+    expect(actions[0].type).toEqual(SAMPLES_SAVING);
+
+    // Delete sample
+    expect(actions[1].type).toEqual(SAMPLES_DELETE);
+
+    // Resolve loading state
+    expect(actions[2].type).toEqual(SAMPLES_SAVED);
   });
 });
