a95805c4dd5994950b2af740fb5af21a87d52553
Anugerah Erlaut <aerlaut@live.com>
2022-04-11 11:35:10 +0100

    Revert "Add loadExperiments/loadProjects that interacts with api v2"
    This reverts commit 74e9a0945a8774e8ac76ad3f06bded57ce7ac87c.

--- src/redux/actions/experiments/loadExperiments.js
+++ src/redux/actions/experiments/loadExperiments.js
@@ -7,43 +7,6 @@ import {
   EXPERIMENTS_LOADING,
 } from 'redux/actionTypes/experiments';
 
-import config from 'config';
-import { api } from 'utils/constants';
-
-const toApiV1 = (experimentV2) => {
-  const {
-    id,
-    name,
-    description,
-    samplesOrder,
-    notifyByEmail,
-    processingConfig,
-    createdAt,
-    pipelines,
-  } = experimentV2;
-
-  const experimentV1 = {
-    experimentId: id,
-    projectId: id,
-    description,
-    experimentName: name,
-    processingConfig,
-    createdDate: createdAt,
-    notifyByEmail,
-    sampleIds: samplesOrder,
-    // lastViewed: ignored, it isn\'t being used in the UI,
-    meta: {
-      // This is always 10x and organism so we can just generate them here
-      organism: null,
-      type: '10x',
-      gem2s: pipelines.gem2s ?? undefined,
-      pipeline: pipelines.qc ?? undefined,
-    },
-  };
-
-  return experimentV1;
-};
-
 const loadExperiments = (
   projectUuid,
 ) => async (dispatch) => {
@@ -51,19 +14,9 @@ const loadExperiments = (
     type: EXPERIMENTS_LOADING,
   });
 
-  let url;
+  const url = `/v1/projects/${projectUuid}/experiments`;
   try {
-    let data;
-
-    if (config.currentApiVersion === api.V1) {
-      url = `/v1/projects/${projectUuid}/experiments`;
-      data = await fetchAPI(url);
-    } else if (config.currentApiVersion === api.V2) {
-      url = `/v2/experiments/${projectUuid}`;
-      data = await fetchAPI(url);
-
-      data = [toApiV1(data)];
-    }
+    const data = await fetchAPI(url);
 
     dispatch({
       type: EXPERIMENTS_LOADED,
