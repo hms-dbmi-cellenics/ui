1cb04d99580b8023d2826d27b23c9943486243c3
Anugerah Erlaut <aerlaut@live.com>
2022-04-13 18:15:16 +0100

    remove console log

--- src/components/plots/CellSizeDistributionHistogram.jsx
+++ src/components/plots/CellSizeDistributionHistogram.jsx
@@ -14,7 +14,6 @@ const CellSizeDistributionHistogram = (props) => {
 
   useEffect(() => {
     if (config && plotData) {
-      console.log('*** config', config);
       setPlotSpec(generateSpec(config, plotData, highestUmi));
     }
   }, [config, plotData]);
