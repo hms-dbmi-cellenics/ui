64af835774e0b29b9c06aa35bf2f9ff373fde41b
cosa65 <martin@biomage.net>
2022-04-18 14:37:49 -0300

    Update updateSample to be more clear about v1 and v2 code separation

--- src/redux/actions/samples/updateSample.js
+++ src/redux/actions/samples/updateSample.js
@@ -15,87 +15,94 @@ import fetchAPI from 'utils/http/fetchAPI';
 import config from 'config';
 import { api } from 'utils/constants';
 
-const sendApiV2Request = async (experimentId, sampleId, diff, dispatch) => {
-  const url = `/v2/experiments/${experimentId}/samples/${sampleId}`;
-  const body = diff;
-
-  dispatch({
-    type: SAMPLES_SAVING,
-    payload: {
-      message: endUserMessages.SAVING_SAMPLE,
-    },
-  });
-
-  try {
-    await fetchAPI(
-      url,
-      {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(body),
-      },
-    );
-  } catch (e) {
-    const errorMessage = handleError(e, endUserMessages.ERROR_SAVING, false);
-
-    dispatch({
-      type: SAMPLES_ERROR,
-      payload: {
-        error: errorMessage,
-      },
-    });
-
-    throw e;
-  }
-
-  dispatch({
-    type: SAMPLES_SAVED,
-  });
-
-  dispatch({
-    type: SAMPLES_UPDATE,
-    payload: {
-      sampleId,
-      sample: diff,
-    },
-  });
-};
-
-const updateSample = (
-  sampleUuid,
-  diff,
-) => async (dispatch, getState) => {
+const updateSampleApiV1 = (sampleUuid, diff) => async (dispatch, getState) => {
   const sample = _.cloneDeep(getState().samples[sampleUuid]);
 
   try {
-    if (config.currentApiVersion === api.V1) {
-      // eslint-disable-next-line no-param-reassign
-      diff.lastModified = moment().toISOString();
+    // eslint-disable-next-line no-param-reassign
+    diff.lastModified = moment().toISOString();
 
-      const newSample = mergeObjectWithArrays(sample, diff);
+    const newSample = mergeObjectWithArrays(sample, diff);
 
-      const notifyUser = false;
-      await dispatch(saveSamples(sample.projectUuid, newSample, true, true, notifyUser));
+    const notifyUser = false;
+    await dispatch(saveSamples(sample.projectUuid, newSample, true, true, notifyUser));
 
-      dispatch({
-        type: SAMPLES_UPDATE,
-        payload: {
-          sampleUuid,
-          sample: diff,
-        },
-      });
-    } else if (config.currentApiVersion === api.V2) {
-      if (_.isNil(diff.name) || diff.metadata) {
-        throw new Error('This action shouldn\'t be used to update metadata with api v2');
-      }
-
-      // in api v2 projectUuid is actually experimentId
-      await sendApiV2Request(sample.projectUuid, sampleUuid, diff, dispatch);
-    }
+    dispatch({
+      type: SAMPLES_UPDATE,
+      payload: {
+        sampleUuid,
+        sample: diff,
+      },
+    });
   } catch (e) {
     handleError(e, endUserMessages.ERROR_SAVING);
+  }
+};
+
+const updateSampleApiV2 = (sampleId, diff) => async (dispatch, getState) => {
+  // In api v2 projectUuid and experimentId are the same
+  const experimentId = getState().samples[sampleId].projectUuid;
+
+  try {
+    if (_.isNil(diff.name) || diff.metadata) {
+      throw new Error('This action can be used to update only the name in sample');
+    }
+
+    const url = `/v2/experiments/${experimentId}/samples/${sampleId}`;
+    const body = diff;
+
+    dispatch({
+      type: SAMPLES_SAVING,
+      payload: {
+        message: endUserMessages.SAVING_SAMPLE,
+      },
+    });
+
+    try {
+      await fetchAPI(
+        url,
+        {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(body),
+        },
+      );
+    } catch (e) {
+      const errorMessage = handleError(e, endUserMessages.ERROR_SAVING, false);
+
+      dispatch({
+        type: SAMPLES_ERROR,
+        payload: {
+          error: errorMessage,
+        },
+      });
+
+      throw e;
+    }
+
+    dispatch({
+      type: SAMPLES_SAVED,
+    });
+
+    dispatch({
+      type: SAMPLES_UPDATE,
+      payload: {
+        sampleId,
+        sample: diff,
+      },
+    });
+  } catch (e) {
+    handleError(e, endUserMessages.ERROR_SAVING);
+  }
+};
+
+const updateSample = (sampleUuid, diff) => {
+  if (config.currentApiVersion === api.V1) {
+    return updateSampleApiV1(sampleUuid, diff);
+  } if (config.currentApiVersion === api.V2) {
+    return updateSampleApiV2(sampleUuid, diff);
   }
 };
 
