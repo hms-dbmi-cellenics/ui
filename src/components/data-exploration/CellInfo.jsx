bd4e5177265b886e23ba2c552fced1e09d447549
Anugerah Erlaut <aerlaut@live.com>
2022-04-20 11:37:29 +0100

    use ref callback to switch position

--- src/components/data-exploration/CellInfo.jsx
+++ src/components/data-exploration/CellInfo.jsx
@@ -1,60 +1,79 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Card } from 'antd';
 import PropTypes from 'prop-types';
 
 const EM = 16; // px
-const CELLINFO_WIDTH = 200; // px
-const CELLINFO_Y_PADDING = 6 * EM;
-const Y_PADDING = 2 * EM;
+const CELLINFO_Y_PADDING = 2 * EM;
 
 const cellInfoStyle = { fontSize: '0.75rem' };
+
+const getCellinfoCoordinates = (coordinates, el, boundingX, boundingY) => {
+  const popupWidth = el?.offsetWidth;
+  const popupHeight = el?.offsetHeight;
+
+  const invertX = () => coordinates.x + popupWidth > boundingX;
+  const invertY = () => coordinates.y + popupHeight + CELLINFO_Y_PADDING > boundingY;
+
+  const left = invertX() ? coordinates.x - (popupWidth + EM) : coordinates.x + EM;
+  const top = invertY() ? coordinates.y - (popupHeight + EM) : coordinates.y + EM;
+
+  return { left, top };
+};
 
 const CellInfo = (props) => {
   const {
     containerWidth, containerHeight, coordinates, cellInfo,
   } = props;
 
-  const numTextRows = (cellInfo.cellSets?.length || 0) + 2;
+  const [tooltipEl, setTooltipEl] = useState(null);
 
-  const invertX = () => coordinates.x + CELLINFO_WIDTH > containerWidth;
-  const invertY = () => coordinates.y + (numTextRows * EM) + CELLINFO_Y_PADDING > containerHeight;
+  const getTooltipElement = useCallback((el) => {
+    if (el) setTooltipEl(el?.firstChild);
+  }, []);
 
-  const left = invertX() ? coordinates.x - (CELLINFO_WIDTH + EM) : coordinates.x + EM;
-  const top = invertY() ? coordinates.y - (numTextRows * EM + Y_PADDING) : coordinates.y + EM;
+  const { left, top } = getCellinfoCoordinates(
+    coordinates,
+    tooltipEl,
+    containerWidth,
+    containerHeight,
+  );
 
   return (
-    <Card
-      size='small'
-      style={{
-        width: CELLINFO_WIDTH,
-        zIndex: 6,
-        border: 0,
-        position: 'absolute',
-        left,
-        top,
-        pointerEvents: 'none',
-      }}
-    >
-      <div style={cellInfoStyle}>
-        {`Cell id: ${cellInfo.cellId}`}
-      </div>
-      {cellInfo.geneName ? (
+    // We have to wrap the <Card> in a <div> because Antd does not correctly set the ref
+    // https://github.com/ant-design/ant-design/issues/28582
+    <div style={{ display: 'inline-block' }} ref={getTooltipElement}>
+      <Card
+        size='small'
+        style={{
+          zIndex: 6,
+          border: 0,
+          position: 'absolute',
+          left,
+          top,
+          pointerEvents: 'none',
+        }}
+      >
         <div style={cellInfoStyle}>
-          {`Gene name: ${cellInfo.geneName}`}
+          {`Cell id: ${cellInfo.cellId}`}
         </div>
-      ) : <></>}
-      {cellInfo.expression !== undefined ? (
-        <div style={cellInfoStyle}>
-          Expression Level:&nbsp;
-          {parseFloat(cellInfo.expression.toFixed(3))}
-        </div>
-      ) : <></>}
-      {cellInfo.cellSets?.length > 0 ? cellInfo.cellSets.map((cellSetName) => (
-        <div style={cellInfoStyle}>
-          {cellSetName}
-        </div>
-      )) : <></>}
-    </Card>
+        {cellInfo.geneName ? (
+          <div style={cellInfoStyle}>
+            {`Gene name: ${cellInfo.geneName}`}
+          </div>
+        ) : <></>}
+        {cellInfo.expression !== undefined ? (
+          <div style={cellInfoStyle}>
+            Expression Level:&nbsp;
+            {parseFloat(cellInfo.expression.toFixed(3))}
+          </div>
+        ) : <></>}
+        {cellInfo.cellSets?.length > 0 ? cellInfo.cellSets.map((cellSetName) => (
+          <div style={cellInfoStyle}>
+            {cellSetName}
+          </div>
+        )) : <></>}
+      </Card>
+    </div>
   );
 };
 
