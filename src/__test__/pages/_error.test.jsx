db7672296265c0de019f7b06c10d6bd1d270e33f
Anugerah Erlaut <aerlaut@live.com>
2022-04-22 12:31:03 +0100

    fix _error page test

--- src/__test__/pages/_error.test.jsx
+++ src/__test__/pages/_error.test.jsx
@@ -1,20 +1,49 @@
+import React from 'react';
 import Error from 'pages/_error';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import postErrorToSlack from 'utils/postErrorToSlack';
 
 import createTestComponentFactory from '__test__/test-utils/testComponentFactory';
+import initialState from 'redux/reducers/cellSets/initialState';
+
+jest.mock('utils/postErrorToSlack');
+
+const mockStore = configureMockStore([thunk]);
+
+const mockErrorProp = {
+  errorObject: { message: 'Some error message' },
+};
+
+const storeState = {
+  // Added cellSets initial state to test for dumping of state
+  cellSets: initialState,
+  networkResources: { environment: 'test' },
+};
 
 const defaultProps = {};
 const renderErrorFactory = createTestComponentFactory(Error, defaultProps);
 
-const renderErrorPage = (newProps) => {
-  render(renderErrorFactory(newProps));
+const renderErrorPage = (newProps, store = storeState) => {
+  render(
+    <Provider store={mockStore(store)}>
+      {renderErrorFactory(newProps)}
+    </Provider>,
+  );
 };
 
 delete window.location;
 window.location = { reload: jest.fn() };
 
 describe('ErrorPage', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('Renders properly without props', () => {
     renderErrorPage();
 
@@ -43,5 +72,28 @@ describe('ErrorPage', () => {
     userEvent.click(screen.getByText(/Reload page/i));
 
     expect(window.location.reload).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should post error to Slack if environment is production', () => {
+    const productionState = {
+      ...storeState,
+      networkResources: { environment: 'production' },
+    };
+
+    renderErrorPage(mockErrorProp, productionState);
+
+    expect(postErrorToSlack).toHaveBeenCalledTimes(1);
+    expect(postErrorToSlack.mock.calls[0]).toMatchSnapshot();
+  });
+
+  it('Should not post error to Slack if environment is not production', () => {
+    const stagingState = {
+      ...storeState,
+      networkResources: { environment: 'staging' },
+    };
+
+    renderErrorPage(mockErrorProp, stagingState);
+
+    expect(postErrorToSlack).not.toHaveBeenCalled();
   });
 });
