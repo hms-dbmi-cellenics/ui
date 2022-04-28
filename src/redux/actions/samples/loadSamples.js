3e1f61e7fac6636c3fec50bdddfdc0013e5e5257
stefanbabukov <stefanbabukov98@gmail.com>
2022-04-28 10:29:37 +0100

    formatting function for v2 added

--- src/redux/actions/samples/loadSamples.js
+++ src/redux/actions/samples/loadSamples.js
@@ -1,5 +1,7 @@
 import fetchAPI from 'utils/http/fetchAPI';
 import handleError from 'utils/http/handleError';
+import config from 'config';
+import { api } from 'utils/constants';
 
 import {
   SAMPLES_LOADED,
@@ -7,16 +9,69 @@ import {
   SAMPLES_LOADING,
 } from 'redux/actionTypes/samples';
 
+const toApiV2 = (samples) => {
+  const apiV2Samples = [{ samples: {} }];
+
+  const buildApiv1FilesObject = (fileObject) => {
+    const fileNames = [];
+    const apiV1FileObject = {};
+    Object.keys(fileObject).forEach((key) => {
+      const fileName = fileObject[key].s3Path.split('/').pop();
+      fileNames.push(fileName);
+
+      apiV1FileObject[fileName] = {
+        path: fileObject[key].s3Path,
+        size: fileObject[key].size,
+        valid: fileObject[key].valid,
+        name: fileName,
+        upload: {
+          status: fileObject[key].uploadStatus,
+        },
+      };
+    });
+    return { apiV1FileObject, fileNames };
+  };
+  const sampleTechnologyConvert = (technology) => {
+    switch (technology) {
+      case '10x': return '10X Chromium';
+      default: return technology;
+    }
+  };
+
+  samples.data.message.forEach((sample) => {
+    const { apiV1FileObject, fileNames } = buildApiv1FilesObject(sample.files);
+    apiV2Samples[0].samples[sample.id] = {
+      metadata: sample.metadata,
+      createdDate: sample.createdAt,
+      name: sample.name,
+      lastModified: sample.updatedAt,
+      files: apiV1FileObject,
+      type: sampleTechnologyConvert(sample.sampleTechnology),
+      fileNames,
+      uuid: sample.id,
+    };
+  });
+  console.log('APIV2 SAMPLES', apiV2Samples);
+  return apiV2Samples;
+};
+
 const loadSamples = (
   experimentId = null, projectUuid = null,
 ) => async (dispatch) => {
-  const url = experimentId ? `/v1/experiments/${experimentId}/samples` : `/v1/projects/${projectUuid}/samples`;
+  const { currentApiVersion } = config;
+  const url = experimentId ? `/${currentApiVersion}/experiments/${experimentId}/samples`
+    : `/${currentApiVersion}/projects/${projectUuid}/samples`;
+
   try {
     dispatch({
       type: SAMPLES_LOADING,
     });
 
-    const data = await fetchAPI(url);
+    let data = await fetchAPI(url);
+
+    if (currentApiVersion === api.V2) {
+      data = toApiV2(data);
+    }
 
     let samples;
 
