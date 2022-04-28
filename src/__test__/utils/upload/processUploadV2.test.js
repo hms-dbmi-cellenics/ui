4d8d0c2109b06330222a082496e40fe1ce859b17
cosa65 <martin@biomage.net>
2022-04-28 10:49:27 -0300

    Add v2 tests

--- src/__test__/utils/upload/processUploadV2.test.js
+++ src/__test__/utils/upload/processUploadV2.test.js
@@ -18,47 +18,51 @@ jest.mock('config', () => ({ currentApiVersion: 'v2' }));
 
 enableFetchMocks();
 
-const validFilesList = [
-  {
-    name: 'WT13/features.tsv.gz',
-    fileObject: {
-      name: 'features.tsv.gz',
-      path: '/WT13/features.tsv.gz',
-      type: 'application/gzip',
-      size: 100,
+const getValidFiles = (cellrangerVersion) => {
+  const filename = cellrangerVersion === 'v2' ? 'genes.tsv.gz' : 'features.tsv.gz';
+
+  return ([
+    {
+      name: `WT13/${filename}`,
+      fileObject: {
+        name: filename,
+        path: `/WT13/${filename}`,
+        type: 'application/gzip',
+        size: 100,
+      },
+      upload: { status: UploadStatus.UPLOADING },
+      errors: '',
+      compressed: true,
+      valid: true,
     },
-    upload: { status: UploadStatus.UPLOADING },
-    errors: '',
-    compressed: true,
-    valid: true,
-  },
-  {
-    name: 'WT13/barcodes.tsv.gz',
-    fileObject: {
-      name: 'barcodes.tsv.gz',
-      path: '/WT13/barcodes.tsv.gz',
-      type: 'application/gzip',
-      size: 100,
+    {
+      name: 'WT13/barcodes.tsv.gz',
+      fileObject: {
+        name: 'barcodes.tsv.gz',
+        path: '/WT13/barcodes.tsv.gz',
+        type: 'application/gzip',
+        size: 100,
+      },
+      upload: { status: UploadStatus.UPLOADING },
+      errors: '',
+      compressed: true,
+      valid: true,
     },
-    upload: { status: UploadStatus.UPLOADING },
-    errors: '',
-    compressed: true,
-    valid: true,
-  },
-  {
-    name: 'WT13/matrix.mtx.gz',
-    fileObject: {
-      name: 'matrix.mtx.gz',
-      path: '/WT13/matrix.mtx.gz',
-      type: 'application/gzip',
-      size: 100,
+    {
+      name: 'WT13/matrix.mtx.gz',
+      fileObject: {
+        name: 'matrix.mtx.gz',
+        path: '/WT13/matrix.mtx.gz',
+        type: 'application/gzip',
+        size: 100,
+      },
+      upload: { status: UploadStatus.UPLOADING },
+      errors: '',
+      compressed: true,
+      valid: true,
     },
-    upload: { status: UploadStatus.UPLOADING },
-    errors: '',
-    compressed: true,
-    valid: true,
-  },
-];
+  ]);
+};
 
 const sampleType = '10X Chromium';
 const mockSampleUuid = 'sample-uuid';
@@ -147,7 +151,7 @@ describe('processUploadV2', () => {
     store = mockStore(initialState);
   });
 
-  it('Uploads and updates redux correctly when there are no errors', async () => {
+  it('Uploads and updates redux correctly when there are no errors with cellranger v3', async () => {
     const mockAxiosCalls = [];
     const uploadSuccess = (params) => {
       mockAxiosCalls.push(params);
@@ -159,7 +163,94 @@ describe('processUploadV2', () => {
       .mockImplementationOnce(uploadSuccess);
 
     await processUpload(
-      validFilesList,
+      getValidFiles('v3'),
+      sampleType,
+      store.getState().samples,
+      mockProjectUuid,
+      store.dispatch,
+    );
+
+    // Wait for uploads to be made
+    await waitForActions(
+      store,
+      new Array(3).fill({
+        type: SAMPLES_FILE_UPDATE,
+        payload: { fileDiff: { upload: { status: UploadStatus.UPLOADED } } },
+      }),
+      { matcher: waitForActions.matchers.containing },
+    );
+
+    // Three axios put calls are made
+    expect(mockAxiosCalls.length).toBe(3);
+    // Each put call is made with the correct information
+    expect(mockAxiosCalls[0].data).toEqual('loadedGzippedFile');
+    expect(mockAxiosCalls[1].data).toEqual('loadedGzippedFile');
+    expect(mockAxiosCalls[2].data).toEqual('loadedGzippedFile');
+
+    // Wait until all put promises are resolved
+    await flushPromises();
+
+    const fileUpdateActions = store.getActions().filter(
+      (action) => action.type === SAMPLES_FILE_UPDATE,
+    );
+    const uploadProperties = fileUpdateActions.map((action) => action.payload.fileDiff.upload);
+    const uploadingStatusProperties = uploadProperties.filter(
+      ({ status }) => status === UploadStatus.UPLOADING,
+    );
+    const uploadedStatusProperties = uploadProperties.filter(
+      ({ status }) => status === UploadStatus.UPLOADED,
+    );
+
+    // There are 3 files actions with status uploading
+    expect(uploadingStatusProperties.length).toEqual(3);
+    // There are 3 files actions with status uploaded
+    expect(uploadedStatusProperties.length).toEqual(3);
+
+    // Calls to loadAndCompressIfNecessary are done correctly
+    expect(loadAndCompressIfNecessary.mock.calls.map((calls) => calls[0])).toMatchSnapshot();
+
+    // If we trigger onCompression then an action to update uploadStatus to COMPRESSING is made
+    const onCompression = loadAndCompressIfNecessary.mock.calls[0][1];
+    onCompression();
+
+    await waitForActions(
+      store,
+      [{
+        type: SAMPLES_FILE_UPDATE,
+        payload: { fileDiff: { upload: { status: UploadStatus.COMPRESSING } } },
+      }],
+      { matcher: waitForActions.matchers.containing },
+    );
+
+    // axios request calls are correct
+    expect(axios.request.mock.calls.map((call) => call[0])).toMatchSnapshot();
+
+    // If we trigger axios onUploadProgress it updates the progress correctly
+    axios.request.mock.calls[0][0].onUploadProgress({ loaded: 20, total: 40 });
+
+    await waitForActions(
+      store,
+      [{
+        type: SAMPLES_FILE_UPDATE,
+        payload: { fileDiff: { upload: { status: UploadStatus.UPLOADING, progress: 50 } } },
+      }],
+      { matcher: waitForActions.matchers.containing },
+    );
+  });
+
+  it('Uploads and updates redux correctly when there are no errors with cellranger v2', async () => {
+    const mockAxiosCalls = [];
+    const uploadSuccess = (params) => {
+      mockAxiosCalls.push(params);
+      return Promise.resolve('Resolved');
+    };
+
+    axios.request.mockImplementationOnce(uploadSuccess)
+      .mockImplementationOnce(uploadSuccess)
+      .mockImplementationOnce(uploadSuccess);
+
+    await processUpload(
+      getValidFiles('v2'),
       sampleType,
       store.getState().samples,
       mockProjectUuid,
@@ -235,7 +326,7 @@ describe('processUploadV2', () => {
   });
 
   it('Updates redux correctly when there are file load and compress errors', async () => {
-    const invalidFiles = validFilesList.map((file) => ({ ...file, valid: false }));
+    const invalidFiles = getValidFiles('v3').map((file) => ({ ...file, valid: false }));
 
     await processUpload(
       invalidFiles,
@@ -291,7 +382,7 @@ describe('processUploadV2', () => {
       .mockImplementationOnce(uploadError);
 
     await processUpload(
-      validFilesList,
+      getValidFiles('v3'),
       sampleType,
       store.getState().samples,
       'errorProjectUuid',
@@ -334,11 +425,11 @@ describe('processUploadV2', () => {
     expect(uploadedFileProperties.length).toEqual(0);
   });
 
-  it('Should not upload files if there are errors creating samples in DynamoDB', async () => {
+  it('Should not upload files if there are errors creating samples in the api', async () => {
     fetchMock.mockReject(new Error('Error'));
 
     await processUpload(
-      validFilesList,
+      getValidFiles('v3'),
       sampleType,
       store.getState().samples,
       mockProjectUuid,
