88e2f4f29ea444ced42dad4dc997fd3310581ffc
cosa65 <martin@biomage.net>
2022-05-03 11:44:17 -0300

    Add test to project card for api v2

--- src/__test__/components/data-management/ProjectCard.test.jsx
+++ src/__test__/components/data-management/ProjectCard.test.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import moment from 'moment';
+
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+
+import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
 import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import moment from 'moment';
+
 import initialState, { projectTemplate } from 'redux/reducers/projects/initialState';
 import ProjectCard from 'components/data-management/ProjectCard';
 
+import config from 'config';
+import { api } from 'utils/constants';
+
+jest.mock('config');
+
 const projectUuid = '12345';
 const projectName = 'Test Project';
+const experimentId = 'experimentId1';
 const samplesIdsArray = new Array(13).fill(null).map((_, i) => (`sample-${i}`));
 const createdDate = moment().subtract(30, 'days').format();
 const lastModified = moment().subtract(30, 'minutes').format();
@@ -21,6 +32,7 @@ const projectState = {
     experiments: [],
     [projectUuid]: {
       ...projectTemplate,
+      experiments: [experimentId],
       name: projectName,
       uuid: projectUuid,
       samples: samplesIdsArray,
@@ -32,7 +44,16 @@ const projectState = {
 
 const mockStore = configureMockStore([thunk]);
 
+// Based on https://stackoverflow.com/a/51045733
+const flushPromises = () => new Promise(setImmediate);
+
 describe('ProjectCard', () => {
+  beforeEach(() => {
+    enableFetchMocks();
+    fetchMock.resetMocks();
+    fetchMock.doMock();
+  });
+
   it('Displays correctly', () => {
     render(
       <Provider store={mockStore(projectState)}>
@@ -66,5 +87,38 @@ describe('ProjectCard', () => {
     // Modal is shown
     const modal = await screen.findByText('Confirm delete');
     expect(modal).toBeInTheDocument();
+  });
+
+  it('Updates project name in api v2 when clicked', async () => {
+    config.currentApiVersion = api.V2;
+
+    render(
+      <Provider store={mockStore(projectState)}>
+        <ProjectCard projectUuid={projectUuid} />
+      </Provider>,
+    );
+
+    // Click edit project button
+    act(() => {
+      userEvent.click(screen.getByRole('button', { name: 'Edit' }));
+    });
+
+    // Write the new name
+    userEvent.clear(screen.getByTestId('editableFieldInput'));
+    userEvent.type(screen.getByTestId('editableFieldInput'), 'new project name');
+
+    // Click save changes button
+    userEvent.click(screen.getByRole('button', { name: 'Save' }));
+
+    await flushPromises();
+
+    expect(fetchMock).toHaveBeenCalledWith(
+      `http://localhost:3000/v2/experiments/${experimentId}`,
+      {
+        body: JSON.stringify({ name: 'new project name' }),
+        headers: { 'Content-Type': 'application/json' },
+        method: 'PATCH',
+      },
+    );
   });
 });
