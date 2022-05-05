163b150ef9d43cb1789e81d3d02abed0d4b9ae1e
Anugerah Erlaut <aerlaut@live.com>
2022-05-05 10:39:52 +0100

    add tests for load cellsets

--- src/__test__/redux/actions/cellSets/loadCellSets.test.js
+++ src/__test__/redux/actions/cellSets/loadCellSets.test.js
@@ -4,7 +4,12 @@ import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
 import loadCellSets from 'redux/actions/cellSets/loadCellSets';
 import initialState from 'redux/reducers/cellSets/initialState';
 
+import config from 'config';
+import { api } from 'utils/constants';
+
 import '__test__/test-utils/setupTests';
+
+jest.mock('config');
 
 enableFetchMocks();
 const mockStore = configureStore([thunk]);
@@ -12,7 +17,7 @@ const mockStore = configureStore([thunk]);
 describe('loadCellSets action', () => {
   const experimentId = '1234';
 
-  const cellSet = {
+  const cellSets = {
     name: 'one', color: '#ff0000', cellIds: new Set([1, 2, 3]),
   };
 
@@ -23,7 +28,9 @@ describe('loadCellSets action', () => {
   };
 
   beforeEach(() => {
-    const response = new Response(JSON.stringify({}));
+    const response = new Response(JSON.stringify({ cellSets }));
+
+    config.currentApiVersion = api.V1;
 
     fetchMock.resetMocks();
     fetchMock.doMock();
@@ -32,14 +39,14 @@ describe('loadCellSets action', () => {
 
   it('Does not dispatch on normal operation', async () => {
     const store = mockStore({ cellSets: { loading: false, error: false }, experimentSettings });
-    store.dispatch(loadCellSets(experimentId, cellSet.name, cellSet.color, cellSet.cellIds));
+    await store.dispatch(loadCellSets(experimentId));
     expect(store.getActions().length).toEqual(0);
   });
 
-  it('Dispatches on loading state', async () => {
-    const store = mockStore({ cellSets: { loading: true, error: false }, experimentSettings });
-    await store.dispatch(loadCellSets(experimentId));
-    expect(store.getActions().length).toBeGreaterThan(0);
+  it('Dispatches on force reload ', async () => {
+    const store = mockStore({ cellSets: { loading: false, error: false }, experimentSettings });
+    await store.dispatch(loadCellSets(experimentId, true));
+    expect(store.getActions().length).toEqual(1);
   });
 
   it('Dispatches on loading and error state', async () => {
@@ -50,7 +57,7 @@ describe('loadCellSets action', () => {
 
   it('Dispatches a loading action when run after an error condition.', async () => {
     const store = mockStore({ cellSets: { loading: false, error: true }, experimentSettings });
-    store.dispatch(loadCellSets(experimentId));
+    await store.dispatch(loadCellSets(experimentId));
 
     const firstAction = store.getActions()[0];
 
@@ -59,7 +66,7 @@ describe('loadCellSets action', () => {
 
   it('Dispatches a loaded action when run with the initial state.', async () => {
     const store = mockStore({ cellSets: initialState, experimentSettings });
-    store.dispatch(loadCellSets(experimentId));
+    await store.dispatch(loadCellSets(experimentId));
 
     const firstAction = store.getActions()[0];
 
@@ -72,9 +79,20 @@ describe('loadCellSets action', () => {
     fetchMock.resetMocks();
     fetchMock.mockReject(new Error('some weird error that happened'));
 
-    store.dispatch(loadCellSets(experimentId));
+    await store.dispatch(loadCellSets(experimentId));
 
     const firstAction = store.getActions()[0];
     expect(firstAction).toMatchSnapshot();
+  });
+
+  it('Uses V2 URL when using API version V2', async () => {
+    config.currentApiVersion = api.V2;
+
+    const store = mockStore({ cellSets: initialState, experimentSettings });
+    await store.dispatch(loadCellSets(experimentId));
+
+    const fetchUrl = fetchMock.mock.calls[0][0];
+
+    expect(fetchUrl).toMatch(/v2\/experiments\/.*\/cellSets/);
   });
 });
