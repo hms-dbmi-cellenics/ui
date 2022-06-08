dc177a9fb6f8e0b938e49d9dade30d5735273e70
cosa65 <martin@biomage.net>
2022-06-08 15:13:51 -0300

    Fix many samples action creators

--- src/__test__/redux/actions/samples/createSample.test.js
+++ src/__test__/redux/actions/samples/createSample.test.js
@@ -8,12 +8,12 @@ import createSample from 'redux/actions/samples/createSample';
 import initialSampleState from 'redux/reducers/samples/initialState';
 import initialProjectState, { projectTemplate } from 'redux/reducers/projects/initialState';
 import initialExperimentState, { experimentTemplate } from 'redux/reducers/experiments/initialState';
-import endUserMessages from 'utils/endUserMessages';
 
 import {
   SAMPLES_CREATE, SAMPLES_SAVING, SAMPLES_ERROR, SAMPLES_SAVED,
 } from 'redux/actionTypes/samples';
 
+import endUserMessages from 'utils/endUserMessages';
 import pushNotificationMessage from 'utils/pushNotificationMessage';
 
 import config from 'config';
@@ -77,57 +77,7 @@ describe('createSample action', () => {
     store = mockStore(initialState);
   });
 
-  it('Runs correctly', async () => {
-    fetchMock.mockResponse(JSON.stringify({}), { url: 'mockedUrl', status: 200 });
-    const newUuid = await store.dispatch(createSample(projectUuid, sampleName, mockType));
-
-    // Fetch call is made
-    const fetchMockFirstCall = fetchMock.mock.calls[0];
-
-    const { body: fetchBody, method: fetchMethod } = fetchMockFirstCall[1];
-    expect(fetchMockFirstCall[0]).toEqual(`http://localhost:3000/v2/projects/${projectUuid}/${mockProject.experiments[0]}/samples`);
-
-    expect(fetchMethod).toEqual('POST');
-    expect(JSON.parse(fetchBody)).toMatchSnapshot();
-
-    // Sends correct actions
-    const actions = store.getActions();
-
-    expect(actions[0].type).toEqual(SAMPLES_SAVING);
-    expect(actions[1].type).toEqual(SAMPLES_CREATE);
-
-    // Returns a new sampleUuid
-    expect(newUuid).toEqual(sampleUuid);
-  });
-
-  it('Shows error message and throws an error when there is a fetch error', async () => {
-    fetchMock.mockResponse(JSON.stringify({ message: 'error' }), { url: 'mockedUrl', status: 400 });
-
-    let newUuid;
-
-    // Fails with error message we sent in response to fetch
-    await expect(async () => {
-      newUuid = await store.dispatch(
-        createSample(projectUuid, sampleName, mockType),
-      );
-    }).rejects.toThrow(endUserMessages.ERROR_CREATING_SAMPLE);
-
-    // Sends correct actions
-    const actions = store.getActions();
-
-    expect(actions[0].type).toEqual(SAMPLES_SAVING);
-    expect(actions[1].type).toEqual(SAMPLES_ERROR);
-
-    // Check no other action was sent
-    expect(actions).toHaveLength(2);
-
-    expect(pushNotificationMessage).toHaveBeenCalledWith('error', endUserMessages.ERROR_CREATING_SAMPLE);
-
-    // It should not return a uuid
-    expect(newUuid).toBeUndefined();
-  });
-
-  it('Works correctly for api v2 with one file being uploaded', async () => {
+  it('Works correctly with one file being uploaded', async () => {
     config.currentApiVersion = api.V2;
     fetchMock.mockResponse(JSON.stringify({}), { url: 'mockedUrl', status: 200 });
 
@@ -151,7 +101,7 @@ describe('createSample action', () => {
     expect(_.map(actions, 'payload')).toMatchSnapshot();
   });
 
-  it('Works correctly for api v2 with many files being uploaded', async () => {
+  it('Works correctly with many files being uploaded', async () => {
     config.currentApiVersion = api.V2;
     fetchMock.mockResponse(JSON.stringify({}), { url: 'mockedUrl', status: 200 });
 
@@ -172,6 +122,23 @@ describe('createSample action', () => {
     // Sends correct actions
     const actions = store.getActions();
     expect(_.map(actions, 'type')).toEqual([SAMPLES_SAVING, SAMPLES_CREATE, SAMPLES_SAVED]);
+    expect(_.map(actions, 'payload')).toMatchSnapshot();
+  });
+
+  it('Throws if the api fails', async () => {
+    config.currentApiVersion = api.V2;
+
+    fetchMock.mockRejectOnce(() => Promise.reject(new Error('Some error')));
+
+    await expect(
+      store.dispatch(
+        createSample(projectUuid, sampleName, mockType, ['matrix.tsv.gz']),
+      ),
+    ).rejects.toThrow(endUserMessages.ERROR_CREATING_SAMPLE);
+
+    // Sends correct actions
+    const actions = store.getActions();
+    expect(_.map(actions, 'type')).toEqual([SAMPLES_SAVING, SAMPLES_ERROR]);
     expect(_.map(actions, 'payload')).toMatchSnapshot();
   });
 
