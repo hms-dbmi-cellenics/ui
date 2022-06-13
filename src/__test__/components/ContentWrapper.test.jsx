f405147ac02e547383078b2c6c4167b330d0b67f
cosa65 <martin@biomage.net>
2022-06-13 11:41:29 -0300

    Adapt unit tests

--- src/__test__/components/ContentWrapper.test.jsx
+++ src/__test__/components/ContentWrapper.test.jsx
@@ -189,7 +189,7 @@ describe('ContentWrapper', () => {
 
   it('has the correct sider and layout style when opened / closed', async () => {
     const siderHasWidth = (container, expectedWidth) => {
-      const div = container.firstChild;
+      const div = container.children[1];
 
       const [sidebar, content] = Array.from(div.children);
 
