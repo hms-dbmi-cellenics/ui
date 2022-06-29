27a209a129dbd31ef7ca24630d4f550449d1adfa
cosa65 <martin@biomage.net>
2022-06-29 12:25:15 -0300

    Update DownloadDataButton

--- src/redux/reducers/experiments/experimentsLoaded.js
+++ src/redux/reducers/experiments/experimentsLoaded.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-param-reassign */
 import produce from 'immer';
 
+import _ from 'lodash';
+
 const experimentsLoaded = produce((draft, action) => {
   const { experiments } = action.payload;
 
@@ -13,8 +15,14 @@ const experimentsLoaded = produce((draft, action) => {
   // if (!Object.keys(state).includes(newActiveExperimentId)) {
   //   newActiveExperimentId = experiments[0]?.id;
   // }
+
+  // WIP would be better to store experiments inside a special property for it
+  // instead of having to store the ids apart to differentiate experiment entries from meta
+  const ids = _.map(experiments, 'id');
+
   draft.meta.activeExperimentId = experiments[0].id;
   draft.meta.loading = false;
+  draft.ids = ids;
 
   experiments.forEach((experiment) => {
     // WIP, deal with this before emrging
