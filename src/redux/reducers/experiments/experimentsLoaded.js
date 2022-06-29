1ad647c5b42b04aedacd3048020f23f6768bbe37
cosa65 <martin@biomage.net>
2022-06-29 15:06:00 -0300

    Add the activeExperimentId selection in again

--- src/redux/reducers/experiments/experimentsLoaded.js
+++ src/redux/reducers/experiments/experimentsLoaded.js
@@ -1,20 +1,19 @@
 /* eslint-disable no-param-reassign */
-import produce from 'immer';
+import produce, { original } from 'immer';
 
 import _ from 'lodash';
 
 const experimentsLoaded = produce((draft, action) => {
   const { experiments } = action.payload;
 
-  console.log('experimentsDebug');
-  console.log(experiments);
+  const originalState = original(draft);
 
-  // const newActiveExperimentId = state.meta.activeExperimentId;
+  let newActiveExperimentId = originalState.meta.activeExperimentId;
 
-  // // If the current active experiment no longer exists, change it
-  // if (!Object.keys(state).includes(newActiveExperimentId)) {
-  //   newActiveExperimentId = experiments[0]?.id;
-  // }
+  // If the current active experiment no longer exists, change it
+  if (!Object.keys(originalState).includes(newActiveExperimentId)) {
+    newActiveExperimentId = experiments[0]?.id;
+  }
 
   // WIP would be better to store experiments inside a special property for it
   // instead of having to store the ids apart to differentiate experiment entries from meta
