ffa33659b333eb73300e4f1d3c40f7df9a2d67c1
cosa65 <martin@biomage.net>
2022-06-30 15:38:13 -0300

    Fix NotifyByEmail

--- src/__test__/components/NotifyByEmail.test.jsx
+++ src/__test__/components/NotifyByEmail.test.jsx
@@ -9,11 +9,8 @@ import { Provider } from 'react-redux';
 import userEvent from '@testing-library/user-event';
 import * as updateExperiment from 'redux/actions/experiments/updateExperiment';
 import * as loadExperiments from 'redux/actions/experiments/loadExperiments';
-import * as loadExperiment from 'redux/actions/experiments/loadExperiment';
 import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
-import mockAPI, {
-  generateDefaultMockAPIResponses,
-} from '__test__/test-utils/mockAPI';
+
 import { makeStore } from 'redux/store';
 import fake from '__test__/test-utils/constants';
 
@@ -24,13 +21,11 @@ const experimentId = fake.EXPERIMENT_ID;
 describe('Notify by email component', () => {
   let updateExperimentSpy;
   let loadExperimentsSpy;
-  let loadExperimentSpy;
 
   beforeEach(() => {
     jest.clearAllMocks();
     updateExperimentSpy = jest.spyOn(updateExperiment, 'default');
     loadExperimentsSpy = jest.spyOn(loadExperiments, 'default');
-    loadExperimentSpy = jest.spyOn(loadExperiment, 'default');
     enableFetchMocks();
     fetchMock.resetMocks();
     fetchMock.doMock();
@@ -45,11 +40,6 @@ describe('Notify by email component', () => {
       </Provider>,
     )));
   };
-  it('loads experiments if  there is an activeProjectUuid', async () => {
-    fetchMock.mockIf(/.*/, mockAPI(generateDefaultMockAPIResponses(experimentId, fake.PROJECT_ID)));
-    await renderNotifyByEmail();
-    expect(loadExperimentSpy).toHaveBeenCalledTimes(1);
-  });
 
   it('Renders Correctly', async () => {
     await renderNotifyByEmail();
