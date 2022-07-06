d496ea1d8c1ff776dc326a9533c3adbd4bf967cb
stefanbabukov <stefanbabukov98@gmail.com>
2022-07-06 11:21:47 +0300

    testing if env variables are defined

--- src/utils/getAWSRegion.js
+++ src/utils/getAWSRegion.js
@@ -1,6 +1,15 @@
-const getAWSRegion = () => {
-  const region = process.env.AWS_REGION || process.env.DEFAULT_REGION || 'eu-west-1';
-  return region;
+const getAWSRegion = (env) => {
+  if (env === 'development') {
+    return 'eu-west-1';
+  }
+
+  const { AWS_REGION, DEFAULT_REGION } = process.env;
+
+  if (!AWS_REGION && !DEFAULT_REGION) {
+    throw new Error('AWS_REGION or DEFAULT_REGION must be set in the environment.');
+  }
+
+  return AWS_REGION || DEFAULT_REGION;
 };
 
 export default getAWSRegion;
--- src/utils/ssr/getEnvironmentInfo.js
+++ src/utils/ssr/getEnvironmentInfo.js
@@ -1,5 +1,6 @@
 import loadEnvironment from 'redux/actions/networkResources/loadEnvironment';
 import { ssrGetCurrentEnvironment } from 'utils/environment';
+import getAWSRegion from 'utils/getAWSRegion';
 
 const getAuthenticationInfo = async (context, store) => {
   if (
@@ -9,7 +10,10 @@ const getAuthenticationInfo = async (context, store) => {
   }
 
   const env = ssrGetCurrentEnvironment();
+  const region = getAWSRegion(env);
+  const { DOMAIN_NAME, AWS_REGION, DEFAULT_REGION } = process.env;
 
+  console.log('ENV IS ', AWS_REGION, process.env.AWS_DEFAULT_REGION, DOMAIN_NAME, DEFAULT_REGION);
   store.dispatch(loadEnvironment(env));
 
   return {};
