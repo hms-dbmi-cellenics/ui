0b365d510a433d4e66dd5c73c1d4d5047382ab7a
cosa65 <martin@biomage.net>
2022-07-18 15:49:44 -0300

    WIP

--- src/components/header/HelpButton.jsx
+++ src/components/header/HelpButton.jsx
@@ -3,18 +3,30 @@ import { Button, Dropdown, Card } from 'antd';
 import { QuestionCircleOutlined, DownOutlined } from '@ant-design/icons';
 
 import config from 'config';
+import { DomainName } from 'utils/deploymentInfo';
+
+// const getStartedLinkIfNotHMS = () => {
+//   if (process.env.DOMAIN_NAME !== DomainName.HMS) {
+//     return ()
+//   }
+// };
 
 const HelpButton = () => {
   const [visible, setVisible] = useState(false);
 
   const overlay = () => (
     <Card size='small' style={{ padding: '1em', width: '265px' }}>
-      For tutorial videos, ‘how to’ guides and FAQs on how to use Cellenics, visit
-      {' '}
-      <a href='https://www.biomage.net/get-started' target='_blank' rel='noreferrer'>our website</a>
-      .
-      <br />
-      <br />
+      {process.env.DOMAIN_NAME !== DomainName.HMS ?? (
+        <>
+          For tutorial videos, ‘how to’ guides and FAQs on how to use Cellenics, visit
+          {' '}
+          <a href='https://www.biomage.net/get-started' target='_blank' rel='noreferrer'>our website</a>
+          .
+          <br />
+          <br />
+        </>
+      )}
+
       If you need additional help with your analysis, email:
       {' '}
       <a href={`mailto:${config.supportEmail}`}>{config.supportEmail}</a>
