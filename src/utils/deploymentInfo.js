79f774950ab640d5f4ff80f6b01b63a64e9bc114
cosa65 <martin@biomage.net>
2022-07-19 11:41:26 -0300

    Fill in correct hms domain name

--- src/utils/deploymentInfo.js
+++ src/utils/deploymentInfo.js
@@ -1,7 +1,7 @@
 const DomainName = {
   BIOMAGE: 'scp.biomage.net',
   BIOMAGE_STAGING: 'scp-staging.biomage.net',
-  HMS: '???@??.net',
+  HMS: 'cellenics.hms.harvard.edu',
 };
 
 // eslint-disable-next-line import/prefer-default-export
