4420bb46b4f8b303ac8191fd0d4d11e09267d5fa
cosa65 <martin@biomage.net>
2022-07-19 18:49:07 -0300

    Make experimentsMetadataRename reducer in experiments slice not change the position of the renamed track

--- src/redux/reducers/experiments/experimentsMetadataRename.js
+++ src/redux/reducers/experiments/experimentsMetadataRename.js
@@ -1,14 +1,16 @@
 const experimentsMetadataRename = (state, action) => {
   const { oldKey, newKey, experimentId } = action.payload;
 
+  const { metadataKeys } = state[experimentId];
+  const newMetadataKeys = metadataKeys.map((metadataKey) => (
+    metadataKey === oldKey ? newKey : metadataKey
+  ));
+
   return {
     ...state,
     [experimentId]: {
       ...state[experimentId],
-      metadataKeys: [
-        ...state[experimentId].metadataKeys.filter((value) => value !== oldKey),
-        newKey,
-      ],
+      metadataKeys: newMetadataKeys,
     },
   };
 };
