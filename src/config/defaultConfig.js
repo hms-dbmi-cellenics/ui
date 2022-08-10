a8387dbe33cdb6b75984abb319a1df5855333580
Alex Pickering <alexvpickering@gmail.com>
2022-08-10 16:32:11 -0700

    add HMS slack tokens

--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -9,8 +9,20 @@ const supportEmailsByAccountId = {
   [AccountId.HMS]: 'alex_pickering@hms.harvard.edu',
 };
 
+const botTokensByAccountId = {
+  [AccountId.BIOMAGE]: 'eG94Yi0xMTc1NDc1NTM5NzE0LTMyMjE3MTYwNDg4OTgtUWFXNmlwTEVSY1RJRXQycVU2RnJ3RGp1', // pragma: allowlist secret
+  [AccountId.HMS]: 'eG94Yi0zODE4MjQwMTYwODY5LTM4Mjk3Njc0OTQ4MDQtbDJtTHF4TTNpZFRRQXZYQUNJTXdFbDBx', // pragma: allowlist secret
+};
+
+const webhookEndpointsByAccountId = {
+  [AccountId.BIOMAGE]: 'aHR0cHMlM0ElMkYlMkZob29rcy5zbGFjay5jb20lMkZzZXJ2aWNlcyUyRlQwMTU1RFpGVk0wJTJGQjAyS0ZESEc0SkslMkZFY3ZZZVhQNVBoM0lQMjE0ZlNTRWhGT3c=', // pragma: allowlist secret
+  [AccountId.HMS]: 'aHR0cHM6Ly9ob29rcy5zbGFjay5jb20vc2VydmljZXMvVDAzUTI3MjRRUksvQjAzUThCR0JLSEIvUk41elZtU1RudXUyMWxURUh1a3I3ajdJ', // pragma: allowlist secret
+};
+
 const config = {
   supportEmail: supportEmailsByAccountId[accountId],
+  botToken: botTokensByAccountId[accountId],
+  webhookEndpoint: webhookEndpointsByAccountId[accountId],
 };
 
 export default config;
--- src/utils/slack.js
+++ src/utils/slack.js
@@ -1,14 +1,16 @@
+import config from 'config';
+
 const decrypt = (hash) => decodeURIComponent(window.atob(hash));
 
 const getWebhookUrl = () => {
-  const webhookEndpoint = 'aHR0cHMlM0ElMkYlMkZob29rcy5zbGFjay5jb20lMkZzZXJ2aWNlcyUyRlQwMTU1RFpGVk0wJTJGQjAyS0ZESEc0SkslMkZFY3ZZZVhQNVBoM0lQMjE0ZlNTRWhGT3c='; // pragma: allowlist secret
+  const { webhookEndpoint } = config;
   return decrypt(webhookEndpoint);
 };
 
 // Token for Logger bot to post errors as file
 // Required becuase webhook doesn't suport file uploads
 const getLoggerBotToken = () => {
-  const botToken = 'eG94Yi0xMTc1NDc1NTM5NzE0LTMyMjE3MTYwNDg4OTgtUWFXNmlwTEVSY1RJRXQycVU2RnJ3RGp1'; // pragma: allowlist secret
+  const { botToken } = config;
   return decrypt(botToken);
 };
 
