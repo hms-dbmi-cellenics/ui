1ffe3091a7e5b8775db65430b6485bdb56b39e17
stefanbabukov <stefanbabukov98@gmail.com>
2022-08-15 13:11:10 +0300

    frequency plot legend fix

--- src/utils/plotSpecs/generateFrequencySpec.js
+++ src/utils/plotSpecs/generateFrequencySpec.js
@@ -4,16 +4,14 @@ import { intersection } from '../cellSetOperations';
 
 const generateSpec = (config, plotData, xNamesToDisplay, yNamesToDisplay) => {
   let legend = [];
-
   if (config.legend.enabled) {
     const positionIsRight = config.legend.position === 'right';
 
     const legendColumns = positionIsRight ? 1 : Math.floor(config.dimensions.width / 85);
     const labelLimit = positionIsRight ? 0 : 85;
-
     legend = [
       {
-        fill: 'color',
+        fill: 'cellSetColors',
         title: 'Cell Set',
         titleColor: config.colour.masterColour,
         type: 'symbol',
@@ -97,9 +95,9 @@ const generateSpec = (config, plotData, xNamesToDisplay, yNamesToDisplay) => {
         range: yNamesToDisplay,
       },
       {
-        name: 'color',
+        name: 'cellSetColors',
         type: 'ordinal',
-        range: { data: 'plotData', field: 'color' },
+        range: plotData.map(({ color }) => color),
         domain: { data: 'plotData', field: 'yCellSetKey' },
       },
     ],
@@ -162,7 +160,7 @@ const generateSpec = (config, plotData, xNamesToDisplay, yNamesToDisplay) => {
             width: { scale: 'x', band: 1, offset: -1 },
             y: { scale: 'y', field: 'y0' },
             y2: { scale: 'y', field: 'y1' },
-            fill: { scale: 'color', field: 'yCellSetKey' },
+            fill: { scale: 'cellSetColors', field: 'yCellSetKey' },
           },
           update: {
             fillOpacity: 1,
