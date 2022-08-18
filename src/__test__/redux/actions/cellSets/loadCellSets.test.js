958b0d06c192762ad4de442f5194fdec379230ce
stefanbabukov <stefanbabukov98@gmail.com>
2022-08-18 15:04:54 +0300

    fix more tests

--- src/__test__/redux/actions/cellSets/loadCellSets.test.js
+++ src/__test__/redux/actions/cellSets/loadCellSets.test.js
@@ -15,9 +15,84 @@ const mockStore = configureStore([thunk]);
 describe('loadCellSets action', () => {
   const experimentId = '1234';
 
-  const cellSets = {
-    name: 'one', color: '#ff0000', cellIds: new Set([1, 2, 3]),
-  };
+  const cellSets = [
+    {
+      key: 'louvain',
+      name: 'louvain clusters',
+      rootNode: true,
+      type: 'cellSets',
+      children: [
+        {
+          key: 'louvain-1',
+          name: 'Cluster 1',
+          rootNode: false,
+          type: 'cellSets',
+          color: '#ee8866',
+          cellIds: [5, 18, 33, 55, 77],
+        },
+        {
+          key: 'louvain-0',
+          name: 'Cluster 0',
+          rootNode: false,
+          type: 'cellSets',
+          color: '#000000',
+          cellIds: [11, 23, 27, 48, 61],
+        },
+        {
+          key: 'louvain-3',
+          name: 'Cluster 3',
+          rootNode: false,
+          type: 'cellSets',
+          color: '#ffaabb',
+          cellIds: [68, 112, 122, 132, 189],
+        },
+        {
+          key: 'louvain-2',
+          name: 'Cluster 2',
+          rootNode: false,
+          type: 'cellSets',
+          color: '#eedd88',
+          cellIds: [3, 34, 42, 49, 71],
+        },
+      ],
+    },
+    {
+      key: 'scratchpad',
+      name: 'Custom cell sets',
+      rootNode: true,
+      children: [
+        {
+          key: '8297d513-6268-4ab0-a03e-e74a23ecec07',
+          name: 'New Cluster',
+          color: '#3957ff',
+          type: 'cellSets',
+          cellIds: [3103, 449, 4023, 2545, 930],
+        },
+        {
+          key: '99a7746b-7f9d-4e54-8acf-53031e4ff023',
+          name: 'New Cluster2',
+          color: '#d3fe14',
+          type: 'cellSets',
+          cellIds: [6536, 3654, 1075, 1538, 193],
+        },
+      ],
+      type: 'cellSets',
+    },
+    {
+      key: 'sample',
+      name: 'Samples',
+      rootNode: true,
+      children: [
+        {
+          key: 'b15fa0d6-74e8-49da-bcbb-aa07127960d0',
+          name: 'Convalescent',
+          color: '#77aadd',
+          cellIds: [0, 1, 2, 4, 5, 7],
+        },
+      ],
+      type: 'metadataCategorical',
+    },
+  ];
 
   const experimentSettings = {
     info: {
