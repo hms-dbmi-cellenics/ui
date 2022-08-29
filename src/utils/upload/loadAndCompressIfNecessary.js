b06b1132e4ec8e174ed61daef75f062a712c050d
Anugerah Erlaut <aerlaut@live.com>
2022-08-29 16:28:20 +0100

    add test to cover spec

--- src/utils/upload/loadAndCompressIfNecessary.js
+++ src/utils/upload/loadAndCompressIfNecessary.js
@@ -1,10 +1,10 @@
 import { gzip } from 'fflate';
-import readFileToBuffer from './readFileToBuffer';
+import readFileToBuffer from 'utils/upload/readFileToBuffer';
 
 const compress = (buffer) => new Promise((resolve, reject) => {
   gzip(buffer, {}, (error, compressed) => {
     if (error) {
-      reject(new Error('error'));
+      reject(new Error(error));
     }
     resolve(Buffer.from((compressed)));
   });
