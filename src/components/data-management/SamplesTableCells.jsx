cc85f3149a5e0b7b079981435a6c94ed5894ca0d
stefanbabukov <stefanbabukov98@gmail.com>
2022-09-19 16:20:08 +0100

    empty commit but new line test

--- src/components/data-management/SamplesTableCells.jsx
+++ src/components/data-management/SamplesTableCells.jsx
@@ -35,6 +35,7 @@ const UploadCell = (props) => {
     file,
   } = tableCellData;
   const { progress = null, status = null } = file?.upload ?? {};
+
   const [uploadDetailsModalVisible, setUploadDetailsModalVisible] = useState(false);
   const uploadDetailsModalDataRef = useRef(null);
 
