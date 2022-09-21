d3cbe2f41eb838653ff19713d96e5388c12b1a99
cosa65 <martin@biomage.net>
2022-09-21 14:21:59 -0300

    Some tests

--- src/__test__/utils/ExpressionMatrix/ExpressionMatrix.test.js
+++ src/__test__/utils/ExpressionMatrix/ExpressionMatrix.test.js
@@ -60,4 +60,55 @@ describe('ExpressionMatrix', () => {
       expect(matrix).toMatchSnapshot();
     });
   });
+
+  describe('Filled matrix', () => {
+    beforeEach(() => {
+      matrix = new ExpressionMatrix();
+      const {
+        order, rawExpression, truncatedExpression, stats,
+      } = getTwoGenesMatrix();
+
+      matrix.setGeneExpression(order, rawExpression, truncatedExpression, stats);
+    });
+
+    it('getRawExpression on existing gene works', () => {
+      expect(matrix.getRawExpression('Lyz2')).toEqual([2, 0, 5]);
+    });
+
+    it('getRawExpression on non existing gene works', () => {
+      expect(matrix.getRawExpression('NotExist')).toBeUndefined();
+    });
+
+    it('getTruncatedExpression works', () => {
+      expect(matrix.getTruncatedExpression('Gzma')).toEqual([1, 0, 0]);
+    });
+
+    it('getStats works', () => {
+      expect(matrix.getStats('Gzma')).toEqual(getTwoGenesMatrix().stats.Gzma);
+    });
+
+    it('geneIsLoaded on existing gene returns true', () => {
+      expect(matrix.geneIsLoaded('Lyz2')).toEqual(true);
+    });
+
+    it('geneIsLoaded on non existing gene returns false', () => {
+      expect(matrix.geneIsLoaded('IdontExist')).toEqual(false);
+    });
+
+    it('genesAreLoaded on existing genes returns true', () => {
+      expect(matrix.genesAreLoaded(['Lyz2', 'Gzma'])).toEqual(true);
+    });
+
+    it('genesAreLoaded on a mix returns false', () => {
+      expect(matrix.genesAreLoaded(['Lyz2', 'IdontExist', 'Gzma'])).toEqual(false);
+    });
+
+    it('getStoredGenes works', () => { });
+
+    it('setGeneExpression works', () => { });
+
+    it('pushGeneExpression works', () => { });
+
+    it('generateIndexFor works', () => { });
+  });
 });
