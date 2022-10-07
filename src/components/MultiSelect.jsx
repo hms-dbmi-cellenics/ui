420db3edc14a2465cb0d20e2dc1edf31b472dd22
cosa65 <martin@biomage.net>
2022-10-07 11:28:20 -0300

    Add onChange to MultiSelect props

--- src/components/MultiSelect.jsx
+++ src/components/MultiSelect.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Select } from 'antd';
 import PropTypes from 'prop-types';
 
 const MultiSelect = (props) => {
-  const { items } = props;
+  const { items, onChange } = props;
 
   const [selectedItems, setSelectedItems] = useState([]);
+
+  useEffect(() => {
+    onChange(selectedItems);
+  }, [selectedItems]);
 
   const filteredItems = items.filter((o) => !selectedItems.includes(o));
 
@@ -23,6 +27,11 @@ const MultiSelect = (props) => {
 
 MultiSelect.propTypes = {
   items: PropTypes.array.isRequired,
+  onChange: PropTypes.func,
+};
+
+MultiSelect.defaultProps = {
+  onChange: () => { },
 };
 
 export default MultiSelect;
