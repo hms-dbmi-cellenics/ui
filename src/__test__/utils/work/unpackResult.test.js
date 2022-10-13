3cb4a3e74b7ffd6786bc9d4bbb5e21028a9eba9d
cosa65 <martin@biomage.net>
2022-10-13 15:55:52 -0300

    Fix unpackResult and add new tests

--- src/__test__/utils/work/unpackResult.test.js
+++ src/__test__/utils/work/unpackResult.test.js
@@ -14,7 +14,7 @@ jest.mock('uint8array-json-parser', () => ({
   JSON_parse: jest.fn(),
 }));
 
-describe('unpackResult', () => {
+describe('unpackResult with json result', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
@@ -33,7 +33,7 @@ describe('unpackResult', () => {
 
     JSON_parse.mockImplementation(() => decompressedObject);
 
-    const result = await unpackResult(storageResp);
+    const result = await unpackResult(storageResp, true);
 
     expect(decompress).toHaveBeenCalledTimes(1);
     expect(JSON_parse).toHaveBeenCalledWith(decompressedUint8);
@@ -50,6 +50,43 @@ describe('unpackResult', () => {
       callback('someError', null);
     });
 
-    await expect(unpackResult(storageResp)).rejects.toEqual('someError');
+    await expect(unpackResult(storageResp, true)).rejects.toEqual('someError');
+  });
+});
+
+describe('unpackResult with non-json result', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('works correctly', async () => {
+    const storageArrayBuffer = new ArrayBuffer();
+
+    const storageResp = { arrayBuffer: () => Promise.resolve(storageArrayBuffer) };
+
+    const decompressedUint8 = new Uint8Array();
+
+    decompress.mockImplementation((aUint8array, callback) => {
+      callback(null, decompressedUint8);
+    });
+
+    const result = await unpackResult(storageResp, false);
+
+    expect(decompress).toHaveBeenCalledTimes(1);
+    expect(JSON_parse).not.toHaveBeenCalled();
+
+    expect(result).toEqual(decompressedUint8);
+  });
+
+  it('rejects if decompress fails', async () => {
+    const storageArrayBuffer = new ArrayBuffer();
+
+    const storageResp = { arrayBuffer: () => Promise.resolve(storageArrayBuffer) };
+
+    decompress.mockImplementation((aUint8array, callback) => {
+      callback('someError', null);
+    });
+
+    await expect(unpackResult(storageResp, true)).rejects.toEqual('someError');
   });
 });
