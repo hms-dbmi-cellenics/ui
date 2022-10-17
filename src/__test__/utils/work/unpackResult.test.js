b73feef169f3caeffa5d106b7cf94db4a8949e6c
cosa65 <martin@biomage.net>
2022-10-17 14:47:12 -0300

    Fix unpackResult tests and add parseResult tests

--- src/__test__/utils/work/unpackResult.test.js
+++ src/__test__/utils/work/unpackResult.test.js
@@ -1,17 +1,10 @@
 import { decompress } from 'fflate';
-// eslint-disable-next-line camelcase
-import { JSON_parse } from 'uint8array-json-parser';
 
 import unpackResult from 'utils/work/unpackResult';
 
 jest.mock('fflate', () => ({
   __esModule: true, // this property makes it work
   decompress: jest.fn(),
-}));
-
-jest.mock('uint8array-json-parser', () => ({
-  __esModule: true, // this property makes it work
-  JSON_parse: jest.fn(),
 }));
 
 describe('unpackResult with json result', () => {
@@ -25,55 +18,14 @@ describe('unpackResult with json result', () => {
     const storageResp = { arrayBuffer: () => Promise.resolve(storageArrayBuffer) };
 
     const decompressedUint8 = new Uint8Array();
-    const decompressedObject = { hi: 'bye' };
 
     decompress.mockImplementation((aUint8array, callback) => {
       callback(null, decompressedUint8);
     });
 
-    JSON_parse.mockImplementation(() => decompressedObject);
-
-    const result = await unpackResult(storageResp, true);
+    const result = await unpackResult(storageResp);
 
     expect(decompress).toHaveBeenCalledTimes(1);
-    expect(JSON_parse).toHaveBeenCalledWith(decompressedUint8);
-
-    expect(result).toEqual(decompressedObject);
-  });
-
-  it('rejects if decompress fails', async () => {
-    const storageArrayBuffer = new ArrayBuffer();
-
-    const storageResp = { arrayBuffer: () => Promise.resolve(storageArrayBuffer) };
-
-    decompress.mockImplementation((aUint8array, callback) => {
-      callback('someError', null);
-    });
-
-    await expect(unpackResult(storageResp, true)).rejects.toEqual('someError');
-  });
-});
-
-describe('unpackResult with non-json result', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it('works correctly', async () => {
-    const storageArrayBuffer = new ArrayBuffer();
-
-    const storageResp = { arrayBuffer: () => Promise.resolve(storageArrayBuffer) };
-
-    const decompressedUint8 = new Uint8Array();
-
-    decompress.mockImplementation((aUint8array, callback) => {
-      callback(null, decompressedUint8);
-    });
-
-    const result = await unpackResult(storageResp, false);
-
-    expect(decompress).toHaveBeenCalledTimes(1);
-    expect(JSON_parse).not.toHaveBeenCalled();
 
     expect(result).toEqual(decompressedUint8);
   });
@@ -87,6 +39,6 @@ describe('unpackResult with non-json result', () => {
       callback('someError', null);
     });
 
-    await expect(unpackResult(storageResp, true)).rejects.toEqual('someError');
+    await expect(unpackResult(storageResp)).rejects.toEqual('someError');
   });
 });
