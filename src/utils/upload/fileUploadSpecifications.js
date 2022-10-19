11030321bc3dacc8ecfc08cf5c670da40c5eeb73
Anugerah Erlaut <aerlaut@live.com>
2022-10-19 10:09:19 +0100

    fix filename validation

--- src/utils/upload/fileUploadSpecifications.js
+++ src/utils/upload/fileUploadSpecifications.js
@@ -21,18 +21,18 @@ const fileUploadSpecifications = {
       { key: 'features.tsv.gz', displayedName: 'genes.tsv' },
       { key: 'matrix.mtx.gz', displayedName: 'matrix.mtx' },
     ],
-    info: ` For each sample, upload a folder containing the 3 count matrix files. The 
+    info: ` For each sample, upload a folder containing the 3 count matrix files. The
     folder's name will be used to name the sample in it. You can change this name later in Data Management.`,
-    fileNameFilter: () => true,
+    fileNameFilter(fileName) { return this.acceptedFiles.has(fileName); },
   },
   'BD Rhapsody': {
-    acceptedFiles: new Set(['expression_data.st']),
+    acceptedFiles: new Set(['expression_data.st', 'expression_data.st.gz']),
     requiredFiles: [{ key: 'expression_data.st.gz', displayedName: 'expression_data.st' }],
-    inputInfo: [['expression_data.st']],
+    inputInfo: [['expression_data.st', 'expression_data.st.gz']],
     info: `For each sample, upload a folder containing the required file. The folder's
     name will be used to name the sample in it.
     You can change this name later in Data Management.`,
-    fileNameFilter: (fileName) => fileName.endsWith('.st'),
+    fileNameFilter: (fileName) => fileName.toLowerCase().match(/.*expression_data.st(.gz)?$/),
   },
 };
 
