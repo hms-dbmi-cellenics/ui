af2636d420ec41cd13361fe8760833832ef05a61
Anugerah Erlaut <aerlaut@live.com>
2022-10-24 16:23:51 +0100

    revert from using immer as it freeze object state

--- src/redux/reducers/samples/samplesOptionsUpdate.js
+++ src/redux/reducers/samples/samplesOptionsUpdate.js
@@ -1,18 +1,26 @@
-/* eslint-disable no-param-reassign */
-import produce from 'immer';
-
-import _ from 'lodash';
-import initialState from './initialState';
-
-const samplesOptionsUpdate = produce((draft, action) => {
+const samplesOptionsUpdate = (state, action) => {
   const { sampleUuids, diff } = action.payload;
 
-  sampleUuids.forEach((sampleUuid) => {
-    const oldOption = draft[sampleUuid].options;
-    draft[sampleUuid].options = _.merge(oldOption, diff);
-  });
+  const newSamples = sampleUuids.reduce((acc, sampleUuid) => {
+    acc[sampleUuid] = {
+      ...state[sampleUuid],
+      options: {
+        ...state[sampleUuid].options,
+        ...diff,
+      },
+    };
 
-  draft.meta.saving = false;
-}, initialState);
+    return acc;
+  }, {});
+
+  return {
+    ...state,
+    ...newSamples,
+    meta: {
+      ...state.meta,
+      saving: false,
+    },
+  };
+};
 
 export default samplesOptionsUpdate;
