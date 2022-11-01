128711d655336eb7d5caff479b3ccd5a6111123d
cosa65 <martin@biomage.net>
2022-11-01 13:45:52 -0300

    Update tests

--- src/__test__/utils/work/generateETag.test.js
+++ src/__test__/utils/work/generateETag.test.js
@@ -3,8 +3,11 @@ import { Environment } from 'utils/deploymentInfo';
 
 import { mockQcPipelineStartDate } from '__test__/utils/work/fetchWork.mock';
 import processingConfigData from '__test__/data/processing_config.json';
+import { makeStore } from 'redux/store';
+import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
+import mockAPI, { generateDefaultMockAPIResponses } from '__test__/test-utils/mockAPI';
 
-const NON_GENE_EXPRESSION_ETAG = 'f0eb883ef077c296b9ca0922af1a9cbe'; // pragma: allowlist secret
+const NON_GENE_EXPRESSION_ETAG = '1b30b8f01bd64667c75db47cd2328b6f'; // pragma: allowlist secret
 const GENE_EXPRESSION_D_ETAG = '78cf13c1fb1596e64f07fa93d6d028a9'; // pragma: allowlist secret
 
 const experimentId = '1234';
@@ -16,34 +19,47 @@ const nonGeneExpressionWorkRequest = {
 
 const { clusteringSettings } = processingConfigData.processingConfig.configureEmbedding;
 
+enableFetchMocks();
+
 describe('generateEtag', () => {
+  let store;
+
   beforeEach(() => {
     jest.resetAllMocks();
 
     global.Math.random = jest.fn(() => 1);
+
+    fetchMock.resetMocks();
+    fetchMock.mockIf(/.*/, mockAPI(generateDefaultMockAPIResponses(experimentId)));
+
+    store = makeStore();
   });
 
   it('Generates the correct ETag', async () => {
-    const ETag = generateETag(
+    const ETag = await generateETag(
       experimentId,
       nonGeneExpressionWorkRequest,
       mockExtras,
       mockQcPipelineStartDate,
       Environment.PRODUCTION,
       clusteringSettings,
+      store.dispatch,
+      store.getState,
     );
 
     expect(ETag).toEqual(NON_GENE_EXPRESSION_ETAG);
   });
 
   it('Generates the correct geneExpression ETag', async () => {
-    const ETag = generateETag(
+    const ETag = await generateETag(
       experimentId,
       { name: 'GeneExpression', genes: ['D'] },
       mockExtras,
       mockQcPipelineStartDate,
       Environment.PRODUCTION,
       clusteringSettings,
+      store.dispatch,
+      store.getState,
     );
 
     expect(ETag).toEqual(GENE_EXPRESSION_D_ETAG);
@@ -52,13 +68,15 @@ describe('generateEtag', () => {
   it('Generates unique key for dev environment', async () => {
     Storage.prototype.getItem = jest.fn(() => 'true');
 
-    generateETag(
+    await generateETag(
       experimentId,
       nonGeneExpressionWorkRequest,
       mockExtras,
       mockQcPipelineStartDate,
       Environment.DEVELOPMENT,
       clusteringSettings,
+      store.dispatch,
+      store.getState,
     );
 
     expect(global.Math.random).toHaveBeenCalledTimes(1);
@@ -67,13 +85,15 @@ describe('generateEtag', () => {
   it('Does not generate a unique key for GetEmbedding in dev environment', async () => {
     Storage.prototype.getItem = jest.fn(() => 'true');
 
-    generateETag(
+    await generateETag(
       experimentId,
       { name: 'GetEmbedding' },
       mockExtras,
       mockQcPipelineStartDate,
       Environment.DEVELOPMENT,
       clusteringSettings,
+      store.dispatch,
+      store.getState,
     );
 
     expect(global.Math.random).not.toHaveBeenCalled();
