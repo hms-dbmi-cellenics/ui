96712e1df034c5102b03e01dd1363e99ec6e1f2f
stefanbabukov <stefanbabukov98@gmail.com>
2022-11-09 10:46:32 +0000

    fix tests yet again

--- src/__test__/utils/work/fetchWork.test.js
+++ src/__test__/utils/work/fetchWork.test.js
@@ -26,7 +26,7 @@ jest.mock(
 );
 
 const experimentId = '1234';
-const NON_GENE_EXPRESSION_ETAG = '713b35c59f447a9d308d0bb36521e851'; // pragma: allowlist secret
+const NON_GENE_EXPRESSION_ETAG = '0c939867c2b8ac9327a9dc5296ec8cb9'; // pragma: allowlist secret
 const GENE_EXPRESSION_ABCD_ETAG = 'c2adef166f733e8323504790fe546c57'; // pragma: allowlist secret
 const timeout = 10;
 
--- src/__test__/utils/work/generateETag.test.js
+++ src/__test__/utils/work/generateETag.test.js
@@ -7,7 +7,7 @@ import { makeStore } from 'redux/store';
 import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
 import mockAPI, { generateDefaultMockAPIResponses } from '__test__/test-utils/mockAPI';
 
-const NON_GENE_EXPRESSION_ETAG = 'a6052b2fb1ff0684862e938a66abef9c'; // pragma: allowlist secret
+const NON_GENE_EXPRESSION_ETAG = '0992ea2e4ebbd56d891dceb91a1a54dc'; // pragma: allowlist secret
 const GENE_EXPRESSION_D_ETAG = '78cf13c1fb1596e64f07fa93d6d028a9'; // pragma: allowlist secret
 
 const experimentId = '1234';
