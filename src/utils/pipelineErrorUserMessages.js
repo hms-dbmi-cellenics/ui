c44b80ed8488f893cf73b9061f8238ba4b5a925a
Alex Pickering <alexvpickering@gmail.com>
2022-11-21 16:51:15 -0800

    add PCA info

--- src/utils/pipelineErrorUserMessages.js
+++ src/utils/pipelineErrorUserMessages.js
@@ -4,6 +4,6 @@ export default {
   ERROR_SEURAT_HVFINFO: '\u26A0\uFE0F Couldn\'t extract `HVFInfo` from the uploaded Seurat object. Please check requirements and re-upload after fixing.',
   ERROR_SEURAT_METADATA: '\u26A0\uFE0F Couldn\'t get the `meta.data` slot from the uploaded Seurat object. Please check requirements and re-upload after fixing.',
   ERROR_SEURAT_CLUSTERS: '\u26A0\uFE0F Couldn\'t get `seurat_clusters` from the uploaded Seurat object. Please check requirements and re-upload after fixing.',
-  ERROR_SEURAT_REDUCTION: '\u26A0\uFE0F Couldn\'t get a default dimensionality reduction from the uploaded Seurat object. Please check requirements and re-upload after fixing.',
+  ERROR_SEURAT_REDUCTION: '\u26A0\uFE0F Couldn\'t get a PCA or default dimensionality reduction from the uploaded Seurat object. Please check requirements and re-upload after fixing.',
   ERROR_SEURAT_LOGCOUNTS: '\u26A0\uFE0F Couldn\'t get the `data` slot from the `RNA` assay of the uploaded Seurat object. Please check requirements and re-upload after fixing.',
 };
