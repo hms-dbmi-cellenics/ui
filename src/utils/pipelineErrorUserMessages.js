c6ef1c8f7d6169bb7d01334a8d62c874516ff23c
Alex Pickering <alexvpickering@gmail.com>
2022-11-21 16:49:57 -0800

    add warning icon to user messages

--- src/utils/pipelineErrorUserMessages.js
+++ src/utils/pipelineErrorUserMessages.js
@@ -1,9 +1,9 @@
 export default {
-  ERROR_SEURAT_RDS: 'We couldn\'t read the uploaded rds file. Please re-upload after fixing.',
-  ERROR_SEURAT_COUNTS: 'We couldn\'t get the `counts` slot from the `RNA` assay of the uploaded Seurat object. Please re-upload after fixing.',
-  ERROR_SEURAT_HVFINFO: 'We couldn\'t extract `HVFInfo` from the uploaded Seurat object. Please re-upload after fixing.',
-  ERROR_SEURAT_METADATA: 'We couldn\'t get the `meta.data` slot from the uploaded Seurat object. Please re-upload after fixing.',
-  ERROR_SEURAT_CLUSTERS: 'We couldn\'t get `seurat_clusters` from the uploaded Seurat object. Please re-upload after fixing.',
-  ERROR_SEURAT_REDUCTION: 'We couldn\'t get a default dimensionality reduction from the uploaded Seurat object. Please re-upload after fixing.',
-  ERROR_SEURAT_LOGCOUNTS: 'We couldn\'t get the `data` slot from the `RNA` assay of the uploaded Seurat object. Please re-upload after fixing.',
+  ERROR_SEURAT_RDS: '\u26A0\uFE0F Couldn\'t read the uploaded rds file. Please check requirements and re-upload after fixing.',
+  ERROR_SEURAT_COUNTS: '\u26A0\uFE0F Couldn\'t get the `counts` slot from the `RNA` assay of the uploaded Seurat object. Please check requirements and re-upload after fixing.',
+  ERROR_SEURAT_HVFINFO: '\u26A0\uFE0F Couldn\'t extract `HVFInfo` from the uploaded Seurat object. Please check requirements and re-upload after fixing.',
+  ERROR_SEURAT_METADATA: '\u26A0\uFE0F Couldn\'t get the `meta.data` slot from the uploaded Seurat object. Please check requirements and re-upload after fixing.',
+  ERROR_SEURAT_CLUSTERS: '\u26A0\uFE0F Couldn\'t get `seurat_clusters` from the uploaded Seurat object. Please check requirements and re-upload after fixing.',
+  ERROR_SEURAT_REDUCTION: '\u26A0\uFE0F Couldn\'t get a default dimensionality reduction from the uploaded Seurat object. Please check requirements and re-upload after fixing.',
+  ERROR_SEURAT_LOGCOUNTS: '\u26A0\uFE0F Couldn\'t get the `data` slot from the `RNA` assay of the uploaded Seurat object. Please check requirements and re-upload after fixing.',
 };
