8121bfce36fa2d83671769ccb643046b74d52549
stefanbabukov <stefanbabukov98@gmail.com>
2022-11-23 08:37:53 +0000

    test added

--- src/__test__/components/data-processing/configure-embedding/ConfigureEmbedding.test.jsx
+++ src/__test__/components/data-processing/configure-embedding/ConfigureEmbedding.test.jsx
@@ -1,16 +1,24 @@
 import ConfigureEmbedding from 'components/data-processing/ConfigureEmbedding/ConfigureEmbedding';
 import React from 'react';
-import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import { initialPlotConfigStates } from 'redux/reducers/componentConfig/initialState';
 import { initialEmbeddingState } from 'redux/reducers/embeddings/initialState';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import { mockCellSets } from '__test__/test-utils/cellSets.mock';
+import { generateDataProcessingPlotUuid } from 'utils/generateCustomPlotUuid';
+import { screen, render } from '@testing-library/react';
+import generateExperimentSettingsMock from '__test__/test-utils/experimentSettings.mock';
+import userEvent from '@testing-library/user-event';
 
-jest.mock('redux/selectors');
 const mockStore = configureStore([thunk]);
 const filterName = 'configureEmbedding';
-const configureEmbeddingFilterName = 'configureEmbedding-1';
+const initialExperimentState = generateExperimentSettingsMock([]);
+
+const {
+  embeddingPreviewBySample, embeddingPreviewByCellSets,
+  embeddingPreviewMitochondrialContent, embeddingPreviewDoubletScore,
+} = initialPlotConfigStates;
 
 const mockedStore = mockStore({
   embeddings: {
@@ -29,38 +37,68 @@ const mockedStore = mockStore({
     },
   },
   cellSets: mockCellSets,
+  cellMeta: {
+    doubletScores: {
+      loading: false,
+      error: false,
+      data: [1, 2, 3, 4, 5],
+    },
+    mitochondrialContent: {
+      loading: false,
+      error: false,
+      data: [6, 7, 8, 9, 10],
+    },
+  },
   experimentSettings: {
     ...initialExperimentState,
   },
   componentConfig: {
-    dataIntegrationFrequency: {
-      config: dataIntegrationFrequencyConfig,
-      plotData: [],
-    },
-    [generateDataProcessingPlotUuid(null, configureEmbeddingFilterName, 1)]: {
+    [generateDataProcessingPlotUuid(null, filterName, 0)]: {
       config: embeddingPreviewBySample,
       plotData: [],
     },
     [generateDataProcessingPlotUuid(null, filterName, 1)]: {
-      config: dataIntegrationElbowConfig,
+      config: embeddingPreviewByCellSets,
+      plotData: [],
+    },
+    [generateDataProcessingPlotUuid(null, filterName, 2)]: {
+      config: embeddingPreviewMitochondrialContent,
+      plotData: [],
+    },
+    [generateDataProcessingPlotUuid(null, filterName, 3)]: {
+      config: embeddingPreviewDoubletScore,
       plotData: [],
     },
   },
 });
+
 describe('Configure Embedding', () => {
-  const renderConfigureEmbedding = () => {
+  const renderConfigureEmbedding = async () => {
     const store = mockedStore;
-    const component = mount(
+    await render(
       <Provider store={store}>
-        <DataIntegration
+        <ConfigureEmbedding
           experimentId='1234'
-          onPipelineRun={jest.fn()}
-          onConfigChange={jest.fn()}
+          key='configureEmbedding'
+          onConfigChange={jest.fn}
         />
       </Provider>,
     );
   };
-  it('renders correctly ', () => {
+  it('renders correctly ', async () => {
+    await renderConfigureEmbedding();
 
+    // four mini-plots and one fullsize rendered
+    const plots = screen.getAllByRole('graphics-document');
+    expect(plots.length).toEqual(5);
+
+    // styling and settings options available
+    expect(screen.getByText('Embedding settings')).toBeDefined();
+    expect(screen.getByText('Clustering settings')).toBeDefined();
+    expect(screen.getByText('Plot options')).toBeDefined();
+
+    // additional select data option available
+    userEvent.click(screen.getByText('Plot options'));
+    expect(screen.getAllByText('Select data')).toBeDefined();
   });
 });
