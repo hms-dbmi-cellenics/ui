01b4040d1aa8b6d672c708bea4bdaf049e467a2f
cosa65 <martin@biomage.net>
2022-12-13 08:49:31 -0300

    Remove loadBackendStatus fully

--- src/redux/actions/pipeline/runSubsetExperiment.js
+++ src/redux/actions/pipeline/runSubsetExperiment.js
@@ -2,9 +2,7 @@ import fetchAPI from 'utils/http/fetchAPI';
 import handleError from 'utils/http/handleError';
 import endUserMessages from 'utils/endUserMessages';
 
-import loadBackendStatus from 'redux/actions/backendStatus/loadBackendStatus';
-
-const runSubsetExperiment = (experimentId, name, cellSetKeys) => async (dispatch) => {
+const runSubsetExperiment = (experimentId, name, cellSetKeys) => async () => {
   try {
     await fetchAPI(
       `/v2/experiments/${experimentId}/subset`,
@@ -20,11 +18,7 @@ const runSubsetExperiment = (experimentId, name, cellSetKeys) => async (dispatch
       },
     );
   } catch (e) {
-    const errorMessage = handleError(e, endUserMessages.ERROR_STARTING_PIPLELINE);
-
-    if (errorMessage !== endUserMessages.ERROR_NO_PERMISSIONS) {
-      await dispatch(loadBackendStatus(experimentId));
-    }
+    handleError(e, endUserMessages.ERROR_STARTING_PIPLELINE);
   }
 };
 
