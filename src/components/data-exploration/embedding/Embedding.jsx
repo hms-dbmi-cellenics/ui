ce5936346cd41a122b8a87fa06c8efee2e7c4412
Anugerah Erlaut <aerlaut@live.com>
2023-01-03 12:24:09 +0000

    do not show tooltip if not hovering over cell

--- src/components/data-exploration/embedding/Embedding.jsx
+++ src/components/data-exploration/embedding/Embedding.jsx
@@ -194,6 +194,8 @@ const Embedding = (props) => {
         expression: expressionToDispatch,
         geneName,
       });
+    } else {
+      setCellInfoTooltip(null);
     }
   }, [selectedCell]);
 
