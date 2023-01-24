e75444a3a269e5d1ec551573f79b1241677a0505
Anugerah Erlaut <aerlaut@live.com>
2023-01-24 14:01:50 +0000

    add plot legend alert component

---
+++ src/components/plots/helpers/PlotLegendAlert.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Alert,
+} from 'antd';
+
+const MAX_LEGEND_ITEMS = 50;
+
+const PlotLegendAlert = () => (
+  <center>
+    <Alert
+      message={(
+        <p>
+          {'We have hidden the plot legend, because it is too large and it interferes '}
+          {'with the display of the plot.'}
+          <br />
+          {'You can still display the plot legend by changing the value of "Toggle Legend" option '}
+          {'in Plot Styling settings under "Legend"'}
+          .
+        </p>
+      )}
+      type='warning'
+    />
+  </center>
+);
+
+export default PlotLegendAlert;
+export { MAX_LEGEND_ITEMS };
