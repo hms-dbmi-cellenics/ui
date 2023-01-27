9ea491ec81969f01aa2f6659a96f8528f4c1aa3e
cosa65 <martin@biomage.net>
2023-01-27 17:45:41 -0300

    Remove no longer used saving loader

--- src/pages/data-management/index.jsx
+++ src/pages/data-management/index.jsx
@@ -29,8 +29,6 @@ const DataManagementPage = () => {
   const domainName = useSelector((state) => state.networkResources?.domainName);
 
   const activeExperiment = experiments[activeExperimentId];
-  const { saving: experimentsSaving } = experiments.meta;
-  const { saving: samplesSaving } = samples.meta;
 
   const [newProjectModalVisible, setNewProjectModalVisible] = useState(false);
 
