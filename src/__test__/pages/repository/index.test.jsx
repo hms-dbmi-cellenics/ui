09df3ad4d977da72a80d5e21938e3a69858b5347
ivababukova <iva@biomage.net>
2023-02-13 13:57:40 +0100

    small non-finished test

---
+++ src/__test__/pages/repository/index.test.jsx
@@ -0,0 +1,56 @@
+import RepositoryPage from 'pages/repository';
+import createTestComponentFactory from '__test__/test-utils/testComponentFactory';
+import { Provider } from 'react-redux';
+import { makeStore } from 'redux/store';
+import { act } from 'react-dom/test-utils';
+import { render } from '@testing-library/react';
+import React from 'react';
+import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
+import mockAPI, {
+  generateDefaultMockAPIResponses,
+} from '__test__/test-utils/mockAPI';
+
+const RepositoryPageFactory = createTestComponentFactory(RepositoryPage);
+
+const renderRepositoryPage = async (store) => {
+  await act(async () => {
+    render(
+      <Provider store={store}>
+        {RepositoryPageFactory(store)}
+      </Provider>,
+    );
+  });
+};
+
+jest.mock('@aws-amplify/auth', () => ({
+  currentAuthenticatedUser: jest.fn().mockImplementation(async () => ({
+    attributes: {
+      'custom:agreed_terms': 'false',
+    },
+  })),
+  federatedSignIn: jest.fn(),
+}));
+
+jest.mock('components/repository/RepositoryTable.jsx', () => {
+  const RepositoryTable = () => <div>Hello, world!!! </div>;
+  return RepositoryTable;
+});
+
+jest.mock('redux/actions/experiments');
+
+enableFetchMocks();
+
+describe('Samples table', () => {
+  let store;
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    store = makeStore();
+    fetchMock.resetMocks();
+    fetchMock.mockIf(/.*/, mockAPI(generateDefaultMockAPIResponses('1234-5678')));
+  });
+
+  it('Does not render child component if terms are not accepted', async () => {
+    await renderRepositoryPage(store);
+  });
+});
