361c4d785aebe593385fa426fd2cbccd81c6eeed
cosa65 <martin@biomage.net>
2023-03-31 14:53:49 -0300

    Add error handling for downloadProcessedMatrix

--- src/utils/extraActionCreators/downloadProcessedMatrix.js
+++ src/utils/extraActionCreators/downloadProcessedMatrix.js
@@ -6,30 +6,36 @@ import getTimeoutForWorkerTask from 'utils/getTimeoutForWorkerTask';
 import writeToFileURL from 'utils/writeToFileURL';
 import downloadFromUrl from 'utils/downloadFromUrl';
 import getEmbeddingETag from 'utils/work/getEmbeddingETag';
+import handleError from 'utils/http/handleError';
+import endUserMessages from 'utils/endUserMessages';
 
 const downloadProcessedMatrix = (experimentId) => async (dispatch, getState) => {
-  await dispatch(loadProcessingSettings(experimentId));
+  try {
+    await dispatch(loadProcessingSettings(experimentId));
 
-  const {
-    method: embeddingMethod,
-  } = getState().experimentSettings.processing.configureEmbedding.embeddingSettings;
+    const {
+      method: embeddingMethod,
+    } = getState().experimentSettings.processing.configureEmbedding.embeddingSettings;
 
-  await dispatch(loadEmbedding(experimentId, embeddingMethod));
+    await dispatch(loadEmbedding(experimentId, embeddingMethod));
 
-  const taskName = 'DownloadAnnotSeuratObject';
+    const taskName = 'DownloadAnnotSeuratObject';
 
-  const body = {
-    name: taskName,
-    embeddingETag: await getEmbeddingETag(experimentId, getState, dispatch),
-  };
+    const body = {
+      name: taskName,
+      embeddingETag: await getEmbeddingETag(experimentId, getState, dispatch),
+    };
 
-  const timeout = getTimeoutForWorkerTask(getState(), taskName);
+    const timeout = getTimeoutForWorkerTask(getState(), taskName);
 
-  const data = await fetchWork(
-    experimentId, body, getState, dispatch, { timeout },
-  );
+    const data = await fetchWork(
+      experimentId, body, getState, dispatch, { timeout },
+    );
 
-  downloadFromUrl(writeToFileURL(data), 'ProcessedSeuratObject.rds');
+    downloadFromUrl(writeToFileURL(data), 'ProcessedSeuratObject.rds');
+  } catch (e) {
+    handleError(e, endUserMessages.ERROR_DOWNLOADING_SEURAT_OBJECT);
+  }
 };
 
 export default downloadProcessedMatrix;
