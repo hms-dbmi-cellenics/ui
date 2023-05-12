e998cf9c168cc9c3290f738d492e79cab26a3391
cosa65 <martin@biomage.net>
2023-05-12 10:13:46 -0300

    Cleanup

--- src/components/data-processing/ChangesNotAppliedModal.jsx
+++ src/components/data-processing/ChangesNotAppliedModal.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import {
