8c3ca415becac1eb8c6a8bc9729175253eaf620b
Kristian-A <kristian.zh.apostolov@gmail.com>
2023-08-07 17:04:12 +0300

    Make relaunch experiment use the /retry endpoint
    Signed-off-by: Kristian-A <kristian.zh.apostolov@gmail.com>

---
+++ src/redux/actions/pipeline/retry.js
@@ -0,0 +1,32 @@
+import fetchAPI from 'utils/http/fetchAPI';
+import handleError from 'utils/http/handleError';
+import endUserMessages from 'utils/endUserMessages';
+import loadBackendStatus from 'redux/actions/backendStatus/loadBackendStatus';
+
+const retry = (experimentId) => async (dispatch) => {
+  try {
+    await fetchAPI(
+      `/v2/experiments/${experimentId}/retry`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    await dispatch(loadBackendStatus(experimentId));
+
+    return true;
+  } catch (e) {
+    const errorMessage = handleError(e, endUserMessages.ERROR_STARTING_PIPLELINE);
+
+    if (errorMessage !== endUserMessages.ERROR_NO_PERMISSIONS) {
+      await dispatch(loadBackendStatus(experimentId));
+    }
+
+    return false;
+  }
+};
+
+export default retry;
