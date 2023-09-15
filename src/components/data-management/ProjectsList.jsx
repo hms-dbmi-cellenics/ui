b0df30a5e4bce1a1c3af99e79e487dde8b033c78
stefanbabukov <stefanbabukov98@gmail.com>
2023-09-15 13:27:32 +0100

    virtualization added
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/components/data-management/ProjectsList.jsx
+++ src/components/data-management/ProjectsList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Space, Skeleton } from 'antd';
+import { Table, Skeleton } from 'antd';
+import { useVT } from 'virtualizedtableforantd4';
 
 import integrationTestConstants from 'utils/integrationTestConstants';
 import ProjectCard from './ProjectCard';
-
 // This makes sure that all the projects can be viewed properly inside the list
 // TODO : This has to be done properly in CSS
 const windowMargin = 130; // px
@@ -14,41 +14,53 @@ const ProjectsList = (props) => {
   const { height, filter } = props;
 
   const experiments = useSelector((state) => state.experiments);
+  const [vt] = useVT(() => ({
+    scroll: {
+      y: height - windowMargin,
+    },
+  }), [height]);
 
   if (experiments.meta.loading) {
     return [...Array(5)].map((_, idx) => <Skeleton key={`skeleton-${idx}`} role='progressbar' active />);
   }
-
   // if there are no element return an emtpy one so that the tests know the list has been loaded
+
   if (experiments.ids.length === 0) {
     return (
       <div data-test-id={integrationTestConstants.ids.PROJECTS_LIST} />
     );
   }
 
+  const dataSource = experiments.ids.map((experimentId) => {
+    const experiment = experiments[experimentId];
+    const matchFilter = experiment.name.match(filter) || experimentId.match(filter);
+    if (!matchFilter) return <></>;
+
+    return {
+      key: experiment.id,
+      content: (
+        <ProjectCard
+          experimentId={experiment.id}
+        />
+      ),
+    };
+  }).filter(Boolean);
+  const columns = [{
+    dataIndex: 'content',
+    render: (content) => content,
+  }];
+
   return (
-    <Space
+    <Table
+      dataSource={dataSource}
+      columns={columns}
+      pagination={false}
+      components={vt}
+      scroll={{ y: height - windowMargin }}
       data-test-id={integrationTestConstants.ids.PROJECTS_LIST}
-      direction='vertical'
-      style={{ height: height - windowMargin, overflowY: 'auto' }}
-    >
-      {
-        experiments.ids.map((experimentId) => {
-          const experiment = experiments[experimentId];
-
-          const matchFilter = experiment.name.match(filter) || experimentId.match(filter);
-
-          if (!matchFilter) return <></>;
-
-          return (
-            <ProjectCard
-              key={experiment.id}
-              experimentId={experiment.id}
-            />
-          );
-        })
-      }
-    </Space>
+      showHeader={false}
+      bordered={false}
+    />
   );
 };
 
