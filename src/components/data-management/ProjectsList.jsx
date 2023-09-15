9d3200d24f390845c743ea08976c9f69729a7543
stefanbabukov <stefanbabukov98@gmail.com>
2023-09-15 16:06:37 +0100

    visualized list
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/components/data-management/ProjectsList.jsx
+++ src/components/data-management/ProjectsList.jsx
@@ -1,66 +1,53 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Table, Skeleton } from 'antd';
-import { useVT } from 'virtualizedtableforantd4';
-
+import { Space, Skeleton } from 'antd';
+import { FixedSizeList as List } from 'react-window';
 import integrationTestConstants from 'utils/integrationTestConstants';
 import ProjectCard from './ProjectCard';
-// This makes sure that all the projects can be viewed properly inside the list
-// TODO : This has to be done properly in CSS
-const windowMargin = 130; // px
+
+const windowMargin = 130;
+
+const Row = ({ index, data, style }) => {
+  const experiment = data[index];
+
+  return (
+    <Space style={{ ...style, width: '100%' }}>
+      <ProjectCard key={experiment.id} experimentId={experiment.id} />
+    </Space>
+  );
+};
 
 const ProjectsList = (props) => {
   const { height, filter } = props;
 
   const experiments = useSelector((state) => state.experiments);
-  const [vt] = useVT(() => ({
-    scroll: {
-      y: height - windowMargin,
-    },
-  }), [height]);
 
   if (experiments.meta.loading) {
     return [...Array(5)].map((_, idx) => <Skeleton key={`skeleton-${idx}`} role='progressbar' active />);
   }
-  // if there are no element return an emtpy one so that the tests know the list has been loaded
 
-  if (experiments.ids.length === 0) {
+  const filteredExperiments = experiments.ids
+    .map((id) => experiments[id])
+    .filter((exp) => exp.name.match(filter) || exp.id.match(filter));
+  if (filteredExperiments.length === 0) {
     return (
       <div data-test-id={integrationTestConstants.ids.PROJECTS_LIST} />
     );
   }
 
-  const dataSource = experiments.ids.map((experimentId) => {
-    const experiment = experiments[experimentId];
-    const matchFilter = experiment.name.match(filter) || experimentId.match(filter);
-    if (!matchFilter) return <></>;
-
-    return {
-      key: experiment.id,
-      content: (
-        <ProjectCard
-          experimentId={experiment.id}
-        />
-      ),
-    };
-  }).filter(Boolean);
-  const columns = [{
-    dataIndex: 'content',
-    render: (content) => content,
-  }];
-
   return (
-    <Table
-      dataSource={dataSource}
-      columns={columns}
-      pagination={false}
-      components={vt}
-      scroll={{ y: height - windowMargin }}
-      data-test-id={integrationTestConstants.ids.PROJECTS_LIST}
-      showHeader={false}
-      bordered={false}
-    />
+    <div data-test-id={integrationTestConstants.ids.PROJECTS_LIST}>
+      <List
+        height={height - windowMargin}
+        itemCount={filteredExperiments.length}
+        itemSize={220}
+        itemData={filteredExperiments}
+        width='100%'
+      >
+        {Row}
+      </List>
+    </div>
   );
 };
 
