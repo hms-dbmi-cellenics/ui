94b02df32a6cb99bcdd5c6ccd5ba9140a5fafb97
stefanbabukov <stefanbabukov98@gmail.com>
2023-09-19 11:11:53 +0100

    variable lenght height added
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/components/data-management/ProjectsList.jsx
+++ src/components/data-management/ProjectsList.jsx
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useCallback, useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Space, Skeleton } from 'antd';
-import { FixedSizeList as List } from 'react-window';
+import { VariableSizeList as List } from 'react-window';
 import integrationTestConstants from 'utils/integrationTestConstants';
 import ProjectCard from './ProjectCard';
 
 const windowMargin = 130;
 
-const Row = ({ index, data, style }) => {
+const Row = ({
+  index, data, style, setSize,
+}) => {
+  const rowRef = useRef();
   const experiment = data[index];
+
+  useEffect(() => {
+    if (rowRef.current) {
+      setSize(index, rowRef.current.getBoundingClientRect().height);
+    }
+  }, [setSize, index, experiment]);
 
   return (
     <Space style={{ ...style, width: '100%' }}>
-      <ProjectCard key={experiment.id} experimentId={experiment.id} />
+      <div ref={rowRef}>
+        <ProjectCard key={experiment.id} experimentId={experiment.id} />
+      </div>
     </Space>
   );
 };
 
 const ProjectsList = (props) => {
   const { height, filter } = props;
+  const listRef = useRef();
+  const sizeMap = useRef({});
 
   const experiments = useSelector((state) => state.experiments);
 
@@ -27,9 +40,20 @@ const ProjectsList = (props) => {
     return [...Array(5)].map((_, idx) => <Skeleton key={`skeleton-${idx}`} role='progressbar' active />);
   }
 
+  const setSize = useCallback((index, size) => {
+    sizeMap.current = { ...sizeMap.current, [index]: size + 5 };
+
+    //  if the height gets changed, we need to reset the heights
+    // so that they are recalculated
+    listRef.current.resetAfterIndex(index);
+  }, []);
+
+  const getSize = (index) => sizeMap.current[index] || 204; // default height if not yet measured
+
   const filteredExperiments = experiments.ids
     .map((id) => experiments[id])
     .filter((exp) => (exp.name.match(filter) || exp.id.match(filter)));
+
   if (filteredExperiments.length === 0) {
     return (
       <div data-test-id={integrationTestConstants.ids.PROJECTS_LIST} />
@@ -39,13 +63,21 @@ const ProjectsList = (props) => {
   return (
     <div data-test-id={integrationTestConstants.ids.PROJECTS_LIST}>
       <List
+        ref={listRef}
         height={height - windowMargin}
-        itemCount={filteredExperiments.length}
-        itemSize={220}
-        itemData={filteredExperiments}
         width='100%'
+        itemCount={filteredExperiments.length}
+        itemSize={getSize}
+        itemData={filteredExperiments}
       >
-        {Row}
+        {({ data, index, style }) => (
+          <Row
+            data={data}
+            index={index}
+            style={style}
+            setSize={setSize}
+          />
+        )}
       </List>
     </div>
   );
