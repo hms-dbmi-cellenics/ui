47d7776289318868c3826dd161bfe29642bf1d3f
stefanbabukov <stefanbabukov98@gmail.com>
2023-10-23 14:55:17 +0100

    move loader
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/pages/experiments/[experimentId]/plots-and-tables/batch-differential-expression/index.jsx
+++ src/pages/experiments/[experimentId]/plots-and-tables/batch-differential-expression/index.jsx
@@ -262,7 +262,7 @@ const BatchDiffExpression = (props) => {
     }
   };
 
-  if (!cellSets.accessible) {
+  if (!cellSets.accessible || dataLoading) {
     return (
       <center>
         <Loader experimentId={experimentId} />
@@ -283,15 +283,13 @@ const BatchDiffExpression = (props) => {
             onChange={(e) => { setChosenOperation(e.target.value); }}
           >
             <Space direction='vertical'>
-              <Space direction='horizontal'>
-                <Radio value='fullList' disabled={dataLoading}>
-                  Generate a full list of marker genes for all cell sets
-                  {'   '}
-                  <Tooltip title='Each cell set will be compared to all other cells, using all samples.'>
-                    <InfoCircleOutlined />
-                  </Tooltip>
-                </Radio>
-              </Space>
+              <Radio value='fullList' disabled={dataLoading}>
+                Generate a full list of marker genes for all cell sets
+                {'   '}
+                <Tooltip title='Each cell set will be compared to all other cells, using all samples.'>
+                  <InfoCircleOutlined />
+                </Tooltip>
+              </Radio>
               <Radio value='compareForCellSets' disabled={isDatasetUnisample || dataLoading}>
                 {
                   isDatasetUnisample ? (
@@ -366,9 +364,6 @@ const BatchDiffExpression = (props) => {
               >
                 Compute and Download
               </Button>
-              {dataLoading && (
-                <Loader experimentId={experimentId} />
-              )}
             </Space>
             This might take several minutes to complete.
           </Space>
