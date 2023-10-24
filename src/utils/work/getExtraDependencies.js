5e5ad9d3b922c856fda82041794663c90376bcb2
cosa65 <martin@biomage.net>
2023-10-24 13:32:53 -0300

    Merge downsampling related dependency getters into a single one
    Signed-off-by: cosa65 <martin@biomage.net>

--- src/utils/work/getExtraDependencies.js
+++ src/utils/work/getExtraDependencies.js
@@ -19,19 +19,25 @@ const getClusteringSettings = async (experimentId, body, dispatch, getState) => 
 
 // Check that the cell sets within the selected selectedCellSet didn't change
 // e.g., if cell set was deleted we can't use cache
-const getSelectedCellSet = async (experimentId, body, dispatch, getState) => {
+const getCellSetsThatAffectDownsampling = async (experimentId, body, dispatch, getState) => {
   // If not downsampling, then there's no dependency on groupedTracks
   if (!body.downsampleSettings) return '';
 
+  const { downsampleSettings: { groupedTracks, selectedCellSet } } = body;
+
   await dispatch(loadCellSets(experimentId));
 
-  const [{ children }] = getCellSetsHierarchyByKeys(
-    [body.downsampleSettings.selectedCellSet],
-  )(getState());
+  const [{ children }] = getCellSetsHierarchyByKeys([selectedCellSet])(getState());
 
-  const cellSetsKeys = children.map((cellSet) => cellSet.key);
+  const selectedCellSetKeys = children.map((cellSet) => cellSet.key);
 
-  return cellSetsKeys;
+  const groupedCellSetKeys = getCellSetsHierarchy(groupedTracks)(getState())
+    .map((cellClass) => cellClass.children)
+    .flat()
+    .map(({ key }) => key);
+
+  // Keep them in separate lists, they each represent different changes in the settings
+  return [selectedCellSetKeys, groupedCellSetKeys];
 };
 
 const getCellSets = async (experimentId, body, dispatch, getState) => {
@@ -42,20 +48,6 @@ const getCellSets = async (experimentId, body, dispatch, getState) => {
   return hierarchy;
 };
 
-const getGroupedTracksCellSets = async (experimentId, body, dispatch, getState) => {
-  // If not downsampling, then there's no dependency on groupedTracks
-  if (!body.downsampleSettings) return '';
-
-  await dispatch(loadCellSets(experimentId));
-
-  const groupedCellSetKeys = getCellSetsHierarchy(body.downsampleSettings.groupedTracks)(getState())
-    .map((cellClass) => cellClass.children)
-    .flat()
-    .map(({ key }) => key);
-
-  return groupedCellSetKeys;
-};
-
 const dependencyGetters = {
   ClusterCells: [],
   ScTypeAnnotate: [],
@@ -64,18 +56,14 @@ const dependencyGetters = {
   ListGenes: [],
   DifferentialExpression: [getClusteringSettings],
   BatchDifferentialExpression: [getClusteringSettings],
-  GeneExpression: [
-    getClusteringSettings, getGroupedTracksCellSets, getSelectedCellSet,
-  ],
+  GeneExpression: [getClusteringSettings, getCellSetsThatAffectDownsampling],
   GetBackgroundExpressedGenes: [getClusteringSettings],
   DotPlot: [getClusteringSettings],
   GetDoubletScore: [],
   GetMitochondrialContent: [],
   GetNGenes: [],
   GetNUmis: [],
-  MarkerHeatmap: [
-    getClusteringSettings, getSelectedCellSet, getGroupedTracksCellSets,
-  ],
+  MarkerHeatmap: [getClusteringSettings, getCellSetsThatAffectDownsampling],
   GetTrajectoryAnalysisStartingNodes: [getClusteringSettings],
   GetTrajectoryAnalysisPseudoTime: [getClusteringSettings],
   GetNormalizedExpression: [getClusteringSettings],
