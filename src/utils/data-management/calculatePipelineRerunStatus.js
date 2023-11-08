221b60247ae86114a5f09c14ef605a6048b3d07c
cosa65 <martin@biomage.net>
2023-11-08 09:09:16 -0300

    Some renames and reordering, add base calculatePipelineRerunStatus
    Signed-off-by: cosa65 <martin@biomage.net>

---
+++ src/utils/data-management/calculatePipelineRerunStatus.js
@@ -0,0 +1,6 @@
+import calculateGem2sRerunStatus from 'utils/data-management/calculateGem2sRerunStatus';
+import calculateQCRerunStatus from 'utils/data-management/calculateQCRerunStatus';
+
+const calculatePipelineRerunStatus = () => {
+
+};
