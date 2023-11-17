67bde7fe58fcefd2da2bc2733e6994d561c20a39
cosa65 <martin@biomage.net>
2023-11-17 10:30:08 -0300

    Rollback seurat valdiator changes
    Signed-off-by: cosa65 <martin@biomage.net>

--- src/utils/upload/sampleValidators.js
+++ src/utils/upload/sampleValidators.js
@@ -2,12 +2,13 @@ import { sampleTech } from 'utils/constants';
 
 import validate10x from 'utils/upload/validate10x';
 import validateRhapsody from 'utils/upload/validateRhapsody';
+import validateSeurat from 'utils/upload/validateSeurat';
 import validateH5 from 'utils/upload/validateH5';
 
 const sampleValidators = {
   [sampleTech['10X']]: validate10x,
   [sampleTech.RHAPSODY]: validateRhapsody,
-  [sampleTech.SEURAT]: () => { },
+  [sampleTech.SEURAT]: validateSeurat,
   [sampleTech.H5]: validateH5,
 };
 
