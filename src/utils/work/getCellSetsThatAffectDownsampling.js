4c198297877f21b403d88d7119998bb875f569f7
Pol Alvarez <pol.avms@gmail.com>
2023-11-22 13:15:59 +0100

    chore: addressed PR comments
    Signed-off-by: Pol Alvarez <pol.avms@gmail.com>

--- src/utils/work/getCellSetsThatAffectDownsampling.js
+++ src/utils/work/getCellSetsThatAffectDownsampling.js
@@ -1,7 +1,7 @@
 import loadCellSets from 'redux/actions/cellSets/loadCellSets';
 import { getCellSetsHierarchy, getCellSetsHierarchyByKeys } from 'redux/selectors';
 
-// Check that the cell sets within the selected selectedCellSet didn't change
+// Check that the cell sets within the selected selectedCellSet and grouped tracks didn't change
 // e.g., if cell set was deleted we can't use cache
 const getCellSetsThatAffectDownsampling = async (
   experimentId, selectedCellSetKey, groupedTracks, dispatch, getState) => {
