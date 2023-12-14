06bb3f13db3e2c50d2b21911cc884815c1071e47
stefanbabukov <stefanbabukov98@gmail.com>
2023-12-14 14:08:52 +0200

    if a plot is removed, update the selected one
    Signed-off-by: stefanbabukov <stefanbabukov98@gmail.com>

--- src/components/plots/styling/MultiViewEditor.jsx
+++ src/components/plots/styling/MultiViewEditor.jsx
@@ -65,7 +65,7 @@ const MultiViewEditor = (props) => {
   useEffect(() => {
     if (!multiViewConfig) return;
 
-    if (!selectedPlotUuid && multiViewPlotUuids.length) {
+    if ((!selectedPlotUuid && multiViewPlotUuids.length) || !multiViewPlotUuids.includes(selectedPlotUuid)) {
       setSelectedPlotUuid(multiViewPlotUuids[0]);
     }
     if (localNRows !== multiViewConfig.nrows) {
