5cad5e1b929e6016a382adcd1b18453b180aea09
Oliver Gibson <76957896+ogibson@users.noreply.github.com>
2024-01-03 16:44:42 -0300

    Update src/components/data-exploration/cell-sets-tool/ScoreCellCycleTool.jsx
    Signed-off-by: Oliver Gibson <76957896+ogibson@users.noreply.github.com>

--- src/components/data-exploration/cell-sets-tool/ScoreCellCycleTool.jsx
+++ src/components/data-exploration/cell-sets-tool/ScoreCellCycleTool.jsx
@@ -11,28 +11,7 @@ import { useDispatch } from 'react-redux';
 
 const cycleScoringTooltipText = (
   <>
-    Automatic annotation is performed using ScType, a marker gene-based tool
-    developed by Aleksandr Ianevski et al.
-    It uses a marker genes database which was build using
-    {' '}
-    <a target='_blank' href='http://biocc.hrbmu.edu.cn/CellMarker/' rel='noreferrer'>CellMarker</a>
-    ,
-    {' '}
-    <a target='_blank' href='https://panglaodb.se/' rel='noreferrer'>PanglaoDB</a>
-    ,
-    and 15 novel cell types with corresponding marker genes added by
-    manual curation of more than 10 papers.
-    The current version of the ScType database contains a total of
-    3,980 cell markers for 194 cell types in 17 human tissues and 4,212 cell markers
-    for 194 cell types in 17 mouse tissues.
-    More details can be found in
-    {' '}
-    <a target='_blank' href='https://www.nature.com/articles/s41467-022-28803-w' rel='noreferrer'>the ScType paper</a>
-    {' '}
-    and in
-    {' '}
-    <a target='_blank' href='https://github.com/IanevskiAleksandr/sc-type' rel='noreferrer'>the ScType github repo</a>
-    .
+    Cell Cycle Regression is only available for mouse and human samples. It relies on the Seurat list of S and G2M genes to score each cell with S and G2M scores. Cells with higher than 0.5 score are classified at that stage of the cell cycle, and cells with less than 0.5 for both are classified as cells in G1. 
   </>
 );
 
