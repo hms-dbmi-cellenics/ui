c4b4c30d46f641d70c7f0028ebbfd8e1bc6e5ca5
sara castellano <sara@biomage.net>
2024-03-05 14:57:58 +0100

    add console log to updateBackendStatus
    Signed-off-by: sara castellano <sara@biomage.net>

--- src/redux/actions/backendStatus/updateBackendStatus.js
+++ src/redux/actions/backendStatus/updateBackendStatus.js
@@ -3,6 +3,9 @@ import {
 } from '../../actionTypes/backendStatus';
 
 const updateBackendStatus = (experimentId, status) => async (dispatch) => {
+  // Log the payload before dispatching the action
+  console.log('Dispatching BACKEND_STATUS_UPDATED with payload:', { experimentId, status });
+
   dispatch({
     type: BACKEND_STATUS_UPDATED,
     payload: { experimentId, status },
