303b787063a75a0122b1ecf356eabe26b2f3e9dd
Alex Pickering <alexvpickering@gmail.com>
2024-09-17 15:36:07 -0700

    generalize obj2s user messages
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- src/utils/pipelineErrorUserMessages.js
+++ src/utils/pipelineErrorUserMessages.js
@@ -1,9 +1,9 @@
 export default {
-  ERROR_SEURAT_RDS: '\u26A0\uFE0F We couldn\'t read the uploaded rds file. Please check requirements and re-upload after fixing.',
-  ERROR_SEURAT_COUNTS: '\u26A0\uFE0F We couldn\'t get the `counts` slot from the `RNA` assay of the uploaded Seurat object. Please check requirements and re-upload after fixing.',
-  ERROR_SEURAT_HVFINFO: '\u26A0\uFE0F We couldn\'t run `FindVariableFeatures` on the `data` slot from the `RNA` assay of the uploaded Seurat object. Please check requirements and re-upload after fixing.',
-  ERROR_SEURAT_METADATA: '\u26A0\uFE0F We couldn\'t get the `meta.data` slot from the uploaded Seurat object. Please check requirements and re-upload after fixing.',
-  ERROR_SEURAT_CLUSTERS: '\u26A0\uFE0F We couldn\'t detect clusters in the `meta.data` slot from the uploaded Seurat object. Please check requirements and re-upload after fixing.',
-  ERROR_SEURAT_REDUCTION: '\u26A0\uFE0F We couldn\'t get the `pca` and `umap` or `tsne` dimensionality reduction from the uploaded Seurat object. Please check requirements and re-upload after fixing.',
-  ERROR_SEURAT_LOGCOUNTS: '\u26A0\uFE0F We couldn\'t get the `data` slot from the `RNA` assay of the uploaded Seurat object. Please check requirements and re-upload after fixing.',
+  ERROR_OBJ2S_RDS: '\u26A0\uFE0F We couldn\'t read the uploaded file. Please check requirements and re-upload after fixing.',
+  ERROR_OBJ2S_COUNTS: '\u26A0\uFE0F We couldn\'t get the feature counts from the uploaded object. Please check requirements and re-upload after fixing.',
+  ERROR_OBJ2S_HVFINFO: '\u26A0\uFE0F We couldn\'t run `FindVariableFeatures` on the logcounts from the uploaded object. Please check requirements and re-upload after fixing.',
+  ERROR_OBJ2S_METADATA: '\u26A0\uFE0F We couldn\'t get the meta data from the uploaded object. Please check requirements and re-upload after fixing.',
+  ERROR_OBJ2S_CLUSTERS: '\u26A0\uFE0F We couldn\'t detect clusters in the metadata from the uploaded object. Please check requirements and re-upload after fixing.',
+  ERROR_OBJ2S_REDUCTION: '\u26A0\uFE0F We couldn\'t get the PCA and UMAP or TSNE dimensionality reduction from the uploaded object. Please check requirements and re-upload after fixing.',
+  ERROR_OBJ2S_LOGCOUNTS: '\u26A0\uFE0F We couldn\'t get the logcounts from the uploaded object. Please check requirements and re-upload after fixing.',
 };
