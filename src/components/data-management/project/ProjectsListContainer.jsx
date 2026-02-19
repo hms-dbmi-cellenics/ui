c9b27204df53b75866fa5ce110797b5a19781bf5
Alex Pickering <alexvpickering@gmail.com>
2026-02-19 17:44:07 -0500

    Downplay Dataset Repository in Data Management
    - Change 'Create New Project' to directly open project creation modal instead of showing dropdown menu with repository option
    - Make 'Add data' button blue (primary) only when project is empty
    - Remove repository navigation option from the UI
    - Update tests to reflect new behavior
    
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- src/components/data-management/project/ProjectsListContainer.jsx
+++ src/components/data-management/project/ProjectsListContainer.jsx
@@ -1,52 +1,29 @@
 import {
   Button,
-  Dropdown,
-  Menu,
   Space,
 } from 'antd';
 import React, { useState } from 'react';
-import { useAppRouter } from 'utils/AppRouteProvider';
 
 import PropTypes from 'prop-types';
 import integrationTestConstants from 'utils/integrationTestConstants';
-import { modules } from 'utils/constants';
 import ProjectsList from './ProjectsList';
 import ProjectSearchBox from './ProjectSearchBox';
 
 const ProjectsListContainer = (props) => {
   const { height, onCreateNewProject } = props;
 
-  const { navigateTo } = useAppRouter();
   const [filterParam, setFilterParam] = useState(new RegExp('.*', 'i'));
-
-  const menuItems = [
-    {
-      label: 'Upload Project',
-      key: 'upload_project',
-      onClick: () => onCreateNewProject(),
-    },
-    {
-      label: 'Select from Dataset Repository',
-      key: 'copy_from_repository',
-      onClick: () => { navigateTo(modules.REPOSITORY); },
-    },
-  ]
 
   return (
     <Space direction='vertical' style={{ width: '100%' }}>
-      <Dropdown
-        menu={{ items: menuItems }}
-        trigger={['click']}
-        placement='bottomRight'
+      <Button
+        data-test-id={integrationTestConstants.ids.CREATE_NEW_PROJECT_BUTTON}
+        type='primary'
+        block
+        onClick={onCreateNewProject}
       >
-        <Button
-          data-test-id={integrationTestConstants.ids.CREATE_NEW_PROJECT_BUTTON}
-          type='primary'
-          block
-        >
-          Create New Project
-        </Button>
-      </Dropdown>
+        Create New Project
+      </Button>
       <ProjectSearchBox onChange={(searchRegex) => setFilterParam(searchRegex)} />
       <ProjectsList height={height} filter={filterParam} />
     </Space>
