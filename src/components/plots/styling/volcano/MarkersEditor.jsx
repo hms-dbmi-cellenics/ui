379655da061419db652c07f0ed79e9042e6b1cbc
Alex Pickering <alexvpickering@gmail.com>
2026-02-19 12:36:41 -0500

    Fix ColorPicker import path in volcano MarkersEditor
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- src/components/plots/styling/volcano/MarkersEditor.jsx
+++ src/components/plots/styling/volcano/MarkersEditor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Form } from 'antd';
-import ColorPicker from '../ColorPicker';
+import ColorPicker from '../../../ColorPicker';
 
 const MarkersEditor = (props) => {
   const { onUpdate, config } = props;
