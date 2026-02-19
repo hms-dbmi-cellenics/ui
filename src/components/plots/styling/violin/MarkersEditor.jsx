530d54cd59df848745559313aee50a057c7bb33b
Alex Pickering <alexvpickering@gmail.com>
2026-02-19 10:56:01 -0500

    Fix violin plot markers editor: add opacity slider, use throttled updates, restructure with consistent labels and bold headings
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- src/components/plots/styling/violin/MarkersEditor.jsx
+++ src/components/plots/styling/violin/MarkersEditor.jsx
@@ -1,52 +1,69 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, Radio, Slider } from 'antd';
+import {
+  Form, Radio, Slider, Space,
+} from 'antd';
+import useUpdateThrottled from 'utils/customHooks/useUpdateThrottled';
 
 const MarkersEditor = (props) => {
   const { onUpdate, config } = props;
+  const [newConfig, handleChange] = useUpdateThrottled(onUpdate, config);
 
   return (
-    <Form
-      size='small'
-      labelCol={{ span: 14 }}
-      wrapperCol={{ span: 18 }}
-    >
-      <Form.Item
-        label='Cell markers'
+    <Space direction='vertical' style={{ width: '100%' }}>
+      <Form
+        size='small'
+        labelCol={{ span: 12, style: { textAlign: 'left' } }}
+        wrapperCol={{ span: 12 }}
       >
-        <Radio.Group
-          onChange={(e) => onUpdate({ selectedPointsVisible: e.target.value === 'show' })}
-          value={config.selectedPointsVisible ? 'show' : 'hide'}
+        <p><strong>Toggle Markers:</strong></p>
+        <Form.Item>
+          <Radio.Group
+            onChange={(e) => handleChange({ selectedPointsVisible: e.target.value === 'show' })}
+            value={newConfig.selectedPointsVisible ? 'show' : 'hide'}
+          >
+            <Radio value='show'>Show</Radio>
+            <Radio value='hide'>Hide</Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item
+          label='Point Size'
         >
-          <Radio value='show'>Show</Radio>
-          <Radio value='hide'>Hide</Radio>
-        </Radio.Group>
-      </Form.Item>
-      <Form.Item
-        label='Point Size'
-      >
-        <Slider
-          value={config.marker.size}
-          min={1}
-          max={100}
-          onChange={(value) => {
-            onUpdate({ marker: { size: value } });
-          }}
-          marks={{ 1: 1, 100: 100 }}
-        />
-      </Form.Item>
-      <Form.Item
-        label='Median and Interquartile range'
-      >
-        <Radio.Group
-          onChange={(e) => onUpdate({ statisticsVisible: e.target.value === 'show' })}
-          value={config.statisticsVisible ? 'show' : 'hide'}
+          <Slider
+            value={newConfig.marker.size}
+            min={1}
+            max={100}
+            onChange={(value) => {
+              handleChange({ marker: { size: value } });
+            }}
+            marks={{ 1: 1, 100: 100 }}
+          />
+        </Form.Item>
+        <Form.Item
+          label='Point Fill Opacity'
         >
-          <Radio value='show'>Show</Radio>
-          <Radio value='hide'>Hide</Radio>
-        </Radio.Group>
-      </Form.Item>
-    </Form>
+          <Slider
+            value={newConfig.marker.opacity}
+            min={1}
+            max={10}
+            onChange={(value) => {
+              handleChange({ marker: { opacity: value } });
+            }}
+            marks={{ 1: 1, 10: 10 }}
+          />
+        </Form.Item>
+        <p><strong>Toggle Statistics:</strong></p>
+        <Form.Item>
+          <Radio.Group
+            onChange={(e) => handleChange({ statisticsVisible: e.target.value === 'show' })}
+            value={newConfig.statisticsVisible ? 'show' : 'hide'}
+          >
+            <Radio value='show'>Show</Radio>
+            <Radio value='hide'>Hide</Radio>
+          </Radio.Group>
+        </Form.Item>
+      </Form>
+    </Space>
   );
 };
 
