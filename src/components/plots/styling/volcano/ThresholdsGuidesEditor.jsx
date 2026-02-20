9d1064504b58fa9b80e40b30b5bccb445e159b2f
Alex Pickering <alexvpickering@gmail.com>
2026-02-20 12:08:49 -0500

    Reorganize volcano plot threshold controls with proper label alignment and horizontal layout
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- src/components/plots/styling/volcano/ThresholdsGuidesEditor.jsx
+++ src/components/plots/styling/volcano/ThresholdsGuidesEditor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Form, Checkbox, Space, Typography,
+  Form, Checkbox, Typography,
 } from 'antd';
 import _ from 'lodash';
 import ColorPicker from 'components/ColorPicker';
@@ -22,89 +22,105 @@ const ThresholdsGuidesEditor = (props) => {
         wrapperCol={{ span: 16 }}
       >
         <p><strong>Adjusted p-value:</strong></p>
-        <Form.Item>
-          <Space direction='vertical' style={{ width: '100%' }}>
-            <SliderWithInput
-              min={0.00001}
-              max={0.5}
-              step={0.001}
-              value={config.adjPvalueThreshold}
-              onUpdate={(val) => debouncedUpdate({ adjPvalueThreshold: val })}
-            />
-            <Checkbox
-              checked={config.showpvalueThresholdGuides}
-              onChange={(e) => {
-                onUpdate({ showpvalueThresholdGuides: e.target.checked });
-              }}
-              style={{ whiteSpace: 'nowrap' }}
-            >
-              Show Guideline
-            </Checkbox>
-            <Form.Item
-              label='Guide Color:'
-              labelCol={{ span: 9, style: { textAlign: 'left' } }}
-              wrapperCol={{ span: 14 }}
-              style={{ marginBottom: 0 }}
-            >
-              <ColorPicker
-                onColorChange={((color) => {
-                  onUpdate({
-                    pvalueThresholdColor: color,
-                  });
-                })}
-                color={config.pvalueThresholdColor}
-                size='small'
-              />
-            </Form.Item>
-            <Text type='secondary'>
-              -log₁₀(adj p-value) =
-              {' '}
-              {config.adjPvalueThreshold > 0 ? (-Math.log10(config.adjPvalueThreshold)).toPrecision(3) : 'Infinity'}
-            </Text>
-          </Space>
+        <Form.Item
+          label='Threshold:'
+          labelCol={{ span: 9, style: { textAlign: 'left' } }}
+          wrapperCol={{ span: 15 }}
+        >
+          <SliderWithInput
+            min={0.00001}
+            max={0.5}
+            step={0.001}
+            value={config.adjPvalueThreshold}
+            onUpdate={(val) => debouncedUpdate({ adjPvalueThreshold: val })}
+          />
+        </Form.Item>
+        <Form.Item
+          label='Show Guideline:'
+          labelCol={{ span: 9, style: { textAlign: 'left' } }}
+          wrapperCol={{ span: 15 }}
+        >
+          <Checkbox
+            checked={config.showpvalueThresholdGuides}
+            onChange={(e) => {
+              onUpdate({ showpvalueThresholdGuides: e.target.checked });
+            }}
+          />
+        </Form.Item>
+        <Form.Item
+          label='Guide Color:'
+          labelCol={{ span: 9, style: { textAlign: 'left' } }}
+          wrapperCol={{ span: 15 }}
+        >
+          <ColorPicker
+            onColorChange={((color) => {
+              onUpdate({
+                pvalueThresholdColor: color,
+              });
+            })}
+            color={config.pvalueThresholdColor}
+            size='small'
+          />
+        </Form.Item>
+        <Form.Item
+          label='Display:'
+          labelCol={{ span: 9, style: { textAlign: 'left' } }}
+          wrapperCol={{ span: 15 }}
+        >
+          <Text type='secondary'>
+            -log₁₀(adj p-value) =
+            {' '}
+            {config.adjPvalueThreshold > 0 ? (-Math.log10(config.adjPvalueThreshold)).toPrecision(3) : 'Infinity'}
+          </Text>
         </Form.Item>
 
         <p><strong>Fold change (log):</strong></p>
-        <Form.Item>
-          <Space direction='vertical' style={{ width: '100%' }}>
-            <SliderWithInput
-              min={0.01}
-              max={5}
-              step={0.01}
-              value={config.logFoldChangeThreshold}
-              onUpdate={(val) => debouncedUpdate({ logFoldChangeThreshold: val })}
-            />
-            <Checkbox
-              checked={config.showLogFoldChangeThresholdGuides}
-              onChange={(e) => {
-                onUpdate({ showLogFoldChangeThresholdGuides: e.target.checked });
-              }}
-              style={{ whiteSpace: 'nowrap' }}
-            >
-              Show Guideline
-            </Checkbox>
-            <Form.Item
-              label='Guide Color:'
-              labelCol={{ span: 9, style: { textAlign: 'left' } }}
-              wrapperCol={{ span: 14 }}
-              style={{ marginBottom: 0 }}
-            >
-              <ColorPicker
-                onColorChange={((color) => {
-                  onUpdate({
-                    logFoldChangeThresholdColor: color,
-                  });
-                })}
-                color={config.logFoldChangeThresholdColor}
-                size='small'
-              />
-            </Form.Item>
-          </Space>
+        <Form.Item
+          label='Threshold:'
+          labelCol={{ span: 9, style: { textAlign: 'left' } }}
+          wrapperCol={{ span: 15 }}
+        >
+          <SliderWithInput
+            min={0.01}
+            max={5}
+            step={0.01}
+            value={config.logFoldChangeThreshold}
+            onUpdate={(val) => debouncedUpdate({ logFoldChangeThreshold: val })}
+          />
+        </Form.Item>
+        <Form.Item
+          label='Show Guideline:'
+          labelCol={{ span: 9, style: { textAlign: 'left' } }}
+          wrapperCol={{ span: 15 }}
+        >
+          <Checkbox
+            checked={config.showLogFoldChangeThresholdGuides}
+            onChange={(e) => {
+              onUpdate({ showLogFoldChangeThresholdGuides: e.target.checked });
+            }}
+          />
+        </Form.Item>
+        <Form.Item
+          label='Guide Color:'
+          labelCol={{ span: 9, style: { textAlign: 'left' } }}
+          wrapperCol={{ span: 15 }}
+        >
+          <ColorPicker
+            onColorChange={((color) => {
+              onUpdate({
+                logFoldChangeThresholdColor: color,
+              });
+            })}
+            color={config.logFoldChangeThresholdColor}
+            size='small'
+          />
         </Form.Item>
 
         <p><strong>Guideline Design</strong></p>
         <Form.Item
           label='Width:'
+          labelCol={{ span: 9, style: { textAlign: 'left' } }}
+          wrapperCol={{ span: 15 }}
         >
           <SliderWithInput
             min={1}
